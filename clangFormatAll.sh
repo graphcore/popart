5237a2edb96f102f5ad732c169bd808e3e953310
James Newling <jamesn@graphcore.ai>
2018-10-06 20:17:44 +0100

    simplify clang-formatting as the project expands to multiple directories

---
+++ clangFormatAll.sh
@@ -0,0 +1,6 @@
+echo "clang-format -i neuralnet/src/*cpp"
+clang-format -i neuralnet/src/*cpp
+echo "clang-format -i neuralnet/include/neuralnet/*hpp"
+clang-format -i neuralnet/include/neuralnet/*hpp
+echo "clang-format -i driver/*cpp"
+clang-format -i driver/*cpp
