62bfa5fae8ab892a9c10587b5ccfe1f70d3cb4c6
David Norman <davidn@graphcore.ai>
2018-11-01 10:49:11 +0000

    Use explicit destination path
    Summary: Don't assume paths are relative to the install dir
    
    Reviewers: jamesn
    
    Reviewed By: jamesn
    
    Differential Revision: https://phabricator.sourcevertex.net/D7240

---
+++ tests/basic/loader_test.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+from pywillow import NllLoss, L1Loss, EarlyInfo, TensorInfo, PyStepIO
+from pywillow import DataFlow, SGD, ConstSGD, WillowNet, getTensorInfo
+from pywillow import AnchorReturnType
+
+
