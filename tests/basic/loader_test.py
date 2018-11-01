b05f022422dbe97c67d2c3be1a4f38e1d453abec
David Norman <davidn@graphcore.ai>
2018-11-01 11:56:14 +0000

    Get the basic loader test running
    Summary:
    Make the loader_test into a ctest target
    
    This required making the libwillow.so having the poplar libraries with the RPATHs
    
    Test Plan: Just run locally
    
    Reviewers: jamesn
    
    Reviewed By: jamesn
    
    Differential Revision: https://phabricator.sourcevertex.net/D7244

--- tests/basic/loader_test.py
+++ tests/basic/loader_test.py
@@ -1,6 +1,8 @@
 import sys
 import os
 
+sys.path.append('../lib')
+
 from pywillow import NllLoss, L1Loss, EarlyInfo, TensorInfo, PyStepIO
 from pywillow import DataFlow, SGD, ConstSGD, WillowNet, getTensorInfo
 from pywillow import AnchorReturnType
