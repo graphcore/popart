6f5b68b49188423146bb7131c41f560f02b58b98
David Norman <davidn@graphcore.ai>
2018-11-20 17:27:27 +0000

    Adding install_release script for the release system
    Summary: Adding install_release script for the release agent
    
    Test Plan: None
    
    Reviewers: anniej
    
    Reviewed By: anniej
    
    Differential Revision: https://phabricator.sourcevertex.net/D7657

---
+++ install_release.py
@@ -0,0 +1,21 @@
+# This script is run by the release agent to create a release of Tensorflow
+
+
+def install_release(release_utils, release_id, snapshot_id, version_str):
+    release_utils.log.info('Tagging poponnx release ' + version_str)
+
+    # Create the release on the document server.
+    release_utils.create_document_release(snapshot_id)
+
+    # Tag the view repository with the release.
+    release_utils.tag_view_repo(
+            'ssh://git@phabricator.sourcevertex.net/diffusion/' \
+            + 'POPONNXVIEW/poponnxview.git',
+            snapshot_id,
+            release_id,
+            version_str)
+
+    # Increment the point version number.
+    release_utils.increment_version_point(
+            'ssh://git@phabricator.sourcevertex.net/diffusion/' \
+            + 'POPONNX/poponnx.git')
