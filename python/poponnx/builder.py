043d07f498ff498d36ce03ea55f206073edb6f3b
Oskar Bunyan <oskarb@graphcore.ai>
2019-02-06 12:39:36 +0000

    Reorder Builder __init__ params
    Summary: Change the ordering of __init__ params to match previous API.
    
    Reviewers: simonl
    
    Reviewed By: simonl
    
    Differential Revision: https://phabricator.sourcevertex.net/D9166

--- python/poponnx/builder.py
+++ python/poponnx/builder.py
@@ -4,14 +4,7 @@ import poponnx
 # A wrapper around the Builder cpp class, renamed BuilderCore in pybind,
 # to enable more pythonic use. See builder.hpp for the class definition.
 class Builder(poponnx.BuilderCore):
-
-    #    def __init__(self, modelProtoOrFilename=None):
-    #        if modelProtoOrFilename is None:
-    #            super(Builder, self).__init__()
-    #        else:
-    #            super(Builder, self).__init__(modelProtoOrFilename)
-
-    def __init__(self, opsets=None, modelProtoOrFilename=None):
+    def __init__(self, modelProtoOrFilename=None, opsets=None):
 
         if (opsets == None):
 
