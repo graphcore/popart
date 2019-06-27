b3ab1a3cac61db3ffe78df6da49b30acd4497951
George Roger <georger@rs5-00.bris.graphcore.ai>
2019-06-27 09:12:12 +0100

    Using replication index in dropout seed
    Summary: Put up diff so I can access it from a different machine
    
    Reviewers: davidn, jamesn, simonl, evan, matthewha
    
    Reviewed By: simonl
    
    Maniphest Tasks: T8906
    
    Differential Revision: https://phabricator.sourcevertex.net/D12287

--- willow/src/popx/op/dropoutx.cpp
+++ willow/src/popx/op/dropoutx.cpp
@@ -1,3 +1,4 @@
+#include <popops/ElementWise.hpp>
 #include <poprand/RandomGen.hpp>
 #include <poponnx/error.hpp>
 #include <poponnx/ir.hpp>
@@ -42,21 +43,44 @@ void DropoutOpx::grow(poplar::program::Sequence &prog) const {
       refTensor = dv_p->dropoutReferenceTensors.at(seedModifier);
     }
 
+    auto seed    = getSeed(prog);
     auto dropout = poprand::dropout(graph(),
-                                    dv_p->getDropoutRandomSeed(),
+                                    &seed,
                                     seedModifier,
                                     getInTensor(DropoutOp::getInIndex()),
                                     refTensor,
                                     dropoutProbability,
                                     1 / dropoutProbability,
                                     prog,
-                                    idStr());
+                                    debugPrefix("dropout"));
 
     setOutTensor(dropoutOp->getOutIndex(), dropout);
   } else {
     // In inference/evaluation mode, dropout is an idendity function
     setOutTensor(DropoutOp::getOutIndex(),
                  getInTensor(DropoutOp::getInIndex()));
+  }
+}
+
+poplar::Tensor DropoutOpx::getSeed(poplar::program::Sequence &prog) const {
+  if (dv_p->getReplicationFactor() == 1) {
+    return *dv_p->getDropoutRandomSeed();
+  } else {
+    static unsigned tileCounter = 0;
+    auto tile = tileCounter % graph().getTarget().getTilesPerIPU();
+    tileCounter++;
+
+    auto indexConstant = graph().addReplicationIndexConstant();
+    graph().setTileMapping(indexConstant, tile);
+
+    auto seed = popops::map(graph(),
+                            popops::expr::BinaryOpType::ADD,
+                            *dv_p->getDropoutRandomSeed(),
+                            indexConstant,
+                            prog,
+                            debugPrefix("seedAddReplicationIndex"));
+
+    return seed;
   }
 }
 
