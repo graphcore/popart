1a70b22c372a9e25df2fe1d6b263423afb7d7b8d
George Roger <georger@graphcore.ai>
2019-07-31 08:39:40 +0100

    Throw an error if onnx has already been imported
    Reviewers: davidn, jamesn, simonl, evan
    
    Reviewed By: simonl
    
    Maniphest Tasks: T9912
    
    Differential Revision: https://phabricator.sourcevertex.net/D13175

---
+++ tests/popart/import_test.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def test_import_order():
+    import onnx
+    with pytest.raises(ImportError) as error:
+        import popart
+
+    assert error.value.args[0] == (
+        'It looks like onnx has already been imported. Due to an ongoing '
+        'issue, popart must be imported before onnx.')
