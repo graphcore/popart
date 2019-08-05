graphcore/popart

commit d6b3666b53ff3c78bc9d5fa181f9e6aa6264dfe0

Pipelined dropout
Summary: DropoutOp can now return a seed. This seed can then be used by the DropoutGradOp

Reviewers: jamesn, evan

Reviewed By: jamesn

Maniphest Tasks: T9981

Differential Revision: https://phabricator.sourcevertex.net/D12889

willow/src/transforms/pipeline.cpp
@@ -210,7 +210,12 @@ bool Pipeline::apply(Graph &graph) const {
       }
     }
 
-    if (isConsumedByOpScheduledPreLoss && isConsumedByOpScheduledPostLoss) {
+    bool isProducedPreLoss =
+        tensor->hasProducer() &&
+        tensor->getProducer()->scheduledPreLoss == ScheduledPreLoss::Yes;
+
+    if ((isConsumedByOpScheduledPreLoss || isProducedPreLoss) &&
+        isConsumedByOpScheduledPostLoss) {
       toStashTensors.push_back(tid);
     }
   }

tests/poponnx/operators_test/dropout_test.py
@@ -24,7 +24,7 @@ def test_dropout_testing(op_tester):
     op_tester.run(init_builder, reference, 'infer')
 
     # ... and with identity pattern
-    op_tester.passes = ['OpToIdentity', 'DropoutGradOp']
+    op_tester.passes = ['OpToIdentity']
     op_tester.run(init_builder, reference, 'infer')
 
 
@@ -56,30 +56,6 @@ def test_dropout_training1():
     reference = d1 * anchors[o2] * (1 / (1 - ratio))
 
     assert (np.isclose(anchors[o1], reference)).all()
-
-
-# Verify that poponnx errors our properly when the DropoutGradOp is
-# not applied
-def test_dropout_training2(op_tester):
-    d1 = np.random.rand(10).astype(np.float32)
-
-    def init_builder(builder):
-        i1 = builder.addInputTensor(d1)
-        [o1] = builder.aiOnnx.dropout([i1], num_outputs=1)
-        builder.addOutputTensor(o1)
-        return [o1, poponnx.reservedGradientPrefix() + i1]
-
-    def reference(ref_data):
-        dropout = torch.nn.Dropout()
-        out = dropout(torch.tensor(d1))
-        return [out]
-
-    # 'DropoutGradOp' pattern not applied
-    with pytest.raises(poponnx.poponnx_exception) as e_info:
-        op_tester.run(init_builder, reference, 'train')
-
-    assert (e_info.value.args[0].startswith(
-        "DropoutGradOp should be optimised out"))
 
 
 # We cannot directly test poponnx dropout vs pytorch here, because they have

tests/poponnx/pipelining_tests/pipeline_test.py
@@ -379,6 +379,133 @@ def test_output_matches_infer():
         assert np.allclose(t1, t2)
 
 
+## TODO T8803 : requires hardware or a sim device
+def test_pipelined_dropout():
+    # The test can be run without pipelining for debugging.
+    def test(do_pipelining):
+        dsize = 10
+        ratio = 0.5
+        if do_pipelining:
+            ipus = 4
+        else:
+            ipus = 1
+        layers = 4
+        batches_per_step = 7
+
+        # Ensure inputs in range [1.0, 2.0] to ensure comparing with 0 is valid
+        ip_shape = [dsize]
+        ip_data = np.full([batches_per_step] + ip_shape, 1).astype(np.float32)
+
+        dropouts = []
+        dropoutGrads = []
+        dropoutInputs = []
+        dropoutOutputs = []
+
+        builder = poponnx.Builder()
+        ip = builder.addInputTensor(poponnx.TensorInfo("FLOAT", ip_shape))
+
+        def add_layer(layer_input, vgraph_num):
+            # This is to get the output of the dropout in the bwd pass.
+            # D_next_layer_in also includes the gradient of the AddOp.
+            identity0 = builder.aiOnnx.identity([layer_input])
+            if do_pipelining:
+                builder.virtualGraph(identity0, vgraph_num)
+
+            [dropout0] = builder.aiOnnx.dropout([identity0],
+                                                num_outputs=1,
+                                                ratio=ratio)
+            if do_pipelining:
+                builder.virtualGraph(dropout0, vgraph_num)
+
+            # the input to the forward pass dropout
+            dropoutInputs.append(identity0)
+            # the input to the backward pass dropout
+            dropoutInputs.append(poponnx.reservedGradientPrefix() + dropout0)
+            # the output of the backward pass dropout
+            dropoutGrads.append(poponnx.reservedGradientPrefix() + identity0)
+            # the output of the forward pass dropout
+            dropouts.append(dropout0)
+
+            # This ensures the all input elements to the dropouts, in both
+            # the forward and backward passes, will be non-zero.
+            add0 = builder.aiOnnx.add([layer_input, dropout0])
+            if do_pipelining:
+                builder.virtualGraph(add0, vgraph_num)
+
+            return add0
+
+        # construct a graph of `layers` number of layers
+        # with each layer on a different IPU.
+        next_layer_in = ip
+        for vgraph in range(layers):
+            next_layer_in = add_layer(next_layer_in, vgraph)
+        out = next_layer_in
+        builder.addOutputTensor(out)
+
+        device = poponnx.DeviceManager().acquireAvailableDevice(numIpus=ipus)
+        if device is None:
+            pytest.skip("Test needs to run on IPU, but none are available")
+
+        dfAnchors = {}
+        for x in dropouts + dropoutGrads + dropoutInputs:
+            dfAnchors[x] = poponnx.AnchorReturnType("ALL")
+
+        dataFlow = poponnx.DataFlow(batches_per_step, dfAnchors)
+
+        loss = poponnx.L1Loss(out, "l1LossVal", 0.1)
+        if do_pipelining:
+            loss.virtualGraph(layers - 1)
+
+        userOptions = poponnx.SessionOptions()
+        userOptions.enableVirtualGraphs = do_pipelining
+        userOptions.enablePipelining = do_pipelining
+
+        session = poponnx.TrainingSession(
+            fnModel=builder.getModelProto(),
+            dataFeed=dataFlow,
+            optimizer=poponnx.ConstSGD(0.1),
+            losses=[loss],
+            userOptions=userOptions,
+            deviceInfo=device)
+
+        session.prepareDevice()
+        session.weightsFromHost()
+        anchors = session.initAnchorArrays()
+        session.setRandomSeed(0)
+
+        stepio = poponnx.PyStepIO({ip: ip_data}, anchors)
+
+        session.run(stepio)
+
+        print(anchors.keys())
+
+        # Check that none of the elements of the dropout inputs are zero
+        for tid in dropoutInputs:
+            x = anchors[tid]
+            print(f'{tid}: {x}')
+            zero = np.zeros(x.shape)
+            assert not np.any(np.equal(x, zero)), \
+                   f'Some elements of dropout input {tid} are zero'
+
+        print()
+
+        # For each dropout, check that the masked out elements are the same
+        # in the forward and backward passes.
+        for fwdId, bwdId in zip(dropouts, dropoutGrads):
+            print(f'{fwdId}:\n{np.sign(anchors[fwdId])}')
+            print(f'{bwdId}:\n{np.sign(anchors[bwdId])}')
+            lhs = np.sign(anchors[fwdId])
+            rhs = np.sign(anchors[bwdId])
+            assert np.array_equal(lhs, rhs), \
+                   f'{fwdId} and {bwdId} did not use the same dropout mask'
+            print()
+
+    # Test without pipelining for debugging purposes.
+    # test(do_pipelining=False)
+
+    test(do_pipelining=True)
+
+
 # Model
 #  <--- ipu0 ----> <--------- ipu1 ---> <------------ ipu2 ------------>
 #

willow/include/poponnx/op.hpp
@@ -98,6 +98,8 @@ public:
     // depending on whether the attribute has been
     // set in the onnx model.
     virtual void setFromAttributes(const Attributes &attributes);
+
+    Ir &getIr() const;
   };
 
   Settings settings;

willow/include/poponnx/op/dropout.hpp
@@ -5,7 +5,26 @@
 
 namespace poponnx {
 
-class DropoutOp : public Op {
+class DropoutBaseOp : public Op {
+public:
+  DropoutBaseOp(const OperatorIdentifier &opid_,
+                float ratio_,
+                uint32_t seedModifier_,
+                const Op::Settings &settings_);
+
+  uint32_t getSeedModifier() const;
+  void setSeedModifier(uint32_t sm);
+  float getRatio() const;
+  void setRatio(float r);
+
+  float getSubgraphValue() const final;
+
+protected:
+  float ratio;
+  uint32_t seedModifier;
+};
+
+class DropoutOp : public DropoutBaseOp {
 public:
   DropoutOp(const OperatorIdentifier &_opid,
             float ratio_,
@@ -23,25 +42,17 @@ public:
   // Ouputs
   static OutIndex getOutIndex() { return 0; }
   static OutIndex getMaskOutIndex() { return 1; }
+  static OutIndex getSeedOutIndex() { return 2; }
 
   bool canBeReplacedByIdentity() override;
-
-  uint32_t getSeedModifier() const { return seedModifier; }
-  void setSeedModifier(uint32_t sm) { seedModifier = sm; }
-  float getRatio() const { return ratio; }
-  void setRatio(float r) { ratio = r; }
-
-  float getSubgraphValue() const final { return getLowSubgraphValue(); }
 
   bool returnMask() const { return output_mask; }
 
 private:
   bool output_mask = false;
-  float ratio;
-  uint32_t seedModifier;
 };
 
-class DropoutGradOp : public Op {
+class DropoutGradOp : public DropoutBaseOp {
 public:
   DropoutGradOp(const DropoutOp &fwdOp);
 
@@ -51,15 +62,8 @@ public:
   void setup() final;
 
   static InIndex getGradInIndex() { return 0; }
+  static InIndex getSeedInIndex() { return 1; }
   static OutIndex getOutIndex() { return 0; }
-  uint32_t getSeedModifier() const { return seedModifier; }
-  float getRatio() const { return ratio; }
-
-  float getSubgraphValue() const final { return getLowSubgraphValue(); }
-
-private:
-  float ratio;
-  uint32_t seedModifier;
 };
 
 } // namespace poponnx

willow/include/poponnx/patterns/dropoutgradoppattern.hpp
@@ -1,19 +1,0 @@
-#ifndef GUARD_NEURALNET_DROPOUT_GRAD_OP_PATTERN_HPP
-#define GUARD_NEURALNET_DROPOUT_GRAD_OP_PATTERN_HPP
-
-#include <poponnx/patterns/patterns.hpp>
-
-namespace poponnx {
-
-// Replace a DropoutGradOp with
-// (grad_in) -> [Dropout] -> (grad_out)
-class DropoutGradOpPattern : public PreAliasPattern {
-public:
-  bool matches(Op *) const override;
-  std::vector<const Tensor *> touches(Op *) const override;
-  bool apply(Op *) const override;
-};
-
-} // namespace poponnx
-
-#endif

willow/include/poponnx/patterns/pattern.hpp
@@ -26,7 +26,6 @@ enum class PreAliasPatternType {
   SQRTGRADOP,
   EXPGRADOP,
   LOGGRADOP,
-  DROPOUTGRADOP,
   LOGSOFTMAXOP,
   COSHOP,
   GEMMDECOMPOSITION,

willow/include/poponnx/patterns/patterns.hpp
@@ -189,9 +189,6 @@ public:
   bool isNegativeOneScaleEnabled() {
     return isPatternEnabled(PreAliasPatternType::NEGATIVEONESCALE);
   }
-  bool isDropoutGradOpEnabled() {
-    return isPatternEnabled(PreAliasPatternType::DROPOUTGRADOP);
-  }
 
   // The following methods are fluent allow you to
   // Pattens().enableInPlace0(false).
@@ -272,9 +269,6 @@ public:
   }
   Patterns &enableNegativeOneScale(bool v) {
     return enablePattern(PreAliasPatternType::NEGATIVEONESCALE, v);
-  }
-  Patterns &enableDropoutGradOp(bool v) {
-    return enablePattern(PreAliasPatternType::DROPOUTGRADOP, v);
   }
 
   std::vector<std::unique_ptr<PreAliasPattern>> getPreAliasList();

commit 2bfae1092b91815ddb7f4f3144449ceb2cc251b7

More pipelining bug fixes
Summary:
Two fixes

1. Want to stream a tensor to multiple IPUs (e.g. optimizer tensors)? Previously, if pipelining was enabled, this would fail during Ir creation, due to unsuccessful 'contiguation' of IPU copies. This diff fixes this problem
  - Fixed this by modifying the interipucopy transform
2. Training with pipelining has been broken since commit `d6b3666b53ff3c78bc9d5fa181f9e6aa6264dfe0`, when tensors 'produced pre loss' are stashed for pipelining.
  - The root issue is that was due to *SoftmaxGradDirect ops did not have their `fromLoss` attribute set as expected, causing subsequent bwd IpuCopy ops' produced tensors judged to be stashable by the pipelining transform.
  - Fixed by making *SoftmaxGradDirect have their `fromLoss` attribute set correctly
  - TODO in a public_examples diff - write a regression test for mnist training with and without pipelining

Test Plan:
All tests pass (os/x)
`cd public_examples/onnx/image_classification/mnist; python popart_mnist.py --num-ipus=2 --pipeline --simulation` now matches `python popart_mnist.py --num-ipus=2`

Reviewers: jamesn, george, matthewha, simonl

Reviewed By: jamesn

Differential Revision: https://phabricator.sourcevertex.net/D13261

willow/include/popart/op/ipucopy.hpp
@@ -5,7 +5,8 @@
 
 namespace popart {
 
-using SourceIpuMap = std::map<TensorId, uint64_t>;
+using SourceIpuMap    = std::map<TensorId, uint64_t>;
+using SourceTensorMap = std::map<uint64_t, std::vector<TensorId>>;
 
 class IpuCopyOp : public Op {
 public:
@@ -17,8 +18,11 @@ public:
 
   uint64_t getDestIpu() const { return destIpu; }
   const SourceIpuMap &getSourceIpus() const;
+  const SourceTensorMap &getSourceTensors() const;
   uint64_t getSourceIpu(const TensorId &tenId) const;
   uint64_t getSourceIpu() const;
+  uint64_t getMinSourceIpu() const;
+  uint64_t getMaxSourceIpu() const;
 
   void appendAttributes(OpSerialiserBase &) const override;
 
@@ -41,6 +45,7 @@ private:
   }
 
   SourceIpuMap sourceIpus;
+  SourceTensorMap sourceTensors;
   uint64_t destIpu;
 };
 } // namespace popart

willow/src/ir.cpp
@@ -1480,9 +1480,9 @@ void Ir::updateVertices() {
   logging::ir::trace(
       "Updating all Vertices (toLoss, fromLoss, scheduledPreLoss)");
 
-  // 1) get All Ops which have toLoss true, and backwards propagate
+  // 1) Get all Ops which have toLoss Yes, and backwards propagate
   std::vector<Op *> toLossFrontier;
-  // 1) get All Ops which have fromLoss Yes, and backwards propagate
+  // 2) Get all Ops which have fromLoss Yes, and forwards propagate
   std::vector<Op *> fromLossFrontier;
   for (auto &id_op : getMainGraph().getOps()) {
     Op *op = id_op.second.get();

willow/src/op/ipucopy.cpp
@@ -38,6 +38,10 @@ void IpuCopyOp::setup() {
 
 const SourceIpuMap &IpuCopyOp::getSourceIpus() const { return sourceIpus; }
 
+const SourceTensorMap &IpuCopyOp::getSourceTensors() const {
+  return sourceTensors;
+}
+
 uint64_t IpuCopyOp::getSourceIpu(const TensorId &tenId) const {
   return sourceIpus.at(tenId);
 }
@@ -51,6 +55,28 @@ uint64_t IpuCopyOp::getSourceIpu() const {
     }
   }
   return sourceIpu;
+}
+
+uint64_t IpuCopyOp::getMinSourceIpu() const {
+  auto minSourceIpu = sourceIpus.begin()->second;
+
+  for (auto id_source : sourceIpus) {
+    if (minSourceIpu < id_source.second) {
+      minSourceIpu = id_source.second;
+    }
+  }
+  return minSourceIpu;
+}
+
+uint64_t IpuCopyOp::getMaxSourceIpu() const {
+  auto maxSourceIpu = sourceIpus.begin()->second;
+
+  for (auto id_source : sourceIpus) {
+    if (maxSourceIpu > id_source.second) {
+      maxSourceIpu = id_source.second;
+    }
+  }
+  return maxSourceIpu;
 }
 
 void IpuCopyOp::appendAttributes(OpSerialiserBase &os) const {
@@ -67,6 +93,12 @@ void IpuCopyOp::connectInTensor(InIndex inIndex,
                                 TensorId tenId,
                                 uint64_t sourceIpu) {
   sourceIpus.insert({tenId, sourceIpu});
+  if (sourceTensors.find(sourceIpu) == sourceTensors.end()) {
+    sourceTensors.insert({sourceIpu, {tenId}});
+  } else {
+    std::vector<TensorId> &tensorIds = sourceTensors.at(sourceIpu);
+    tensorIds.push_back(tenId);
+  }
   defaultConnectInTensor(inIndex, tenId);
 }
 

willow/src/op/softmax.cpp
@@ -123,6 +123,13 @@ std::unique_ptr<Op> SoftmaxGradDirectOp::clone() const {
 void SoftmaxGradDirectOp::setup() {
   // gradient of activations has same shape as probabilities
   outInfo(getOutIndex()) = inInfo(getInIndex());
+
+  // When replacing a SoftmaxGradOp with a SoftmaxGradDirectOp, we are
+  // disconnecting the path from the final loss to the ops downstream.
+  // However, we don't want updateVertices() alter the attributes of these
+  // ops after performing the SoftmaxGradDirectOp transform. So here we
+  // 'pretend' that the SoftmaxGradDirectOp has a path from the final loss.
+  fromLoss = PathFromLoss::Yes;
 }
 
 bool SoftmaxGradDirectOp::hasNlllFwdOp() const {
@@ -186,6 +193,14 @@ void NlllWithSoftmaxGradDirectOp::setup() {
   outInfo(getLossOutIndex())
       .set(inInfo(getProbsInIndex()).dataType(),
            inInfo(getLabelInIndex()).shape());
+
+  // When replacing a Nlll and SoftmaxGradOp with a NlllWithSoftmaxGradDirectOp,
+  // we are disconnecting the path from the final loss to the ops downstream.
+  // However, we don't want updateVertices() alter the attributes of these
+  // ops after performing the NlllWithSoftmaxGradDirectOp transform. So here we
+  // 'pretend' that the NlllWithSoftmaxGradDirectOp has a path from the final
+  // loss.
+  fromLoss = PathFromLoss::Yes;
 }
 
 namespace {

willow/src/patterns/contiguateipucopyindices.cpp
@@ -12,11 +12,11 @@ namespace popart {
 bool ContiguateIpuCopyIndicesPattern::matches(Op *op) const {
   auto copyOp = dynamic_cast<IpuCopyOp *>(op);
   if (copyOp) {
-    if (copyOp->getSourceIpus().size() != 1) {
+    if (copyOp->getSourceTensors().size() != 1) {
       return false;
     }
-    auto delta = static_cast<int64_t>(copyOp->getDestIpu()) -
-                 static_cast<int64_t>(copyOp->getSourceIpu());
+    auto delta = static_cast<VGraphId>(copyOp->getDestIpu()) -
+                 static_cast<VGraphId>(copyOp->getSourceIpu());
 
     if (delta == +1 || delta == -1) {
       return false;
@@ -38,11 +38,11 @@ bool ContiguateIpuCopyIndicesPattern::apply(Op *op) const {
   auto originalIpuCopyOp = dynamic_cast<IpuCopyOp *>(op);
 
   // Creation of intermediate IpuCopyOps:
-  int64_t firstIpuId = originalIpuCopyOp->getSourceIpu();
-  int64_t finalIpuId = originalIpuCopyOp->getDestIpu();
-  auto delta         = firstIpuId < finalIpuId ? +1 : -1;
+  VGraphId firstIpuId = originalIpuCopyOp->getSourceIpu();
+  VGraphId finalIpuId = originalIpuCopyOp->getDestIpu();
+  auto delta          = firstIpuId < finalIpuId ? +1 : -1;
   std::vector<std::unique_ptr<IpuCopyOp>> seq;
-  for (int64_t src = firstIpuId; src != finalIpuId; src += delta) {
+  for (VGraphId src = firstIpuId; src != finalIpuId; src += delta) {
     auto dst = src + delta;
     seq.push_back(std::make_unique<IpuCopyOp>(
         Onnx::CustomOperators::IpuCopy, dst, op->getSettings()));
@@ -55,24 +55,25 @@ bool ContiguateIpuCopyIndicesPattern::apply(Op *op) const {
         tensors.first, tensors.second, originalIpuCopyOp->getSourceIpu());
   }
 
-  // Connect the output tensor to the back of the sequence
+  // Connect the output tensor to the back of the sequence. Note: this
+  // can have more than one ouput (all with the same destIPUs) if this
+  // IpuCopy has been merged in the MergeCopies transform
   auto &finalIpuCopy = seq.back();
-  if (op->output->tensorIdMap().size() != 1) {
-    throw error("ILE : expected a single output Tensor for an IpuCopyOp");
-  }
   for (auto &tensors : op->output->tensorIdMap()) {
     finalIpuCopy->connectOutTensor(tensors.first, tensors.second);
   }
 
   // Connect the sequence of IpuCopys with intermediate Tensors
   int seqIndex = 0;
-  for (int64_t src = firstIpuId; src != finalIpuId - delta; src += delta) {
+  for (VGraphId src = firstIpuId; src != finalIpuId - delta; src += delta) {
     auto srcOp  = seq.at(seqIndex).get();
     auto destOp = seq.at(seqIndex + 1).get();
-    auto tensor = PreAliasPattern::createIntermediateTensorId(
-        originalIpuCopyOp->output->id(0));
-    srcOp->createAndConnectOutTensor(0, tensor);
-    destOp->connectInTensor(0, tensor, src + delta);
+    for (int i = 0; i < originalIpuCopyOp->output->n(); i++) {
+      auto tensor = PreAliasPattern::createIntermediateTensorId(
+          originalIpuCopyOp->output->id(i));
+      srcOp->createAndConnectOutTensor(i, tensor);
+      destOp->connectInTensor(i, tensor, src + delta);
+    }
     ++seqIndex;
   }
 

willow/src/transforms/interipucopy.cpp
@@ -243,6 +243,34 @@ bool InterIpuCopy::apply(Graph &graph) const {
 
       auto sourceOp = *s.second.begin();
 
+      if (graph.getIr().getSessionOptions().enablePipelining) {
+        // We add an additional constraint on the order of IPU copies here:
+        //
+        // 'All stream tensors are to be copied to a larger IPU number
+        //  than that of sourceOp, except for optimizer stream tensors,
+        //  which are to be copied to a smaller IPU number than that of
+        //  sourceOp'
+        //
+        auto optTensors = graph.getIr().optimizerTensors();
+        if (std::find(optTensors.begin(),
+                      optTensors.end(),
+                      graph.getTensors().get(s.first)) == optTensors.end()) {
+          // sourceOp should be op mapped to smallest vGraphId
+          for (Op *op : s.second) {
+            if (op->getVirtualGraphId() < sourceOp->getVirtualGraphId()) {
+              sourceOp = op;
+            }
+          }
+        } else {
+          // sourceOp should be op mapped to largest vGraphId
+          for (Op *op : s.second) {
+            if (op->getVirtualGraphId() > sourceOp->getVirtualGraphId()) {
+              sourceOp = op;
+            }
+          }
+        }
+      }
+
       // Get which ipu the to op is on
       int64_t sourceIpu = -1;
       if (sourceOp->hasVirtualGraphId()) {

willow/src/transforms/pipeline.cpp
@@ -151,15 +151,15 @@ bool Pipeline::apply(Graph &graph) const {
 
   auto ipuCopies = getIpuCopyOps();
   for (auto ipuCopyOp : ipuCopies) {
-    uint64_t sourceIpu = ipuCopyOp->getSourceIpu();
-    uint64_t destIpu   = ipuCopyOp->getDestIpu();
+    uint64_t destIpu = ipuCopyOp->getDestIpu();
     // For an inference graph, or fwd pass of a training graph,
     if (ipuCopyOp->toLoss == PathToLoss::Yes || !ir.canTrain()) {
-      if (destIpu <= sourceIpu) {
-        throw error("For pipelining, forward IpuCopyOps  must copy to an "
+      uint64_t maxSourceIpu = ipuCopyOp->getMaxSourceIpu();
+      if (destIpu <= maxSourceIpu) {
+        throw error("For pipelining, forward IpuCopyOps must copy to an "
                     "IPU of larger index. However {} copies from {} to {}.",
                     ipuCopyOp->debugName(),
-                    sourceIpu,
+                    maxSourceIpu,
                     destIpu);
       }
     }
@@ -167,11 +167,12 @@ bool Pipeline::apply(Graph &graph) const {
     // for bwd pass, IPU Copy must have destination with lower VGraphId than
     // source
     else {
-      if (destIpu >= sourceIpu) {
-        throw error("For pipelining, backward IpuCopyOps  must copy to an "
+      uint64_t minSourceIpu = ipuCopyOp->getMinSourceIpu();
+      if (destIpu >= minSourceIpu) {
+        throw error("For pipelining, backward IpuCopyOps must copy to an "
                     "IPU of smaller index. However {} copies from {} to {}.",
                     ipuCopyOp->debugName(),
-                    sourceIpu,
+                    minSourceIpu,
                     destIpu);
       }
     }
@@ -198,7 +199,7 @@ bool Pipeline::apply(Graph &graph) const {
   ContiguateIpuCopyIndicesPattern contiguator;
   for (auto ipuCopyOp : ipuCopies) {
     if (contiguator.matches(ipuCopyOp)) {
-      logging::transform::debug("Contiguating {}", ipuCopyOp->str());
+      logging::transform::debug("Contiguating {}", ipuCopyOp->debugName());
       contiguator.apply(ipuCopyOp);
     }
   }
@@ -210,7 +211,11 @@ bool Pipeline::apply(Graph &graph) const {
     auto destIpu   = static_cast<int64_t>(ipuCopyOp->getDestIpu());
     auto delta     = destIpu - sourceIpu;
     if (delta != 1 && delta != -1) {
-      throw error("ILE: Failed to contiguate all IpuCopyOps");
+      throw error("ILE: IpuCopy {} is not contiguous. It copies from IPU {} to "
+                  "IPU {}. Failed to contiguate all IpuCopyOps",
+                  ipuCopyOp->debugName(),
+                  sourceIpu,
+                  destIpu);
     }
   }
 