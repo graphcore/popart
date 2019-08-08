graphcore/popart

commit f97ebaed87990e1c6857227dfae082d45b436368

PathFromLoss set immediately following construction of Backwards pass
Summary:
The main change is in ir.cpp, where at the end of
backwards pass construction, all Tensors with
the gradient prefix have pathFromLoss set to Yes.
Also:
(1) format all,
(2) extended topo cons tests,
(3) Task generation,
(4) Initial recompute with pipelining scaffolding.

Test Plan: All recompute tests pass

Reviewers: evan, george, matthewha

Reviewed By: evan

Differential Revision: https://phabricator.sourcevertex.net/D13350

python/popart.cpp
@@ -528,12 +528,13 @@ PYBIND11_MODULE(popart_core, m) {
       .def("__repr__", &PrepareDeviceError::what)
       .def("isSuccessful", &PrepareDeviceError::isSuccessful)
       .def("getSummaryReport", &PrepareDeviceError::getSummaryReport)
-      .def("getGraphReport",
-           [](const PrepareDeviceError &error, bool use_cbor) {
-             auto report = error.getGraphReport(use_cbor);
-             return py::bytes(report);
-           },
-           py::arg("use_cbor") = false);
+      .def(
+          "getGraphReport",
+          [](const PrepareDeviceError &error, bool use_cbor) {
+            auto report = error.getGraphReport(use_cbor);
+            return py::bytes(report);
+          },
+          py::arg("use_cbor") = false);
 
   py::class_<InferenceSession>(m, "InferenceSessionCore")
       .def(py::init(&InferenceSession::createFromOnnxModel),
@@ -544,21 +545,22 @@ PYBIND11_MODULE(popart_core, m) {
            py::arg("inputShapeInfo"),
            py::arg("userOptions"),
            py::arg("patterns"))
-      .def("prepareDevice",
-           [](InferenceSession &session, PrepareDeviceError *status) {
-             try {
-               session.prepareDevice();
-             } catch (const popart::memory_allocation_err &e) {
-               if (status != nullptr) {
-                 status->exception = e.clone();
-                 status->success   = false;
-               } else {
-                 // rethrow the exception
-                 throw;
-               }
-             }
-           },
-           py::arg("err").none())
+      .def(
+          "prepareDevice",
+          [](InferenceSession &session, PrepareDeviceError *status) {
+            try {
+              session.prepareDevice();
+            } catch (const popart::memory_allocation_err &e) {
+              if (status != nullptr) {
+                status->exception = e.clone();
+                status->success   = false;
+              } else {
+                // rethrow the exception
+                throw;
+              }
+            }
+          },
+          py::arg("err").none())
       .def("setRandomSeed",
            &InferenceSession::setRandomSeed,
            py::arg("seedValue"))
@@ -568,18 +570,20 @@ PYBIND11_MODULE(popart_core, m) {
       .def("modelToHost", &InferenceSession::modelToHost)
       .def("getInfo", &InferenceSession::getInfo)
       .def("getSummaryReport", &InferenceSession::getSummaryReport)
-      .def("getGraphReport",
-           [](const InferenceSession &session, bool use_cbor) {
-             auto report = session.getGraphReport(use_cbor);
-             return py::bytes(report);
-           },
-           py::arg("use_cbor") = false)
-      .def("getExecutionReport",
-           [](const InferenceSession &session, bool use_cbor) {
-             auto report = session.getExecutionReport(use_cbor);
-             return py::bytes(report);
-           },
-           py::arg("use_cbor") = false)
+      .def(
+          "getGraphReport",
+          [](const InferenceSession &session, bool use_cbor) {
+            auto report = session.getGraphReport(use_cbor);
+            return py::bytes(report);
+          },
+          py::arg("use_cbor") = false)
+      .def(
+          "getExecutionReport",
+          [](const InferenceSession &session, bool use_cbor) {
+            auto report = session.getExecutionReport(use_cbor);
+            return py::bytes(report);
+          },
+          py::arg("use_cbor") = false)
       .def("getSerializedGraph",
            [](const InferenceSession &session) {
              auto report = session.getSerializedGraph();
@@ -599,21 +603,22 @@ PYBIND11_MODULE(popart_core, m) {
            py::arg("userOptions"),
            py::arg("patterns"))
       .def("updateOptimizer", &TrainingSession::updateOptimizer)
-      .def("prepareDevice",
-           [](TrainingSession &session, PrepareDeviceError *status) {
-             try {
-               session.prepareDevice();
-             } catch (const popart::memory_allocation_err &e) {
-               if (status != nullptr) {
-                 status->exception = e.clone();
-                 status->success   = false;
-               } else {
-                 // rethrow the exception
-                 throw;
-               }
-             }
-           },
-           py::arg("err").none())
+      .def(
+          "prepareDevice",
+          [](TrainingSession &session, PrepareDeviceError *status) {
+            try {
+              session.prepareDevice();
+            } catch (const popart::memory_allocation_err &e) {
+              if (status != nullptr) {
+                status->exception = e.clone();
+                status->success   = false;
+              } else {
+                // rethrow the exception
+                throw;
+              }
+            }
+          },
+          py::arg("err").none())
       .def("setRandomSeed",
            &TrainingSession::setRandomSeed,
            py::arg("seedValue"))
@@ -626,18 +631,20 @@ PYBIND11_MODULE(popart_core, m) {
       .def("modelToHost", &TrainingSession::modelToHost)
       .def("getInfo", &TrainingSession::getInfo)
       .def("getSummaryReport", &TrainingSession::getSummaryReport)
-      .def("getGraphReport",
-           [](const TrainingSession &session, bool use_cbor) {
-             auto report = session.getGraphReport(use_cbor);
-             return py::bytes(report);
-           },
-           py::arg("use_cbor") = false)
-      .def("getExecutionReport",
-           [](const TrainingSession &session, bool use_cbor) {
-             auto report = session.getExecutionReport(use_cbor);
-             return py::bytes(report);
-           },
-           py::arg("use_cbor") = false)
+      .def(
+          "getGraphReport",
+          [](const TrainingSession &session, bool use_cbor) {
+            auto report = session.getGraphReport(use_cbor);
+            return py::bytes(report);
+          },
+          py::arg("use_cbor") = false)
+      .def(
+          "getExecutionReport",
+          [](const TrainingSession &session, bool use_cbor) {
+            auto report = session.getExecutionReport(use_cbor);
+            return py::bytes(report);
+          },
+          py::arg("use_cbor") = false)
       .def("getSerializedGraph",
            [](const TrainingSession &session) {
              auto report = session.getSerializedGraph();
@@ -695,15 +702,16 @@ PYBIND11_MODULE(popart_core, m) {
       .def("addInputTensorFromParentGraph",
            &Builder::addInputTensorFromHigherScope,
            py::arg("tensorId"))
-      .def("addInitializedInputTensor",
-           [](Builder &builder, py::array array, std::string &debugPrefix) {
-             ConstVoidData initData;
-             initData.data = array.request().ptr;
-             initData.info = getTensorInfo(array);
-             return builder.addInitializedInputTensor(initData, debugPrefix);
-           },
-           py::arg("initVal"),
-           py::arg("debugPrefix") = std::string())
+      .def(
+          "addInitializedInputTensor",
+          [](Builder &builder, py::array array, std::string &debugPrefix) {
+            ConstVoidData initData;
+            initData.data = array.request().ptr;
+            initData.info = getTensorInfo(array);
+            return builder.addInitializedInputTensor(initData, debugPrefix);
+          },
+          py::arg("initVal"),
+          py::arg("debugPrefix") = std::string())
       .def("addOutputTensor", &Builder::addOutputTensor, py::arg("outputName"))
 
       // Accessors for the ai.onnx domain builder interfac
@@ -811,18 +819,20 @@ PYBIND11_MODULE(popart_core, m) {
                &Builder::virtualGraph),
            py::arg("nodeOutputNames"),
            py::arg("value") = 0)
-      .def("virtualGraph",
-           [](Builder &self, int64_t index) -> AttributeContextManager {
-             AttributeContextManager acm(self, sVirtualGraphAttribute, index);
-             return acm;
-           },
-           py::arg("value"))
-      .def("nameScope",
-           [](Builder &self, const std::string &name) -> NameContextManager {
-             NameContextManager ncm(self, name);
-             return ncm;
-           },
-           py::arg("name"))
+      .def(
+          "virtualGraph",
+          [](Builder &self, int64_t index) -> AttributeContextManager {
+            AttributeContextManager acm(self, sVirtualGraphAttribute, index);
+            return acm;
+          },
+          py::arg("value"))
+      .def(
+          "nameScope",
+          [](Builder &self, const std::string &name) -> NameContextManager {
+            NameContextManager ncm(self, name);
+            return ncm;
+          },
+          py::arg("name"))
       .def("getVirtualGraph",
            static_cast<int64_t (Builder::*)(const TensorId &)>(
                &Builder::getVirtualGraph),

willow/include/popart/tensornames.hpp
@@ -25,11 +25,15 @@ TensorId getRecompId(const TensorId &tenId);
 // get an variable tensor's post-update name, based on original name
 TensorId getUpdatedVarId(const TensorId &id);
 
-constexpr const char *reservedGradientPrefix() { return "D___"; }
-constexpr const char *reservedUpdatedVarPrefix() { return "UV___"; }
-constexpr const char *reservedAccumulationPrefix() { return "A___"; }
-constexpr const char *reservedAccumulationOutPrefix() { return "AO___"; }
-constexpr const char *reservedAccumulationResetPrefix() { return "R___"; }
+constexpr const char *reservedGradientPrefix() { return "Gradient___"; }
+constexpr const char *reservedUpdatedVarPrefix() { return "UpdatedVar___"; }
+constexpr const char *reservedAccumulationPrefix() { return "Accumulation___"; }
+constexpr const char *reservedAccumulationOutPrefix() {
+  return "AccumulationOut___";
+}
+constexpr const char *reservedAccumulationResetPrefix() {
+  return "AccumulationReset___";
+}
 constexpr const char *reservedStashedPrefix() { return "Stashed___"; }
 constexpr const char *reservedRestoredPrefix() { return "Restored___"; }
 

willow/src/transforms/pipeline.cpp
@@ -65,9 +65,14 @@ namespace popart {
 std::size_t Pipeline::id() { return typeid(Pipeline).hash_code(); }
 
 bool Pipeline::apply(Graph &graph) const {
+
   auto &ir     = graph.getIr();
   auto numIPUs = ir.getDeviceInfo()->getNumIpus();
   // We use numIPUs as proxy for sharding factor, not quite correct:TODO T10254
+
+  if (ir.autoRecomputationEnabled()) {
+    throw error("Auto recomputation for Pipelining needs implementing");
+  }
 
   // First, some checks that pipelining is compatible with other user options:
 
@@ -105,10 +110,9 @@ bool Pipeline::apply(Graph &graph) const {
   }
 
   // 3. Currently recomputation is not supported with pipelining (TODO T9575)
-  if (ir.autoRecomputationEnabled() ||
-      ir.getMainGraph().hasUserRecomputeOps()) {
-    throw error(
-        "When pipelining is enabled, recomputation is currently not allowed");
+  if (ir.getMainGraph().hasUserRecomputeOps()) {
+    throw error("When pipelining is enabled, user annotation for recomputation "
+                "is not allowed");
   }
 
   // 4. Forward layers must be sharded with increasing IPU index
@@ -153,27 +157,32 @@ bool Pipeline::apply(Graph &graph) const {
   for (auto ipuCopyOp : ipuCopies) {
     uint64_t destIpu = ipuCopyOp->getDestIpu();
     // For an inference graph, or fwd pass of a training graph,
-    if (ipuCopyOp->toLoss == PathToLoss::Yes || !ir.canTrain()) {
+    if (!ir.canTrain() ||
+        ipuCopyOp->scheduledPreLoss == ScheduledPreLoss::Yes) {
       uint64_t maxSourceIpu = ipuCopyOp->getMaxSourceIpu();
       if (destIpu <= maxSourceIpu) {
-        throw error("For pipelining, forward IpuCopyOps must copy to an "
-                    "IPU of larger index. However {} copies from {} to {}.",
-                    ipuCopyOp->debugName(),
-                    maxSourceIpu,
-                    destIpu);
+        throw error(
+            "For pipelining, forward IpuCopyOps must copy to an "
+            "IPU of larger index. However {} ({}) copies from {} to {}.",
+            ipuCopyOp->debugName(),
+            ipuCopyOp->str(),
+            maxSourceIpu,
+            destIpu);
       }
     }
 
     // for bwd pass, IPU Copy must have destination with lower VGraphId than
     // source
-    else {
+    else if (ipuCopyOp->scheduledPreLoss == ScheduledPreLoss::No) {
       uint64_t minSourceIpu = ipuCopyOp->getMinSourceIpu();
       if (destIpu >= minSourceIpu) {
-        throw error("For pipelining, backward IpuCopyOps must copy to an "
-                    "IPU of smaller index. However {} copies from {} to {}.",
-                    ipuCopyOp->debugName(),
-                    minSourceIpu,
-                    destIpu);
+        throw error(
+            "For pipelining, backward IpuCopyOps must copy to an "
+            "IPU of smaller index. However {} ({}) copies from {} to {}.",
+            ipuCopyOp->debugName(),
+            ipuCopyOp->str(),
+            minSourceIpu,
+            destIpu);
       }
     }
   }
@@ -194,6 +203,8 @@ bool Pipeline::apply(Graph &graph) const {
   }
 
   // Now apply the transform
+
+  // TODO T10373 : support copies from multiple sources
 
   // 0. Contiguate the IPUCopies
   ContiguateIpuCopyIndicesPattern contiguator;
@@ -236,6 +247,8 @@ bool Pipeline::apply(Graph &graph) const {
     throw error(
         "ILE: no Op with PathFromLoss::Yes, yet canTrain() is true, bailing");
   }
+  logging::transform::debug("First PathFromLoss::Yes in schedule is {}.",
+                            (*firstFromLoss)->str());
 
   // 1. Find all tensors in the fwd pass that are inputs to ops in the bwd pass
   std::vector<TensorId> toStashTensors;

tests/popart/virtual_graph_test.py
@@ -284,8 +284,9 @@ def test_streaming_optimizer_tensors():
             losses[0].virtualGraph(1)
 
         optimizer = popart.ConstSGD(0.01)
-        optimizer = popart.SGD(
-            learning_rate=1.0, weight_decay=0.0, loss_scaling=1.0)
+        optimizer = popart.SGD(learning_rate=1.0,
+                               weight_decay=0.0,
+                               loss_scaling=1.0)
 
         opts = popart.SessionOptionsCore()
         opts.enableVirtualGraphs = useVirtualGraphs

commit 0d0150663f2ba96f0fc738ee0df4f86ba3e2fd20

Fixes for SGD with non-pipelined and pipelined models
Summary:
This diff does the following:
- Edit virtual_graph_test.py::test_streaming_optimizer_tensors to catch the failing case of N>2 IPUs
- In devicex.cpp look for whether the IpuCopyOp copies the streamed optimizer tensors, AND ALSO their copies, now fixing the above test.
- Now that IpuCopies of optimizer tensors are run in a fragment outside of the pipelined program, we can change the pipelining transform to
    - not stash optimizer tensors
    - not check whether optimizer tensors are copied to contiguous IPUs
- This change means pipelined models now work with SGD (broken since `f97ebaed87990e1c6857227dfae082d45b436368`*)

*Commit `f97ebaed87990e1c6857227dfae082d45b436368` broke pipelining models that use SGD. This is because it caused the `scheduledPreLoss` attribute of the optimizer tensors in the SGD model to change between the InterIpuCopy transform and the Pipeline transform. This is a valid change. However, pipelined models with SGD failed the check that IpuCopies in the fwd pass go in ascending order.

Test Plan:
`./test.sh popart -j8` passes all tests on os/x

Also now passing:
```
cd public_examples
git checkout T-pipeline-command-line-option
pytest /Users/evang/dev/public_examples/tests/onnx/test_image_classification.py
```

Reviewers: jamesn, george, simonl, matthewha

Reviewed By: jamesn

Differential Revision: https://phabricator.sourcevertex.net/D13444

python/popart.cpp
@@ -948,6 +948,9 @@ PYBIND11_MODULE(popart_core, m) {
   m.def("reservedAccumulationResetPrefix", &reservedAccumulationResetPrefix);
   m.def("reservedStashedPrefix", &reservedStashedPrefix);
   m.def("reservedRestoredPrefix", &reservedRestoredPrefix);
+  m.def("reservedLearnRatePrefix", &reservedLearnRatePrefix);
+  m.def("reservedWeightDecayPrefix", &reservedWeightDecayPrefix);
+  m.def("reservedLossScalingPrefix", &reservedLossScalingPrefix);
 
   // Exceptions are processed explicitly to allow the main dynamic library
   // to do the type inference.  This prevents some inter dynamic library type

willow/include/popart/op.hpp
@@ -262,6 +262,10 @@ public:
 
   virtual bool isIpuCopyOp() const;
 
+  // Returns true for Ops that copy only optimizer tensors
+  // from one IPU to another
+  virtual bool copiesOptimizerTensors() const;
+
   // helper functions, access fields of input and output
   Tensor *inTensor(InIndex index);
   const Tensor *inTensor(InIndex index) const;

willow/include/popart/op/ipucopy.hpp
@@ -32,6 +32,8 @@ public:
 
   bool isIpuCopyOp() const final;
 
+  bool copiesOptimizerTensors() const final;
+
   void connectInTensor(InIndex, TensorId, uint64_t sourceIpu);
 
   // A string of the form "[ sourceIpus ] --> [ destIpu ]"

willow/include/popart/tensor.hpp
@@ -117,6 +117,9 @@ public:
   void setProducer(Op *);
   void resetProducer(Op *);
   bool hasProducer() const;
+  // Returns true for stream tensors that are optimizer tensors, as
+  // well as their copies
+  bool isOptimizerTensor() const;
   bool hasTensorData() const;
   TensorData *tensorData();
   const TensorData *tensorData() const;

willow/include/popart/tensornames.hpp
@@ -37,6 +37,11 @@ constexpr const char *reservedAccumulationResetPrefix() {
 constexpr const char *reservedStashedPrefix() { return "Stashed___"; }
 constexpr const char *reservedRestoredPrefix() { return "Restored___"; }
 
+constexpr const char *reservedLearnRatePrefix() { return "scaledLearnRate_"; }
+constexpr const char *reservedWeightDecayPrefix() { return "weightDecay_"; }
+constexpr const char *reservedLossScalingPrefix() { return "lossScaling_"; }
+
+std::vector<std::string> reservedOptimizerPrefixes();
 std::vector<std::string> reservedPrefixes();
 
 } // namespace popart

willow/src/op.cpp
@@ -82,6 +82,7 @@ view::RegMap Op::bwdRegMap(InIndex i) const {
 
 bool Op::isLossOp() const { return false; }
 bool Op::isIpuCopyOp() const { return false; }
+bool Op::copiesOptimizerTensors() const { return false; }
 
 Op::~Op() = default;
 

willow/src/op/ipucopy.cpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <popart/graph.hpp>
 #include <popart/op/ipucopy.hpp>
 #include <popart/opmanager.hpp>
 #include <popart/opserialiser.hpp>
@@ -89,6 +90,24 @@ void IpuCopyOp::appendAttributes(OpSerialiserBase &os) const {
 }
 
 bool IpuCopyOp::isIpuCopyOp() const { return true; }
+
+bool IpuCopyOp::copiesOptimizerTensors() const {
+  int optTensorCount = 0;
+  for (auto tid_vgraphid : getSourceIpus()) {
+    Tensor *t = getGraph().getTensors().get(tid_vgraphid.first);
+    if (t->isOptimizerTensor()) {
+      optTensorCount++;
+    }
+  }
+
+  // Only return true if all tensors being copied
+  // by op are optimizer tensors
+  if (optTensorCount == getSourceIpus().size()) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 void IpuCopyOp::connectInTensor(InIndex inIndex,
                                 TensorId tenId,

willow/src/optimizer.cpp
@@ -13,13 +13,13 @@ Optimizer::Optimizer()                  = default;
 Optimizer::Optimizer(const Optimizer &) = default;
 
 TensorId getScaledLearningRateId(DataType dtype) {
-  return "scaledLearnRate_" + getDataTypeInfoMap().at(dtype).name();
+  return reservedLearnRatePrefix() + getDataTypeInfoMap().at(dtype).name();
 }
 TensorId getWeightDecayId(DataType dtype) {
-  return "weightDecay_" + getDataTypeInfoMap().at(dtype).name();
+  return reservedWeightDecayPrefix() + getDataTypeInfoMap().at(dtype).name();
 }
 TensorId getLossScalingId(DataType dtype) {
-  return "lossScaling_" + getDataTypeInfoMap().at(dtype).name();
+  return reservedLossScalingPrefix() + getDataTypeInfoMap().at(dtype).name();
 }
 
 // convert a float to type T

willow/src/popx/devicex.cpp
@@ -1335,22 +1335,7 @@ PriTask Devicex::opTask(Op *op, double priority, TaskId prevOpTaskId) {
     }
     // else if this Op is in the main scope
     else {
-      auto isOptimizerTensorCopy = [&]() {
-        if (!op->isConvertibleTo<IpuCopyOp>()) {
-          return false;
-        }
-
-        auto optimizerTensors = ir().optimizerTensors();
-        for (auto input : op->input->tensors()) {
-          if (boost::find(optimizerTensors, input) == optimizerTensors.end()) {
-            return false;
-          }
-        }
-
-        return true;
-      };
-
-      if (isOptimizerTensorCopy()) {
+      if (op->copiesOptimizerTensors()) {
         growOpx(progs.streamOptimizerFromHostFragment());
       }
       // pre-loss : create vertices for all recompute types

willow/src/tensor.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cstring>
+#include <string>
 
 #include <popart/error.hpp>
 #include <popart/onnxutil.hpp>
@@ -8,6 +9,7 @@
 #include <popart/tensor.hpp>
 #include <popart/tensordata.hpp>
 #include <popart/tensorindex.hpp>
+#include <popart/tensornames.hpp>
 #include <popart/util.hpp>
 
 namespace popart {
@@ -214,6 +216,16 @@ Op *Tensor::getProducer() const {
 Op *Tensor::getProducerUnsafe() const { return producer; }
 
 bool Tensor::hasProducer() const { return producer != nullptr; }
+
+bool Tensor::isOptimizerTensor() const {
+  for (auto optPref : reservedOptimizerPrefixes()) {
+    std::size_t found = id.find(optPref);
+    if (found != std::string::npos) {
+      return true;
+    }
+  }
+  return false;
+}
 
 void Consumers::increment(Op *op) {
   auto found = consumers_m.find(op);

willow/src/tensornames.cpp
@@ -25,14 +25,25 @@ TensorId getEdgeGradId(TensorId tenId, OpId opId, int index) {
   return edgeGradId;
 }
 
+std::vector<std::string> reservedOptimizerPrefixes() {
+  return {reservedLearnRatePrefix(),
+          reservedWeightDecayPrefix(),
+          reservedLossScalingPrefix()};
+}
+
 std::vector<std::string> reservedPrefixes() {
-  return {reservedGradientPrefix(),
-          reservedUpdatedVarPrefix(),
-          reservedAccumulationPrefix(),
-          reservedAccumulationOutPrefix(),
-          reservedAccumulationResetPrefix(),
-          reservedStashedPrefix(),
-          reservedRestoredPrefix()};
+  std::vector<std::string> prefs = {reservedGradientPrefix(),
+                                    reservedUpdatedVarPrefix(),
+                                    reservedAccumulationPrefix(),
+                                    reservedAccumulationOutPrefix(),
+                                    reservedAccumulationResetPrefix(),
+                                    reservedStashedPrefix(),
+                                    reservedRestoredPrefix()};
+
+  std::vector<std::string> optPrefs = reservedOptimizerPrefixes();
+  prefs.insert(prefs.end(), optPrefs.begin(), optPrefs.end());
+
+  return prefs;
 }
 
 } // namespace popart

willow/src/transforms/interipucopy.cpp
@@ -252,15 +252,13 @@ bool InterIpuCopy::apply(Graph &graph) const {
       if (graph.getIr().getSessionOptions().enablePipelining) {
         // We add an additional constraint on the order of IPU copies here:
         //
-        // 'All stream tensors are to be copied to a larger IPU number
-        //  than that of sourceOp, except for optimizer stream tensors,
-        //  which are to be copied to a smaller IPU number than that of
-        //  sourceOp'
+        // 'All stream tensors scheduled pre-loss are to be copied to a
+        //  larger IPU number than that of sourceOp.
+        //  All stream tensors scheduled post-loss are to be copied to a
+        //  smaller IPU number than that of sourceOp'
         //
-        auto optTensors = graph.getIr().optimizerTensors();
-        if (std::find(optTensors.begin(),
-                      optTensors.end(),
-                      graph.getTensors().get(streamId)) == optTensors.end()) {
+        if (graph.getTensors().get(s.first)->scheduledPreLoss ==
+            ScheduledPreLoss::Yes) {
           // sourceOp should be op mapped to smallest vGraphId
           for (Op *op : consumers) {
             if (op->getVirtualGraphId() < sourceOp->getVirtualGraphId()) {

willow/src/transforms/pipeline.cpp
@@ -159,6 +159,13 @@ bool Pipeline::apply(Graph &graph) const {
 
   auto ipuCopies = getIpuCopyOps();
   for (auto ipuCopyOp : ipuCopies) {
+    // Ignore copies of optimizer tensors. These are run inside a different
+    // program fragment to the pipelined program, so don't have to follow
+    // the same constraints of other IpuCopy ops.
+    if (ipuCopyOp->copiesOptimizerTensors()) {
+      continue;
+    }
+
     uint64_t destIpu = ipuCopyOp->getDestIpu();
     // For an inference graph, or fwd pass of a training graph,
     if (!ir.canTrain() ||
@@ -260,11 +267,15 @@ bool Pipeline::apply(Graph &graph) const {
     // Not a candidate for stashing if the tensor:
     // - has no consumers
     // - is a variable tensor
+    // - is an optimizer tensor
     // - is on the final IPU
     if (tensor->consumers.getOps().empty()) {
       continue;
     }
     if (tensor->tensorType() == TensorType::Variable) {
+      continue;
+    }
+    if (tensor->isOptimizerTensor()) {
       continue;
     }
 

tests/popart/virtual_graph_test.py
@@ -244,44 +244,53 @@ def test_virtual_graph_bad_index():
 
 
 def test_streaming_optimizer_tensors():
-    input_data = np.random.rand(2, 2).astype(np.float32)
+    bps = 5
+    input_data = np.random.rand(bps, 2, 2).astype(np.float32)
     weight_data_0 = np.random.rand(2, 2).astype(np.float32)
     weight_data_1 = np.random.rand(2, 2).astype(np.float32)
+    weight_data_2 = np.random.rand(2, 2).astype(np.float32)
 
-    def run_test(useVirtualGraphs):
+    def run_test(enablePipelining):
         popart.getLogger().setLevel("TRACE")
 
         builder = popart.Builder()
 
         i1 = builder.addInputTensor(
-            popart.TensorInfo("FLOAT", input_data.shape))
-        w1 = builder.addInitializedInputTensor(weight_data_0)
-        w2 = builder.addInitializedInputTensor(weight_data_1)
+            popart.TensorInfo("FLOAT", input_data.shape[1::]))
+        w0 = builder.addInitializedInputTensor(weight_data_0)
+        w1 = builder.addInitializedInputTensor(weight_data_1)
+        w2 = builder.addInitializedInputTensor(weight_data_2)
 
-        # with builder.virtualGraph(0):
-        o1 = builder.aiOnnx.matmul([i1, w1])
-        if useVirtualGraphs:
-            builder.virtualGraph(o1, 0)
+        o0 = builder.aiOnnx.matmul([i1, w0])
+        if enablePipelining:
+            builder.virtualGraph(o0, 0)
 
-        # with builder.virtualGraph(1):
+        o1 = builder.aiOnnx.matmul([o0, w1])
+        if enablePipelining:
+            builder.virtualGraph(o1, 1)
+
         o2 = builder.aiOnnx.matmul([o1, w2])
-        if useVirtualGraphs:
-            builder.virtualGraph(o2, 1)
+        if enablePipelining:
+            builder.virtualGraph(o2, 2)
 
         o = o2
         builder.addOutputTensor(o)
 
         proto = builder.getModelProto()
 
+        if enablePipelining:
+            anchorId = popart.reservedLearnRatePrefix() + "FLOAT_c0"
+        else:
+            anchorId = popart.reservedLearnRatePrefix() + "FLOAT"
+
         # Need to anchor the output of the backward pass to stop it being pruned
-        dataFlow = popart.DataFlow(1, {
-            o: popart.AnchorReturnType("ALL"),
-        })
+        dataFlow = popart.DataFlow(bps,
+                                   {anchorId: popart.AnchorReturnType("ALL")})
 
         losses = [popart.L1Loss(o, "l1LossVal", 0.1)]
-        #Make sure that the loss is also assigned to a virtual graph
-        if useVirtualGraphs:
-            losses[0].virtualGraph(1)
+        # Make sure that the loss is also assigned to a virtual graph
+        if enablePipelining:
+            losses[0].virtualGraph(2)
 
         optimizer = popart.ConstSGD(0.01)
         optimizer = popart.SGD(learning_rate=1.0,
@@ -289,11 +298,12 @@ def test_streaming_optimizer_tensors():
                                loss_scaling=1.0)
 
         opts = popart.SessionOptionsCore()
-        opts.enableVirtualGraphs = useVirtualGraphs
+        opts.enableVirtualGraphs = enablePipelining
+        opts.enablePipelining = enablePipelining
 
         numIPUs = 1
-        if useVirtualGraphs:
-            numIPUs = 4
+        if enablePipelining:
+            numIPUs = 3
 
         session = popart.TrainingSession(
             fnModel=proto,
@@ -313,19 +323,17 @@ def test_streaming_optimizer_tensors():
         session.weightsFromHost()
         session.optimizerFromHost()
 
-        # run 6 steps, changing the optimizer halfway through
+        # run 2 steps, changing the optimizer halfway through
         result = []
-        for i in range(3):
-            session.run(stepio)
-            result.append(np.copy(anchors[o]))
+        session.run(stepio)
+        result.append(np.copy(anchors[anchorId]))
 
         session.updateOptimizer(
             popart.SGD(learning_rate=0.5, weight_decay=0.0, loss_scaling=1.0))
         session.optimizerFromHost()
 
-        for i in range(3):
-            session.run(stepio)
-            result.append(np.copy(anchors[o]))
+        session.run(stepio)
+        result.append(np.copy(anchors[anchorId]))
 
         return result
 