8e6bb6b6571efd9c4d2e63500e4d2e1175de74c0
James Newling <jamesn@graphcore.ai>
2019-10-28 13:30:13 +0000

    Move char vector up a scope level to keep it live til after used
    Test Plan: Bert model reaches engine compilation (Ubuntu and OS/X)
    
    Reviewers: oskarb
    
    Reviewed By: oskarb
    
    Differential Revision: https://phabricator.sourcevertex.net/D16656

--- willow/src/patterns/sgd1decompose.cpp
+++ willow/src/patterns/sgd1decompose.cpp
@@ -41,7 +41,9 @@ void addAcclInTensor(SGD1VarUpdateComboOp &comboOp,
   // having data, as it is the slice of the original weight data. We can try and
   // back-track to find the data. TODO T12031 move the below logic to tensor
   // class and generalize.
+  std::vector<char> sliceOutTemp;
   if (!weight.hasTensorData()) {
+
     constexpr const char *infoString =
         "In addAcclInTensor, trying to get the weight's data to initialize the "
         "accumulation tensor with required weight decay term. ";
@@ -57,9 +59,11 @@ void addAcclInTensor(SGD1VarUpdateComboOp &comboOp,
                     "work back through, it is a " +
                     weight.getProducer()->str());
       }
+
       ConstExprSlice ceSlice(asSlice);
-      auto out   = ceSlice.compute();
-      weightVal0 = static_cast<const T *>(static_cast<void *>(out.data()));
+      sliceOutTemp = ceSlice.compute();
+      weightVal0 =
+          static_cast<const T *>(static_cast<void *>(sliceOutTemp.data()));
     }
   } else {
     weightVal0 = static_cast<const T *>(weight.tensorData()->data());
