e05a3e06e22cab5bd4f96903eb4660d6b9950a9b
Matthew Haddock <matthewha@graphcore.ai>
2019-11-04 10:49:51 +0000

    Add shapeinference to cube custom op.
    Summary: Initial attempt at shape inference
    
    Test Plan:
    This example passes.
    
    @alexandrosk has also independently used the custom op feature with shape inference.
    
    Reviewers: simonl, jamesn, george, evan
    
    Reviewed By: evan
    
    Subscribers: alexandrosk
    
    Maniphest Tasks: T11617
    
    Differential Revision: https://phabricator.sourcevertex.net/D15265

--- examples/custom_ops/custom_op.cpp
+++ examples/custom_ops/custom_op.cpp
@@ -6,7 +6,7 @@
 // This example if compiled into a .so shared object library, will be usable in
 // python.
 // To compile, cd into the custom_ops directory, then run:
-// `g++  -fPIC custom_op.cpp -shared -lpopart -o custom_op.so`
+// `cmake .` and `make`
 // This will create an .so file that can be used in the python file. Make sure
 // any virtualenvs and poplar are activated then run `python custom_op.py` and
 // you should see a printout of some inputs, weights and outputs.
@@ -14,11 +14,16 @@
 #include <memory>
 #include <popart/builder.hpp>
 #include <popart/devicemanager.hpp>
+
+// #include <popart/filereader.hpp>
+#include <popart/ir.hpp>
+
 #include <popart/logging.hpp>
 #include <popart/ndarraywrapper.hpp>
 #include <popart/op.hpp>
 #include <popart/op/l1.hpp>
 #include <popart/opmanager.hpp>
+#include <popart/opserialiser.hpp>
 #include <popart/optimizer.hpp>
 #include <popart/patterns/pattern.hpp>
 #include <popart/popx/opx.hpp>
@@ -28,25 +33,36 @@
 #include <popart/tensorinfo.hpp>
 #include <popart/tensornames.hpp>
 
+#include <poprand/RandomGen.hpp>
+
 #include <popops/ElementWise.hpp>
 
+#include <popart/names.hpp>
+#include <popart/opidentifier.hpp>
+
+#include <onnx/defs/schema.h>
+#include <onnx/defs/shape_inference.h>
+
 namespace {
+
 // for C++11 compatibility, we don't use std::make_unique
 template <typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args &&... args) {
   return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
+
 } // namespace
 
 // Use extern to avoid mangled names when importing to python
 extern "C" {
 
 namespace Onnx {
+
 namespace CustomOperators {
-const popart::OperatorIdentifier Cube = {"ai.acme", "Cube", 1};
+const popart::OperatorIdentifier Cube = {"com.acme", "Cube", 1};
 } // namespace CustomOperators
 namespace CustomGradOperators {
-const popart::OperatorIdentifier CubeGrad = {"ai.acme", "CubeGrad", 1};
+const static popart::OperatorIdentifier CubeGrad = {"com.acme", "CubeGrad", 1};
 } // namespace CustomGradOperators
 } // namespace Onnx
 
@@ -62,11 +78,69 @@ const popart::OperatorIdentifier CubeGrad = {"ai.acme", "CubeGrad", 1};
 //
 // We do training in this example, so the four classes implemented are:
 //
-
 class CubeOp;
 class CubeGradOp;
 class CubeOpx;
 class CubeGradOpx;
+
+// The forward Op
+
+class CubeOp : public popart::Op {
+
+public:
+  CubeOp(const popart::OperatorIdentifier &_opid,
+         const popart::Op::Settings &settings_)
+      : popart::Op(_opid, settings_) {}
+
+  // Configure the output popart Tensor
+  virtual void setup() { outInfo(0) = inInfo(0); }
+
+  std::unique_ptr<Op> clone() const final { return make_unique<CubeOp>(*this); }
+
+  // An estimate of how valuable sub-graph matching will be
+  float getSubgraphValue() const final { return getLowSubgraphValue(); }
+};
+
+static std::unique_ptr<popart::Op>
+cubeOpFactory(const popart::OperatorIdentifier &_opid,
+              const popart::Op::Settings &settings,
+              const popart::Attributes &attr) {
+
+  return make_unique<CubeOp>(_opid, settings);
+}
+
+static popart::OpCreator<CubeOp>
+    cubeOpCreator({Onnx::CustomOperators::Cube}, cubeOpFactory, true);
+
+// The forward Opx (poplar implementation of the forward Op)
+
+class CubeOpx : public popart::popx::Opx {
+
+public:
+  CubeOpx(popart::Op *op, popart::popx::Devicex *devicex)
+      : popart::popx::Opx(op, devicex) {
+
+    // Not strictly necessary, we check that op is castable to a CubeOp *.
+    verifyOp<CubeOp>(op, Onnx::CustomOperators::Cube);
+  }
+
+  void grow(poplar::program::Sequence &prog) const {
+
+    auto &op = getOp<CubeOp>();
+
+    auto input = getInTensor(0);
+
+    auto output = popops::map(
+        graph(),
+        popops::expr::Mul(popops::expr::Mul(popops::expr::_1, popops::expr::_1),
+                          popops::expr::_1),
+        {getInTensor(0)},
+        prog,
+        debugPrefix());
+
+    setOutTensor(0, output);
+  }
+};
 
 // The gradient Op
 class CubeGradOp : public popart::Op {
@@ -89,23 +163,23 @@ public:
   // the 0'th output Tensor of the CubeOp.
   // Supposing the CubeOp has input Tensor T0 and output Tensor T1,
   //
-  //   input at index 0 (T0)
-  //          |
-  //        CubeOp
-  //          |
-  //   output at index 0 (T1)
+  //  input at index 0 (T0)
+  //         |
+  //       CubeOp
+  //         |
+  //  output at index 0 (T1)
   //
   // Then the picture described by the map below looks like,
   //
   //
-  //    input at index 0 (gradient of T1)
-  //         |   input at index 1 (T1)
-  //         |     |
-  //         |     |
-  //        CubeGradOp
-  //            |
-  //            |
-  //   output at index 0 (gradient of T0)
+  //   input at index 0 (gradient of T1)
+  //        |   input at index 1 (T1)
+  //        |     |
+  //        |     |
+  //       CubeGradOp
+  //           |
+  //           |
+  //  output at index 0 (gradient of T0)
   //
   virtual const std::vector<popart::GradInOutMapper> &gradInputInfo() const {
     static const std::vector<popart::GradInOutMapper> inInfo = {
@@ -123,59 +197,6 @@ public:
 
   // an estimate of how valuable sub-graph matching will be
   float getSubgraphValue() const final { return getLowSubgraphValue(); }
-};
-
-// The forward Op
-class CubeOp : public popart::Op {
-public:
-  CubeOp(const popart::OperatorIdentifier &_opid,
-         const popart::Op::Settings &settings_)
-      : popart::Op(_opid, settings_) {}
-
-  // The output popart Tensor has the same inputInfo and numerical type
-  // (i.e. the same TensorInfo) as the input Tensor. This function is
-  // required for inputInfo/type inference
-  //
-  virtual void setup() { outInfo(0) = inInfo(0); }
-
-  std::unique_ptr<Op> clone() const final { return make_unique<CubeOp>(*this); }
-
-  // There is only one Gradient Op for CubeOp, a CubeGradOp
-  // It is possible to have multiple Gradient Ops
-  // (Conv has 2 in popart, one for weights and one for activations)
-  //
-  std::vector<std::unique_ptr<popart::Op>> getGradOps() {
-    std::vector<std::unique_ptr<Op>> upops;
-    upops.emplace_back(new CubeGradOp(*this));
-    return upops;
-  }
-
-  // an estimate of how valuable sub-graph matching will be
-  float getSubgraphValue() const final { return getLowSubgraphValue(); }
-};
-
-static popart::OpCreator<CubeOp> cubeOpCreator(Onnx::CustomOperators::Cube);
-
-// forward Opx (poplar implementation of the forward Op)
-class CubeOpx : public popart::popx::Opx {
-public:
-  CubeOpx(popart::Op *op, popart::popx::Devicex *devicex)
-      : popart::popx::Opx(op, devicex) {
-    // not strictly necessary, we check that op is castable to a CubeOp *.
-    verifyOp<CubeOp>(op, Onnx::CustomOperators::Cube);
-  }
-
-  void grow(poplar::program::Sequence &prog) const final {
-    // Cube the input. We create a poplar::Tensor of name outId(0)
-    insert(outId(0),
-           popops::map(graph(),
-                       popops::expr::Mul(popops::expr::Mul(popops::expr::_1,
-                                                           popops::expr::_1),
-                                         popops::expr::_1),
-                       {get(inId(0))},
-                       prog,
-                       debugPrefix()));
-  }
 };
 
 class CubeGradOpx : public popart::popx::Opx {
@@ -197,7 +218,7 @@ public:
                         popops::expr::Mul(popops::expr::Mul(popops::expr::_1,
                                                             popops::expr::_1),
                                           popops::expr::_2)),
-                    {get(inId(0)), get(inId(1))}, // FwdOut, GradOut
+                    {getInTensor(0), getInTensor(1)}, // FwdOut, GradOut
                     prog,
                     debugPrefix()));
   }
@@ -208,3 +229,42 @@ static popart::popx::OpxCreator<CubeOpx>
 static popart::popx::OpxCreator<CubeGradOpx>
     cubeGradOpxCreator(Onnx::CustomGradOperators::CubeGrad);
 }
+namespace ONNX_NAMESPACE {
+
+void CubeShapeInference(InferenceContext &ctx) {
+  propagateShapeAndTypeFromFirstInput(ctx);
+}
+
+static const char CubeDoc[] = "Cube cubes (x^3) each element of the tensor.";
+
+ONNX_OPERATOR_SET_SCHEMA_EX(
+    Cube,
+    comAcme,
+    "com.acme",
+    1,
+    false,
+    OpSchema()
+        .SetDoc(CubeDoc)
+        .Input(0, "X", "Input tensor", "T")
+        .Output(0, "Y", "Output tensor", "T")
+        .TypeConstraint(
+            "T",
+            {"tensor(float)", "tensor(int32)", "tensor(float16)"},
+            "Constrain input and output types to signed numeric tensors.")
+        .TypeAndShapeInferenceFunction(CubeShapeInference));
+
+static bool registerOps() {
+  auto &d = ONNX_NAMESPACE::OpSchemaRegistry::DomainToVersionRange::Instance();
+  d.AddDomainToVersion("com.acme", 1, 1);
+
+  ONNX_NAMESPACE::RegisterSchema(
+      GetOpSchema<ONNX_OPERATOR_SET_SCHEMA_CLASS_NAME(comAcme, 1, Cube)>());
+
+  return true;
+}
+
+static bool ret = registerOps();
+
+} // namespace ONNX_NAMESPACE
+
+int main() { return 0; }
