283aa84ee2ee6a2ecb55cc96c8994dd51b84c3a2
Matthew Haddock <matthewha@graphcore.ai>
2020-01-08 15:25:09 +0000

    Fix segfault before  prepareDevice has been called
    Summary:
    The simplest solutions are the best.
    
    Not sure about the error message, suggestions welcome.
    
    Test Plan: Passes all and new test.
    
    Reviewers: evan, george, jamesn
    
    Reviewed By: george
    
    Maniphest Tasks: T14589
    
    Differential Revision: https://phabricator.sourcevertex.net/D19480

--- tests/popart/export_test.py
+++ tests/popart/export_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import popart
 import test_util as tu
+import pytest
 
 # model:
 #
@@ -69,3 +70,45 @@ def test_constants_preserved():
     # confirm that the output is correct. See T6186, which this tests
     assert (np.sum(np.abs(anchorArrays[o2] - np.array([[7, 8], [9, 10]]))) <
             1e-8)
+
+
+def test_no_prepare_device():
+    popart.getLogger().setLevel("TRACE")
+
+    # Check that `session.modelToHost` can be called when using a
+    # model with a constant node, without throwing an exceptions
+    builder = popart.Builder()
+
+    i1 = builder.addInputTensor(popart.TensorInfo("FLOAT", [2, 2]))
+    i2 = builder.addInputTensor(popart.TensorInfo("FLOAT", [2, 2]))
+    c = builder.aiOnnx.constant(np.array([[1, 2], [3, 4]], dtype=np.float32))
+    o1 = builder.aiOnnx.add([i1, i2])
+    o2 = builder.aiOnnx.add([o1, c])
+    builder.addOutputTensor(o2)
+
+    proto = builder.getModelProto()
+
+    anchors = {o2: popart.AnchorReturnType("ALL")}
+
+    dataFlow = popart.DataFlow(1, anchors)
+
+    optimizer = popart.ConstSGD(0.01)
+
+    losses = [popart.L1Loss(o2, "l1LossVal", 0.1)]
+
+    opts = popart.SessionOptions()
+
+    session = popart.TrainingSession(fnModel=proto,
+                                     dataFeed=dataFlow,
+                                     userOptions=opts,
+                                     losses=losses,
+                                     optimizer=optimizer,
+                                     deviceInfo=tu.get_poplar_cpu_device())
+
+    # No session.prepareDevice()
+
+    with pytest.raises(popart.popart_exception) as e_info:
+        session.modelToHost('session_proto.onnx')
+
+    assert (e_info.value.args[0].startswith(
+        "session.prepareDevice() must be called before trying to save"))
