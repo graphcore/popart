graphcore/popart

commit a733e6977af235bc903a8def63c2316cd8e72f74

Add option to choose IPU version with IPUModel.
Summary:
Add the option. Change defaults so they are default for
the model selcted.

Reviewers: #popart, jamesn, matthewha

Reviewed By: #popart, jamesn, matthewha

Subscribers: matthewha

Differential Revision: https://phabricator.sourcevertex.net/D20787

willow/src/popx/devicexmanager.cpp
@@ -116,6 +116,18 @@ bool mapFind<bool>(const std::map<std::string, std::string> &map,
   }
 }
 
+template <>
+std::string mapFind<std::string>(const std::map<std::string, std::string> &map,
+                                 const char *key,
+                                 std::string defaultValue) {
+  auto it = map.find(key);
+  if (it != map.end()) {
+    return it->second;
+  } else {
+    return defaultValue;
+  }
+}
+
 std::shared_ptr<popart::DeviceInfo> DevicexManager::createHostDevice(
     popart::DeviceType type,
     const std::map<std::string, std::string> &options) {
@@ -141,14 +153,18 @@ std::shared_ptr<popart::DeviceInfo> DevicexManager::createHostDevice(
     return std::make_shared<DevicexCpuInfo>(*this, device);
   }
   case DeviceType::IpuModel: {
-    checkOptions({"numIPUs", "tilesPerIPU", "compileIPUCode"});
+    checkOptions({"numIPUs", "tilesPerIPU", "compileIPUCode", "ipuVersion"});
 
     // Create an ipumodel, using the values set in the options map, else use the
     // defaults
-    poplar::IPUModel ipuModel;
-    ipuModel.numIPUs        = mapFind(options, "numIPUs", 1);
-    ipuModel.tilesPerIPU    = mapFind(options, "tilesPerIPU", 1216);
-    ipuModel.compileIPUCode = mapFind(options, "compileIPUCode", true);
+    const std::string ipuVersion =
+        mapFind<std::string>(options, "ipuVersion", "ipu1");
+    poplar::IPUModel ipuModel(ipuVersion.c_str());
+    ipuModel.numIPUs = mapFind(options, "numIPUs", int(ipuModel.numIPUs));
+    ipuModel.tilesPerIPU =
+        mapFind(options, "tilesPerIPU", int(ipuModel.tilesPerIPU));
+    ipuModel.compileIPUCode =
+        mapFind(options, "compileIPUCode", ipuModel.compileIPUCode);
 
     poplar::Device device = ipuModel.createDevice();
     return std::make_shared<DevicexIpuModelInfo>(*this, device);

commit 92bc2168f807daaa4213acb62c767c4e434666b4

Fix memory_regression_test
Summary:
Revision a733e6977af2 introduced a test failure due to change in default
for compileIPUCode (must be true but models default to false).

Reviewers: matthewha, #popart, jamesn

Reviewed By: #popart, jamesn

Differential Revision: https://phabricator.sourcevertex.net/D20808

willow/src/popx/devicexmanager.cpp
@@ -163,8 +163,7 @@ std::shared_ptr<popart::DeviceInfo> DevicexManager::createHostDevice(
     ipuModel.numIPUs = mapFind(options, "numIPUs", int(ipuModel.numIPUs));
     ipuModel.tilesPerIPU =
         mapFind(options, "tilesPerIPU", int(ipuModel.tilesPerIPU));
-    ipuModel.compileIPUCode =
-        mapFind(options, "compileIPUCode", ipuModel.compileIPUCode);
+    ipuModel.compileIPUCode = mapFind(options, "compileIPUCode", true);
 
     poplar::Device device = ipuModel.createDevice();
     return std::make_shared<DevicexIpuModelInfo>(*this, device);