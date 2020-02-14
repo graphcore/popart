1d748db25ca71995634f3a8ae979f90c567d0545
Matthew Haddock <matthewha@graphcore.ai>
2020-02-14 17:49:36 +0000

    Add type hints, docstrings and tidy front end.
    Summary:
    A bunch of tidying documentation and type hints for the python front end. No functionality changes.
    
    This makes it easier for the user to see expected types and info about the expected values for the different inputs, especially on a good IDE.
    
    It was Friday and I was on a lot of painkillers, so there may be a few spelling errors.
    
    Test Plan: All tests pass.
    
    Reviewers: #popart, jamesn
    
    Reviewed By: #popart, jamesn
    
    Subscribers: jamesn
    
    Maniphest Tasks: T16118
    
    Differential Revision: https://phabricator.sourcevertex.net/D21301

--- python/popart/builder.py
+++ python/popart/builder.py
@@ -1,26 +1,40 @@
-import popart
+from typing import Any, Dict, Iterable, List, Union
+
 import numpy as np
+
+import popart
 from popart_core import _BuilderCore
 
 
-class Builder(object):
+class Opset():
+    """Minimal base class for the opsets
+
+    Arguments:
+        builder {Builder} -- An interface for a Builder, used for creating ONNX graphs.
+        version {int} -- Opset version to use for the given opset sub-class.
+    """
+
+    def __init__(self, builder: "Builder", version: int) -> None:
+        self._builder = builder
+        self.version = version
+
+
+class Builder():
     """ A wrapper around the Builder cpp class, renamed BuilderCore in pybind,
     to enable more pythonic use. See builder.hpp for the class definition.
+
+    Arguments:
+        modelProtoOrFilename {str} -- Model protobuf string or file path of saved
+            onnx model proto. (default: {None})
+        opsets {dict} -- Dict of opset versions (default: {None})
+        builderCore {_BuilderCore} -- _BuilderCore object if wanting to create a subgraph
+            builder using an existing buildercore object. (default: {None})
     """
 
     def __init__(self,
-                 modelProtoOrFilename=None,
-                 opsets=None,
-                 builderCore=None):
-        """Class initializer
-
-        Arguments:
-            modelProtoOrFilename {str} -- Model protobuf string or file path of saved
-                onnx model proto. (default: {None})
-            opsets {dict} -- Dict of opset versions (default: {None})
-            builderCore {_BuilderCore} -- Buildercore object if wanting to create a subgraph
-                builder using an existing buildercore object. (default: {None})
-        """
+                 modelProtoOrFilename: Union[str, bytes] = None,
+                 opsets: Dict[str, int] = None,
+                 builderCore: _BuilderCore = None) -> None:
         if builderCore is None:
             if modelProtoOrFilename is None:
                 self._impl = _BuilderCore()
@@ -42,36 +56,41 @@ class Builder(object):
         self.aiGraphcoreOpset1 = AiGraphcoreOpset1(self,
                                                    self.opsets["ai.graphcore"])
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Any:
         """Reroute all attribute requests to the underlying _BuilderCore object
 
         Arguments:
-            name {str} -- attribute required.
+            name {str} --  attribute required.
 
         Returns:
-            various -- return of the builder._impl.attr call.
+            Any --  return of the builder._impl.attr call.
         """
         return getattr(self._impl, name)
 
-    def reshape_const(self, aiOnnx, args, shape, debugPrefix=""):
+    def reshape_const(self,
+                      aiOnnx: Opset,
+                      args: List[str],
+                      shape: Iterable[int],
+                      debugPrefix: str = "") -> List[int]:
         """Const version of the reshape op.
 
         Arguments:
             aiOnnx {Opset} -- versioned aiOnnx opset e.g. aiOnnxOpset11
-            args {list} -- List of tensor ids to feed as arguments.
-            shape {list} -- Shape to reshape to.
+            args {List[str]} --  List of tensor ids to feed as arguments.
+            shape {Iterable[int]} -- shape to rehape to e.g. [3, 2, 4]
 
         Keyword Arguments:
             debugPrefix {str} -- String to use as a debug prefix. (default: {""})
 
         Returns:
-            str -- Output tensor id.
+            List[int] -- Output tensor ids.
         """
+
         newShape = aiOnnx.constant(
             np.array(shape).astype(np.int64), debugPrefix + "_const")
         return aiOnnx.reshape([args[0], newShape], debugPrefix)
 
-    def createSubgraphBuilder(self):
+    def createSubgraphBuilder(self) -> 'Builder':
         """Create a child builder to add ops to a subgraph using a call operation.
 
         Returns:
@@ -81,14 +100,19 @@ class Builder(object):
         return Builder(builderCore=subBuilderCore)
 
 
-class Opset(object):
-    def __init__(self, builder, version):
-        self._builder = builder
-        self.version = version
-
-
 class AiOnnx(Opset):
-    def __init__(self, builder, version):
+    """Return the builder interface for the given ai.onnx version.
+
+    Arguments:
+        builder {Builder} -- parent class for access.
+        version {int} -- ai.Onnx opset version to use; 6 < version < 11
+            (default 10).
+
+    Raises:
+        ValueError: Error thrown if an invalid ai.Onnx opset version provided.
+    """
+
+    def __init__(self, builder: Builder, version: int) -> None:
         super(AiOnnx, self).__init__(builder, version)
         if self.version == 11:
             self.aiOnnx = self._builder.aiOnnxOpset11
@@ -103,50 +127,143 @@ class AiOnnx(Opset):
         elif self.version == 6:
             self.aiOnnx = self._builder.aiOnnxOpset6
         else:
-            # Need to raise an exception here
-            pass
+            raise ValueError(
+                f"Unsupported or unrecognized ai.Onnx version: {self.version}")
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Any:
+        """Reroute all attribute requests to the underlying _BuilderCore object
+
+        Arguments:
+            name {str} -- Attribute required.
+
+        Returns:
+            Any -- return of the builder._impl.attr call.
+        """
         return getattr(self.aiOnnx, name)
 
-    def logical_if(self, args, num_outputs, else_branch, then_branch, name=""):
+    def logical_if(self,
+                   args: List[str],
+                   num_outputs: int,
+                   else_branch: Builder,
+                   then_branch: Builder,
+                   name: str = "") -> List[str]:
+        """If conditional operation.
+
+        Arguments:
+            args {List[str]} -- List of tensor ids to feed as arguments.
+            num_outputs {int} -- Number of output tensors from the if operator.
+                else_branch {Builder} -- SubgraphBuilder for the graph to run if condition
+                is false. Has num_outputs outputs: values you wish to be live-out to the enclosing 
+                scope. The number of outputs must match the number of outputs in the then_branch.
+            then_branch {Builder} -- SubgraphBuilder for the graph to run if condition is true.
+                Has num_outputs outputs: values you wish to be live-out to the enclosing scope.
+                The number of outputs must match the number of outputs in the else_branch.
+
+        Keyword Arguments:
+            name {str} -- A string to prepend to the name of the tensor (default: {""})
+
+        Returns:
+            List[str] -- Output tensor ids.
+        """
         return self.aiOnnx.logical_if(args, num_outputs, else_branch._impl,
                                       then_branch._impl, name)
 
-    def loop(self, args, num_outputs, body, debugPrefix=""):
+    def loop(self,
+             args: List[str],
+             num_outputs: int,
+             body: Builder,
+             debugPrefix: str = "") -> List[str]:
+        """Generic Looping construct op.
+
+        Arguments:
+            args {List[str]} -- List of tensor ids to feed as arguments.
+            num_outputs {int} --  Number of output tensors from the loop operator.
+            body {Builder} -- SubgraphBuilder for the graph to run in the loop.
+
+        Keyword Arguments:
+            debugPrefix {str} -- A string to prepend to the name of the tensor (default: {""})
+
+        Returns:
+            List[str] -- Output tensor ids.
+        """
         return self.aiOnnx.loop(args, num_outputs, body._impl, debugPrefix)
 
 
 class AiOnnxMl(Opset):
-    def __init__(self, builder, version):
+    """Return the builder interface for the given ai.onnx.ml version.
+
+    Raises:
+        ValueError: Error thrown if an invalid ai.onnx.ml opset version provided.
+    """
+
+    def __init__(self, builder: Builder, version: int) -> None:
         super(AiOnnxMl, self).__init__(builder, version)
         if self.version == 1:
             self.aiOnnxMl = self._builder.aiOnnxMlOpset1
         else:
-            # Need to raise an exception here
-            pass
+            raise ValueError(
+                f"Unsupported or unrecognized ai.OnnxMl version: {self.version}"
+            )
 
-    def __getattr__(self, name):
+    def __getattr__(self, name) -> Any:
         return getattr(self.aiOnnxMl, name)
 
 
 class AiGraphcore(Opset):
-    def __init__(self, builder, version):
+    """Return the builder interface for the given ai.graphcore version.
+
+    Raises:
+        ValueError: Error thrown if an invalid ai.graphcore opset version provided.
+    """
+
+    def __init__(self, builder: Builder, version: int) -> None:
         super(AiGraphcore, self).__init__(builder, version)
         if self.version == 1:
             self.aiGraphcore = self._builder.aiGraphcoreOpset1
         else:
-            # Need to raise an exception here
-            pass
+            raise ValueError(
+                f"Unsupported or unrecognized ai.graphcore version: {self.version}"
+            )
 
-    def call(self, args, num_outputs, callee, debugName=""):
+    def call(self,
+             args: List[int],
+             num_outputs: int,
+             callee: Builder,
+             debugName: str = "") -> List[str]:
+        """Add a call operation to the model
+
+        This is a poplar extension, to expose manual code re-use to
+        the builder
+
+        Arguments:
+            args {List[int]} -- List of tensor ids to feed as arguments.
+            num_outputs {int} -- Number of output tensors from the called graph.
+            callee {Builder} -- SubgraphBuilder for the graph to be called.
+
+        Keyword Arguments:
+            debugName {str} -- A string to prepend to the name of the tensor (default: {""})
+
+        Returns:
+            List[str] -- Output tensor ids.
+        """
         return self.aiGraphcore.call(args, num_outputs, callee._impl,
                                      debugName)
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Any:
+        """Reroute all attribute requests to the underlying _BuilderCore object
+
+        Arguments:
+            name {str} --  attribute required.
+
+        Returns:
+            Any --  return of the builder._impl.attr call.
+        """
         return getattr(self.aiGraphcore, name)
 
 
 class AiGraphcoreOpset1(AiGraphcore):
-    def __init__(self, builder, version):
+    """Sub-class for backwards compatibility. Will forward all calls to AiGraphcore class.
+    """
+
+    def __init__(self, builder: Builder, version: int) -> None:
         super(AiGraphcoreOpset1, self).__init__(builder, version)
--- python/popart/session.py
+++ python/popart/session.py
@@ -1,21 +1,32 @@
-import popart
+from typing import Dict, List, Union
+
 import numpy as np
+
+import popart
 from popart_core import _InferenceSessionCore, _TrainingSessionCore
-# A wrapper around the Session cpp class, renamed SessionCore in pybind,
-# to enable more pythonic use. See session.hpp for parameter descriptions
 
 
-def _initAnchorArrays(self):
+def _initAnchorArrays(sess: Union["InferenceSession", "TrainingSession"]
+                      ) -> Dict[str, np.array]:
+    """Create the anchor arrays to feed data back into python with.
+
+    Arguments:
+        sess {InferenceSession or TrainingSession} -- popart session.
+
+    Returns:
+        Dict[str, np.array] --  Dict of anchor names and their relevant np arrays.
+    """
+
     anchorArrays = {}
-    for anchor in self.dataFeed.anchors():
-        anchorInfo = self.getInfo(anchor)
+    for anchor in sess.dataFeed.anchors():
+        anchorInfo = sess.getInfo(anchor)
 
         # Anchor tensor shape corresponds to a single input sample. The
         # anchor array, as returned to the user, has a shape which is a
         # function of sample shape, step size, and return type
         anchorShape = anchorInfo.shape()
-        batchesPerStep = self.dataFeed.batchesPerStep()
-        artId = self.dataFeed.art(anchor).id()
+        batchesPerStep = sess.dataFeed.batchesPerStep()
+        artId = sess.dataFeed.art(anchor).id()
 
         # There are some conditions where the output is a single sample,
         # and therefore has the same shape as the anchor tensor
@@ -23,21 +34,20 @@ def _initAnchorArrays(self):
         # output.
         # With all options enabled return anchors are of the shape:
         # [batches_per_step, accl_factor, repl_factor, micro_batch, *data_shape]
-        # TODO: T12496 confirm this ordering is correct with a test.
 
-        anchorArrayShape = [self.replicationFactor]
+        anchorArrayShape = [sess.replicationFactor]
         if artId == popart.AnchorReturnTypeId.FINAL or artId == popart.AnchorReturnTypeId.SUM:
             pass
         elif artId == popart.AnchorReturnTypeId.ALL:
-            anchorArrayShape.insert(0, self.accumulationFactor)
+            anchorArrayShape.insert(0, sess.accumulationFactor)
             anchorArrayShape.insert(0, batchesPerStep)
         elif artId == popart.AnchorReturnTypeId.EVERYN:
-            if (batchesPerStep % self.dataFeed.art(anchor).rp() != 0):
+            if batchesPerStep % sess.dataFeed.art(anchor).rp() != 0:
                 raise RuntimeError(
                     "Invalid anchor period, does not divide batchesPerStep")
 
-            arp = self.dataFeed.art(anchor).rp()
-            anchorArrayShape.insert(0, self.accumulationFactor)
+            arp = sess.dataFeed.art(anchor).rp()
+            anchorArrayShape.insert(0, sess.accumulationFactor)
             anchorArrayShape.insert(0, batchesPerStep // arp)
 
         anchorArrayShape = [x for x in anchorArrayShape if x != 1]
@@ -49,35 +59,76 @@ def _initAnchorArrays(self):
     return anchorArrays
 
 
-# Custom expection thrown by the devicePrepare call
 class PrepareDeviceException(popart.popart_exception):
-    def __init__(self, e):
+    """Custom expection thrown by the devicePrepare call
+    """
+
+    def __init__(self, e: popart.popart_exception) -> None:
+        """Class initializer
+
+        Arguments:
+            e {popart.popart_exception} -- Popart exception to be thrown.
+        """
         super(popart.popart_exception, self).__init__(str(e))
         self.error = e
 
-    def getSummaryReport(self):
+    def getSummaryReport(self) -> str:
+        """Get the summary report
+
+        Returns:
+            str -- The summary report string.
+        """
         return self.error.getSummaryReport()
 
-    def getGraphReport(self):
+    def getGraphReport(self) -> str:
+        """Get the graph report
+
+        Returns:
+            str -- The graph report string.
+        """
         return self.error.getGraphReport()
 
 
 class InferenceSession(_InferenceSessionCore):
+    """ Create a runtime class for executing an ONNX graph on a set of IPU
+    hardware for inference.
+
+    A wrapper around the Session cpp class, renamed SessionCore in pybind,
+    to enable more pythonic use. See session.hpp for parameter descriptions.
+
+    Arguments:
+        fnModel {bytes} -- ONNX model proto. Usually a loaded ONNX model, or from
+            builder.getModelProto()
+        dataFeed {Dict[int, Dict]} -- Configuration for the data feeds and fetches
+        deviceInfo {popart.DeviceInfo} -- DeviceInfo object specifying device type
+            (IPU, IPUModel, CPU) and count.
+        losses {List[popart.Loss]} -- A list of loss layers to use when training (default: {[]})
+        inputShapeInfo {popart.InputShapeInfo} -- Information about the shapes of input and output
+            tensors (default: {popart.InputShapeInfo()})
+        passes {popart.Patterns} -- Patterns to be run for optimization etc.
+            Note: default for passes must not be popart.Patterns()
+            when `import popart` is run, the default arguments are created.
+            If the user then loads a custom pattern via:
+            ctypes.cdll.LoadLibrary(custom_pattern_lib.so)
+            The already constructed popart.Patterns will
+            not include the custom pattern. (default: {None})
+        userOptions {popart.SessionOptions} -- Session options to apply. 
+            (default: {popart.SessionOptions()})
+
+    Raises:
+        popart.PrepareDeviceException: Exception thrown if an invalid device is provided.
+    """
+
     def __init__(
             self,
-            fnModel,
-            dataFeed,
-            deviceInfo,
-            losses=[],
-            inputShapeInfo=popart.InputShapeInfo(),
-            # default for passes must not be popart.Patterns()
-            # when `import popart` is run, the default arguments are created.
-            # If the user then loads a custom pattern via:
-            #     ctypes.cdll.LoadLibrary(custom_pattern_lib.so)
-            # The already constructed popart.Patterns will
-            # not include the custom pattern.
-            passes=None,
-            userOptions=popart.SessionOptions()):
+            fnModel: bytes,
+            dataFeed: Dict[int, Dict],
+            deviceInfo: popart.DeviceInfo,
+            losses: List[popart.Loss] = [],
+            inputShapeInfo: popart.InputShapeInfo = popart.InputShapeInfo(),
+            passes: popart.Patterns = None,
+            userOptions: popart.SessionOptions = popart.SessionOptions()
+    ) -> None:
 
         if passes == None:
             passes = popart.Patterns()
@@ -87,41 +138,76 @@ class InferenceSession(_InferenceSessionCore):
                              inputShapeInfo, userOptions, passes)
 
         self.dataFeed = dataFeed
-        self.replicationFactor = userOptions.replicatedGraphCount if userOptions.enableReplicatedGraphs else 1
-        self.accumulationFactor = userOptions.accumulationFactor if userOptions.enableGradientAccumulation else 1
+        self.replicationFactor = userOptions.replicatedGraphCount if \
+            userOptions.enableReplicatedGraphs else 1
+        self.accumulationFactor = userOptions.accumulationFactor if \
+            userOptions.enableGradientAccumulation else 1
 
-    def initAnchorArrays(self):
+    def initAnchorArrays(self) -> Dict[str, np.array]:
+        """Create the anchor arrays to feed data back into python with.
+
+        Returns:
+            Dict[str, np.array] -- Dict of anchor names and their relevant np arrays.
+        """
         return _initAnchorArrays(self)
 
-    def prepareDevice(self):
+    def prepareDevice(self) -> None:
+        """Prepare the network for execution.
+
+        This will create the poplar::Graph, poplar::Engine, and setting up
+        poplar::Streams.
+
+        Raises:
+            popart.PrepareDeviceException: Exception thrown if an invalid device is provided.
+        """
 
         err = popart.PrepareDeviceError()
         super(InferenceSession, self).prepareDevice(err)
 
         # If an error occurred during the perpareDevice raise an exception
-        if (not err.isSuccessful()):
+        if not err.isSuccessful():
             raise popart.PrepareDeviceException(err)
 
 
 class TrainingSession(_TrainingSessionCore):
+    """Create a runtime class for executing an ONNX graph on a set of IPU
+        hardware for training
+
+        A wrapper around the Session cpp class, renamed SessionCore in pybind,
+        to enable more pythonic use. See session.hpp for parameter descriptions.
+
+        Arguments:
+            fnModel {bytes} -- ONNX model proto. Usually a loaded ONNX model,
+                or from builder.getModelProto()
+            dataFeed {Dict[int, Dict]} -- Configuration for the data feeds and fetches
+            losses {List[popart.Loss]} -- A list of loss layers to use when training
+            optimizer {popart.Optimizer} -- The type of optimizer to use when training
+                and it's properties.
+            deviceInfo {popart.DeviceInfo} -- DeviceInfo object specifying device type
+                (IPU, IPUModel, CPU) and count.
+            inputShapeInfo {popart.InputShapeInfo} -- Information about the shapes of
+                input and output tensors (default: {popart.InputShapeInfo()})
+            passes {popart.Patterns} -- Optimization patterns to apply (default: {None})
+            userOptions {popart.SessionOptions} -- Session options to apply. 
+                (default: {popart.SessionOptions()})
+
+        Raises:
+            popart.PrepareDeviceException: Exception thrown if an invalid device is provided.
+    """
+
     def __init__(
             self,
-            fnModel,
-            dataFeed,
-            losses,
-            optimizer,
-            deviceInfo,
-            inputShapeInfo=popart.InputShapeInfo(),
-            # default for passes must not be popart.Patterns()
-            # when `import popart` is run, the default arguments are created.
-            # If the user then loads a custom pattern via:
-            #     ctypes.cdll.LoadLibrary(custom_pattern_lib.so)
-            # The already constructed popart.Patterns will
-            # not include the custom pattern.
-            passes=None,
-            userOptions=popart.SessionOptions()):
+            fnModel: bytes,
+            dataFeed: Dict[int, Dict],
+            losses: List[popart.Loss],
+            optimizer: popart.Optimizer,
+            deviceInfo: popart.DeviceInfo,
+            inputShapeInfo: popart.InputShapeInfo = popart.InputShapeInfo(),
+            passes: popart.Patterns = None,
+            userOptions: popart.SessionOptions = popart.SessionOptions()
+    ) -> None:
 
-        if passes == None:
+        if passes is None:
             passes = popart.Patterns()
 
         super(TrainingSession,
@@ -129,17 +215,31 @@ class TrainingSession(_TrainingSessionCore):
                              inputShapeInfo, userOptions, passes)
 
         self.dataFeed = dataFeed
-        self.replicationFactor = userOptions.replicatedGraphCount if userOptions.enableReplicatedGraphs else 1
-        self.accumulationFactor = userOptions.accumulationFactor if userOptions.enableGradientAccumulation else 1
+        self.replicationFactor = userOptions.replicatedGraphCount if \
+            userOptions.enableReplicatedGraphs else 1
+        self.accumulationFactor = userOptions.accumulationFactor if \
+            userOptions.enableGradientAccumulation else 1
 
-    def initAnchorArrays(self):
+    def initAnchorArrays(self) -> Dict[str, np.array]:
+        """Create the anchor arrays to feed data back into python with.
+
+        Returns:
+            Dict[str, np.array] -- Dict of anchor names and their relevant np arrays.
+        """
         return _initAnchorArrays(self)
 
-    def prepareDevice(self):
+    def prepareDevice(self) -> None:
+        """Prepare the network for execution.
 
+        This will create the poplar::Graph, poplar::Engine, and setting up
+        poplar::Streams.
+
+        Raises:
+            popart.PrepareDeviceException: Exception thrown if an invalid device is provided.
+        """
         err = popart.PrepareDeviceError()
         super(TrainingSession, self).prepareDevice(err)
 
         # If an error occured during the perpareDevice raise an exception
-        if (not err.isSuccessful()):
+        if not err.isSuccessful():
             raise popart.PrepareDeviceException(err)
