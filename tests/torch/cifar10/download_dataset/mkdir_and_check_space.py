697e86e7bdfa524fff190480327d03e55ec5aa61
Nicholas Westlake <nicholasw@graphcore.ai>
2020-04-30 16:16:28 +0100

    Fix CIFAR10 mkdir_and_check_space test for os x
    Reviewers: #popart, evan
    
    Reviewed By: #popart, evan
    
    Maniphest Tasks: T20020
    
    Differential Revision: https://phabricator.sourcevertex.net/D25165

--- tests/torch/cifar10/download_dataset/mkdir_and_check_space.py
+++ tests/torch/cifar10/download_dataset/mkdir_and_check_space.py
@@ -9,9 +9,11 @@ MB_REQUIRED = 400
 
 def enough_hardrive_space():
     df_out = subprocess.check_output(
-        ["df", "--output=avail", "-m",
-         str(c10datadir.c10datadir)]).decode("utf-8")
-    mb_remaining = int(df_out.strip().split("\n")[-1])
+        ["df", "-m", str(c10datadir.c10datadir)]).decode("utf-8")
+    second_line = df_out.strip().split("\n")[-1]
+
+    cols = [col for col in second_line.split(" ") if col.strip() != ""]
+    mb_remaining = int(cols[3])
 
     return (mb_remaining >= MB_REQUIRED)
 
