23346b168b0edc3b978d1a74b8177b929e2ffee4
George Roger <georger@graphcore.ai>
2020-06-19 14:44:14 +0100

    PRelu op forward pass.
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #popart, jamesn, evan
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #popart, jamesn
    
    Subscribers: hatemh
    
    Maniphest Tasks: T22205
    
    Differential Revision: https://phabricator.sourcevertex.net/D27669

---
+++ willow/include/popart/popx/op/prelux.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2018 Graphcore Ltd. All rights reserved.
+#ifndef GUARD_NEURALNET_PRELUX_HPP
+#define GUARD_NEURALNET_PRELUX_HPP
+
+#include <popart/names.hpp>
+#include <popart/popx/op/elementwisex.hpp>
+
+namespace popart {
+
+class PReluOp;
+
+namespace popx {
+
+class PReluOpx : public ElementWiseBinaryOpx {
+public:
+  PReluOpx(Op *, Devicex *);
+  void grow(poplar::program::Sequence &) const final;
+};
+
+} // namespace popx
+} // namespace popart
+
+#endif
