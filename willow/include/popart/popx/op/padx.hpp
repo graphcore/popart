8b743e1bdb1cb7b2e13f0820f33096ec3770d397
James Newling <jamesn@graphcore.ai>
2020-07-24 14:54:32 +0100

    Ref: T24234 Insert a clone and copy of all padding regions in Pad implementation
    Summary:
    - Resolve 12% regression on BERT pretraining of T24234
    - The main change is to clone and copy padding regions.
    - Example: Suppose input of shape (2,) is padded with padLow = (1,) padUpp = (3,)
      so that the output is shape (6,).
      Before: the padding regions of shapes (1,) and (3,) are poplar constants.
      After: the padding regions are clones and copies of the above constants.
      The "core" of the Tensor is unchanged, i.e. the region of (2,) remains an alias of X.
    - It is not clear to me how this change reduces cycles, possibly
    1) the clone removes self-aliases or
    2) the copy from constant effects Tensor liveness, which spills over into a cycle change.
    
    Test Plan: All relevant tests pass
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, evan, george
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, george
    
    Subscribers: evan
    
    Maniphest Tasks: T24234
    
    Differential Revision: https://phabricator.sourcevertex.net/D29624

--- willow/include/popart/popx/op/padx.hpp
+++ willow/include/popart/popx/op/padx.hpp
@@ -31,17 +31,21 @@ class BasePadOp;
 // The padding values (the '.'s above) should be rewritten to their constant
 // value (we're assuming constant padding here, not edge or reflect) every run
 // of the Pad Op, otherwise if they are modified inplace they will be incorrect
-// in subsequent iterations. However, with the current implementation, we do not
-// need to reset the padding values, because they are poplar constant Tensors
-// and therefore cannot be modified by downstream consumers, even if the Ir
-// claims that they are modified inplace.
+// in subsequent iterations.
 //
-// Thus we do:
+// Two options to ensure they are always correct:
 //
-// A) nothing, no need to reset the .s as they are poplar constants.
-// B) nothing.
-// C) copy the xs, no need to reset the .s as they are poplar constants.
-// D) copy the xs.
+// 1) make them poplar constant Tensors, which cannot (by definition) be
+// modified by downstream consumers, even if the Ir claims that they are
+// modified inplace.
+//
+// 2) run a program to set them to the constant value everytime that pad's grow
+// function is called.
+//
+// We have oscillated between the 2 approaches, the current approach is 2. We
+// ensure the padding is set to the correct value every iteration. See the
+// member "cloneNcopyEdges".
+//
 //
 namespace popx {
 
@@ -53,7 +57,6 @@ public:
                          poplar::program::Sequence &) const;
 
 private:
-  bool mustRewritePadding() const { return true; }
   poplar::Tensor constantModePadGrow(poplar::Tensor inTensor,
                                      poplar::program::Sequence &) const;
 
@@ -64,9 +67,35 @@ private:
   std::pair<bool, poplar::Tensor> getPropitiousPadLayout() const;
 
   // TODO T22336  : move this functionality to poplibs
+  // Pad the tensor "toPad", so that it has the same shape as toMapEdgesFrom,
+  // and the padding added has the same tile mapping as the corresponding
+  // element of toMapEdgesFrom. toMapEdgesFrom must be of the correct shape, as
+  // specified by the Ir PadOp.
   poplar::Tensor
   padWithTargetMapping(const poplar::Tensor &toPad,
                        const poplar::Tensor &toMapEdgesFrom) const;
+
+  // Return a Tensor of the same shape as inTensor, which is an alias of
+  // inTensor at the core, and a copy of inTensor on the padding edges.
+  poplar::Tensor cloneNcopyEdges(poplar::Tensor inTensor,
+                                 poplar::program::Sequence &) const;
+
+  struct Chisseled {
+    Chisseled(poplar::Tensor c,
+              const std::vector<poplar::Tensor> &l,
+              const std::vector<poplar::Tensor> &u)
+        : core(c), lows(l), upps(u) {}
+    poplar::Tensor core;
+    std::vector<poplar::Tensor> lows;
+    std::vector<poplar::Tensor> upps;
+  };
+
+  Chisseled getChisseled(const poplar::Tensor &) const;
+
+  poplar::Tensor flip(const poplar::Tensor &) const;
+
+  poplar::Tensor unflippedPadGrow(poplar::Tensor inTensor,
+                                  poplar::program::Sequence &) const;
 };
 
 class PadOpx : public BasePadOpx {
--- willow/src/popx/op/padx.cpp
+++ willow/src/popx/op/padx.cpp
@@ -121,9 +121,9 @@ BasePadOpx::padWithTargetMapping(const poplar::Tensor &toPad,
     }
   }
 
-  // return a Tensor which has the same tile mapping as x, but is constant of
-  auto getConstPad = [this, padValue](const poplar::Tensor &x,
-                                      const std::string &dbs) {
+  // return a Tensor which has the same tile mapping as x, but is constant
+  const auto getConstPad = [this, padValue](const poplar::Tensor &x,
+                                            const std::string &dbs) {
     auto constPad = graph().addConstant<float>(
         x.elementType(),
         x.shape(),
@@ -133,21 +133,14 @@ BasePadOpx::padWithTargetMapping(const poplar::Tensor &toPad,
     return constPad;
   };
 
+  auto chisseled = getChisseled(toMapEdgesFrom);
   std::vector<poplar::Tensor> leftPads(tRank);
   std::vector<poplar::Tensor> rightPads(tRank);
-  auto chisseledToEmulate = toMapEdgesFrom;
-
   for (size_t d = 0; d < tRank; ++d) {
-    const auto d_u32   = static_cast<uint32_t>(d);
-    const auto dimSize = chisseledToEmulate.dim(d_u32);
-    const auto low     = lowerPadding[d];
-    const auto upp     = dimSize - upperPadding[d];
-
-    const auto lowToClone = chisseledToEmulate.slice(0, low, d_u32);
-    const auto uppToClone = chisseledToEmulate.slice(upp, dimSize, d_u32);
-    leftPads[d]  = getConstPad(lowToClone, "/leftPad_dim" + std::to_string(d));
-    rightPads[d] = getConstPad(uppToClone, "/rightPad_dim" + std::to_string(d));
-    chisseledToEmulate = chisseledToEmulate.slice(low, upp, d_u32);
+    leftPads[d] =
+        getConstPad(chisseled.lows[d], "/leftPad_dim" + std::to_string(d));
+    rightPads[d] =
+        getConstPad(chisseled.upps[d], "/rightPad_dim" + std::to_string(d));
   }
 
   auto padded = toPad;
@@ -170,10 +163,79 @@ BasePadOpx::padWithTargetMapping(const poplar::Tensor &toPad,
   return padded;
 }
 
+BasePadOpx::Chisseled BasePadOpx::getChisseled(const poplar::Tensor &t0) const {
+
+  auto t             = t0;
+  const size_t tRank = t.rank();
+  auto &&op          = getBasePadOp();
+
+  const auto lowerPadding = op.getLowerPadding();
+  const auto upperPadding = op.getUpperPadding();
+
+  std::vector<poplar::Tensor> lowPads(tRank);
+  std::vector<poplar::Tensor> uppPads(tRank);
+
+  for (size_t d = 0; d < tRank; ++d) {
+    const auto d_u32   = static_cast<uint32_t>(d);
+    const auto dimSize = t.dim(d_u32);
+    const auto low     = lowerPadding[d];
+    const auto upp     = dimSize - upperPadding[d];
+    lowPads[d]         = t.slice(0, low, d_u32);
+    uppPads[d]         = t.slice(upp, dimSize, d_u32);
+    t                  = t.slice(low, upp, d_u32);
+  }
+  return {t, lowPads, uppPads};
+}
+
+poplar::Tensor
+BasePadOpx::cloneNcopyEdges(poplar::Tensor t,
+                            poplar::program::Sequence &se) const {
+
+  logging::devicex::debug("Cloning and copying the constant padding for op {}",
+                          getBasePadOp().str());
+
+  const auto tRank = t.rank();
+
+  // partition the input into the "core" and the padding edges.
+  auto chisseled = getChisseled(t);
+  t              = chisseled.core;
+  auto leftPads  = chisseled.lows;
+  auto rightPads = chisseled.upps;
+
+  // clone and copy the edges
+  // These copies will be merged by poplar, so it is not necessary to
+  // concatenate all the Tensors and create one single large copy program.
+  for (auto &p : leftPads) {
+    if (p.numElements() > 0) {
+      p = cloneNcopy(se, p);
+    }
+  }
+  for (auto &p : rightPads) {
+    if (p.numElements() > 0) {
+      p = cloneNcopy(se, p);
+    }
+  }
+
+  // wrap the core in the copied edges. Thus the core remains an alias od the
+  // input.
+  for (int d = static_cast<int>(tRank) - 1; d >= 0; --d) {
+    const auto d_u64 = static_cast<size_t>(d);
+    const auto d_u32 = static_cast<uint32_t>(d);
+
+    if (leftPads[d_u64].numElements() > 0) {
+      t = poplar::concat({leftPads[d_u64], t}, d_u32);
+    }
+    if (rightPads[d_u64].numElements() > 0) {
+      t = poplar::concat({t, rightPads[d_u64]}, d_u32);
+    }
+  }
+
+  return t;
+}
+
 poplar::Tensor
 BasePadOpx::constantModePadGrow(poplar::Tensor inTensor,
-                                poplar::program::Sequence &) const {
-
+                                poplar::program::Sequence &s) const {
   auto mk_padded =
       [this, &inTensor](const popops::padding::MappingMethod mappingMethod)
       -> poplar::Tensor {
@@ -194,26 +256,35 @@ BasePadOpx::constantModePadGrow(poplar::Tensor inTensor,
   // elements, so "edge" mapping is not possible and we must do it ourselves.
   // Note a tensor has 0 elements iff it has a zero-sized dimension.
   if (inTensor.numElements() == 0) {
-    auto outTensor = mk_padded(popops::padding::MappingMethod::NONE);
-
+    poplar::Tensor outTensor = mk_padded(popops::padding::MappingMethod::NONE);
     dv_p->getLinearMapper().mapTensor(Opx::graph(), outTensor);
-
     return outTensor;
   }
 
+  poplar::Tensor outTensor;
   // Can we find a good layout for the constant padding?
-  auto propitious = getPropitiousPadLayout();
+  const auto propitious = getPropitiousPadLayout();
   if (propitious.first) {
-    return padWithTargetMapping(inTensor, propitious.second);
+    outTensor = padWithTargetMapping(inTensor, propitious.second);
   }
 
   else {
-    return mk_padded(popops::padding::MappingMethod::EDGE);
+    outTensor = mk_padded(popops::padding::MappingMethod::EDGE);
   }
+
+  // We clone and copy edges, but keep the core (the tensor being padded) as an
+  // alias. It is not 100% clear why this helps, but it has been observed to
+  // reduce the cycle count. Speculative explanantions:
+  // - by cloning, all self-aliases are removed which mean consumers can be
+  // inplace.
+  // - the constants are replaced by non-constants, which has implications for
+  // liveness, which has a knock-on effect for cycles.
+  return cloneNcopyEdges(outTensor, s);
 }
 
-poplar::Tensor BasePadOpx::padGrow(poplar::Tensor inTensor,
-                                   poplar::program::Sequence &prog) const {
+poplar::Tensor
+BasePadOpx::unflippedPadGrow(poplar::Tensor inTensor,
+                             poplar::program::Sequence &prog) const {
 
   auto &&padBaseOp = getBasePadOp();
   const auto mode  = padBaseOp.getMode();
@@ -239,33 +310,34 @@ poplar::Tensor BasePadOpx::padGrow(poplar::Tensor inTensor,
   throw error(oss.str());
 }
 
+poplar::Tensor BasePadOpx::padGrow(poplar::Tensor inTensor,
+                                   poplar::program::Sequence &prog) const {
+  return unflippedPadGrow(flip(inTensor), prog);
+}
+
 void PadOpx::grow(poplar::program::Sequence &prog) const {
-  auto in0      = Opx::getInTensor(BasePadOp::getInIndex());
-  auto inTensor = cloneNcopy(prog, in0);
-
-  auto &&padBaseOp = getBasePadOp();
-  for (const unsigned &flip : padBaseOp.getFlips()) {
-    inTensor = inTensor.reverse(flip);
-  }
-
-  setOutTensor(BasePadOp::getOutIndex(), padGrow(inTensor, prog));
+  auto in0    = Opx::getInTensor(BasePadOp::getInIndex());
+  auto padded = padGrow(cloneNcopy(prog, in0), prog);
+  setOutTensor(BasePadOp::getOutIndex(), padded);
 }
 
 void PadInplaceOpx::grow(poplar::program::Sequence &prog) const {
-
-  auto in0      = Opx::getInTensor(BasePadOp::getInIndex());
-  auto inTensor = cloneNcopy(prog, in0);
-
-  auto &&padBaseOp = getBasePadOp();
-  for (const unsigned &flip : padBaseOp.getFlips()) {
-    inTensor = inTensor.reverse(flip);
-  }
-
-  setOutTensor(BasePadOp::getOutIndex(), padGrow(inTensor, prog));
+  auto in0    = Opx::getInTensor(BasePadOp::getInIndex());
+  auto padded = padGrow(in0, prog);
+  setOutTensor(BasePadOp::getOutIndex(), padded);
 }
 
 PadGradOpx::PadGradOpx(Op *op, Devicex *devicex) : SliceOpx(op, devicex) {
   verifyOp<PadGradOpx>(op, Onnx::GradOperators::PadGrad);
+}
+
+poplar::Tensor BasePadOpx::flip(const poplar::Tensor &inTensor) const {
+  auto oTensor     = inTensor;
+  auto &&padBaseOp = getBasePadOp();
+  for (auto f : padBaseOp.getFlips()) {
+    oTensor = oTensor.reverse(f);
+  }
+  return oTensor;
 }
 
 PadInplaceOpx::PadInplaceOpx(Op *op, Devicex *devicex)
