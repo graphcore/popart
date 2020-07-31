78fe6b1a748829cc269e82ff1f573c9488d41286
George Roger <georger@graphcore.ai>
2020-07-31 14:05:54 +0100

    Remove backward compatibility with poponnx.
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, evan
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, evan
    
    Maniphest Tasks: T23429
    
    Differential Revision: https://phabricator.sourcevertex.net/D29930

--- willow/src/util.cpp
+++ willow/src/util.cpp
@@ -10,21 +10,7 @@
 namespace popart {
 
 char *getPopartEnvVar(std::string env_var) {
-  auto result = std::getenv(logging::format("POPART_{}", env_var).c_str());
-  if (result) {
-    return result;
-  }
-
-  result = std::getenv(logging::format("POPONNX_{}", env_var).c_str());
-  if (result != nullptr) {
-    std::cerr << logging::format(
-        "You are using a deprecated environment variable "
-        "'POPONNX_{}', please change it to 'POPART_{}'\n",
-        env_var,
-        env_var);
-  }
-
-  return result;
+  return std::getenv(logging::format("POPART_{}", env_var).c_str());
 }
 
 std::ostream &operator<<(std::ostream &ss, const std::vector<std::size_t> &v) {
