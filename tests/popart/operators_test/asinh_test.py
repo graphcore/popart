Fix identity loss grad scaling when reduction type is 'mean'
Summary:
The way the identity loss grad op scales its output when reduction type is 'mean' is incorrect in two ways:
- it scales the output by (1 * globalReplicationFactor), instead of (1 / globalReplicationFactor)
- it scales the output by num_input_elements (== 1), instead of  num_output_elements (can be any number)

This diff fixes the above, and fixes the tests that started failing as a result.

Fixes T32419

Test Plan: I've added test coverage in global_batch_size_test.py, but this is still commented out in the test's CMakeLists.txt because of a timeout issue (T33333)

Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jamesn, shirazb

Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jamesn, shirazb

Subscribers: jamesn, sylvainv, shirazb

Maniphest Tasks: T32419

Differential Revision: https://phabricator.sourcevertex.net/D39494

tests/popart/operators_test/asinh_test.py
@@ -76,7 +76,7 @@ def test_asinh_grad(op_tester):
 
     def reference(ref_data):
         out = np.arcsinh(d1)
-        d__o = derivative_asinh(d1)
+        d__o = derivative_asinh(d1) * ref_data.getOutputTensorGrad(0)
         return [out, d__o, None]
 
     op_tester.setPatterns([

tests/popart/operators_test/atanh_test.py
@@ -67,7 +67,7 @@ def test_atanh_grad(op_tester):
 
     def reference(ref_data):
         out = np.arctanh(d1)
-        d__o = derivative_atanh(d1)
+        d__o = derivative_atanh(d1) * ref_data.getOutputTensorGrad(0)
         return [out, d__o, None]
 
     op_tester.setPatterns([

tests/popart/operators_test/operators_test.py
@@ -1530,7 +1530,7 @@ def test_acosh_grad(op_tester):
 
     def reference(ref_data):
         out = np.arccosh(d1)
-        d__o = derivative_acosh(d1)
+        d__o = derivative_acosh(d1) * ref_data.getOutputTensorGrad(0)
         return [out, d__o, None]
 
     op_tester.setPatterns([
@@ -1603,7 +1603,6 @@ def test_atan_grad(op_tester):
         out.backward(torch.tensor(d__o))
         return [out, a.grad, None]
 
-    # op_tester.setPatterns(['OpToIdentity'], enableRuntimeAsserts=False)
     op_tester.run(init_builder, reference, 'train')
 
 

tests/popart/operators_test/slice_test.py
@@ -240,15 +240,15 @@ def test_slice_end_out_of_bounds(op_tester):
         o = builder.aiOnnx.slice([i1, starts, ends, axes])
         builder.addOutputTensor(o)
 
-        return [o, popart.reservedGradientPrefix() + i1]
+        return [o, popart.reservedGradientPrefix() + i1, popart.reservedGradientPrefix() + o]
 
     def reference(ref_data):
         o = d1[:, 1:1000]
 
-        o_grad = np.ones(o.shape, dtype=np.float32)
+        o_grad = np.ones(o.shape, dtype=np.float32) * ref_data.getOutputTensorGrad(0)
         i1_grad = np.pad(o_grad, [(0, 0), (1, 0), (0, 0)], constant_values=0.)
 
-        return [o, i1_grad]
+        return [o, i1_grad, None]
 
     op_tester.run(init_builder, reference, 'train')
 