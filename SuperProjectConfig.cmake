5d98384ac60e1fcc971736877fe954290c325444
Shiraz Butt <shirazb@graphcore.ai>
2021-02-10 16:13:44 +0000

    Amend SuperProjectConfig.cmake now Popart only lives in poplar view
    Summary:
    Add documentation explaining not to add to this file and why.
    
    Remove setting of CMAKE_INDEPENDENT_CODE - we are letting poplar_packaging's
    config handle all view-wide state
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jamesn
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jamesn
    
    Subscribers: jamesn
    
    Maniphest Tasks: T32910
    
    Differential Revision: https://phabricator.sourcevertex.net/D40349

--- SuperProjectConfig.cmake
+++ SuperProjectConfig.cmake
@@ -1,27 +1,54 @@
+#[[
+  SuperProjectConfig.cmake
 
-# Other projects in the view (in particular Boost) will not get built with fPIC
-# unless we set this here. (CBT causes projects to inherit certain cache entires
-# including this one, then boost_scripts will add fPIC to the boost cxx flags if
-# it is set)
-set(CMAKE_POSITION_INDEPENDENT_CODE ON CACHE BOOL
-  "Default value for POSITION_INDEPENDENT_CODE of targets.")
+  ------------------------------------------------------------------------------
+  This file was needed when Popart used to live in its own view. It is now
+  unnecessary. Please do not add to it.
+  ------------------------------------------------------------------------------
 
-#
-# Usage:
-#   _popart_pass_through_cache_var(<var> [PATH])
-#
-# Implicitly passes through a variable to Popart that has been defined directly
-# on the super project, like -Dvar=val, instead of explicitly like
-# -DPOPART_CMAKE_ARGS=-Dvar=val.
-#
-# In general, for a path, an absolute path should be given, but for backwards
-# compatibility we support paths relative to the view build dir. Popart itself
-# (not the superproject, which this file is part of) documents that it expects
-# an absolute path; thus if you pass a relative path in POPART_CMAKE_ARGS, it
-# will not get evalulated correctly.
-#
-# If user for some reason defined both directly and in POPART_CMAKE_ARGS, the
-# definition in POPART_CMAKE_ARGS will get overwritten with this definition.
+  A SuperProjectConfig.cmake file is used to configure the view itself, not 
+  Popart. It is run as part of the CBT super-project. It is not run as part of
+  Popart's own build.
+
+  poplar_packaging already has a SuperProjectConfig.cmake that configures the
+  Poplar view. We should keep all view-wide configuration to that file only, NOT
+  HERE. For example, setting CMAKE_POSITION_INDEPEDENT_CODE for the whole view
+  should be done there only.
+
+  VERY IMPORTANT:   DO NOT DEFINE NEW POPART CMAKE VARIABLES HERE.
+
+  Popart CMake variables should be defined in Popart's actual CMake files, not
+  this file. It is correct to force the user to configure the view like
+  -DPOPART_CMAKE_ARGS="-DSOME_ARG=1", instead of -DSOME_ARG=1 (expecting that
+  this file will pass through the variable to Popart itself). 
+
+  The below code for enabling this is for backwards compatability only as this
+  had already been done, causing bugs - see T32416.
+
+  From a design perspective, Popart is its own individual project, so its
+  variables should be defined in Popart, not as part of the view. Popart should
+  build just fine outside of the view.
+]]
+
+#[[
+  macro(_popart_pass_through_cache_var var is_path)
+
+  Usage:
+    _popart_pass_through_cache_var(<var> [PATH])
+ 
+  Implicitly passes through a variable to Popart that has been defined directly
+  on the super project, like -Dvar=val, instead of explicitly like
+  -DPOPART_CMAKE_ARGS=-Dvar=val.
+ 
+  In general, for a path, an absolute path should be given, but for backwards
+  compatibility we support paths relative to the view build dir. Popart itself
+  (not the superproject, which this file is part of) documents that it expects
+  an absolute path; thus if you pass a relative path in POPART_CMAKE_ARGS, it
+  will not get evalulated correctly.
+ 
+  If user for some reason defined both directly and in POPART_CMAKE_ARGS, the
+  definition in POPART_CMAKE_ARGS will get overwritten with this definition.
+]]
 macro(_popart_pass_through_cache_var var is_path)
   if(DEFINED ${var})   
     set(_val "${${var}}")
@@ -53,6 +80,7 @@ _popart_pass_through_cache_var(C10_DIR PATH)
 _popart_pass_through_cache_var(POPART_PKG_DIR PATH)
 _popart_pass_through_cache_var(POPART_CBT_VIEW_DIR PATH)
 
+# TODO(T34104): Move this to poplar_packaging/SuperProjectConfig.cmake
 set(CMAKE_CONFIGURATION_TYPES "Release" "Debug" "MinSizeRel" "RelWithDebInfo")
 set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS ${CMAKE_CONFIGURATION_TYPES})
 # Enable IN_LIST operator
