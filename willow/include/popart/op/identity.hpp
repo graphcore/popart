c77ab0f93d40c2e007d4294fe58eab274b3bba61
Roman Derco <romand@graphcore.ai>
2021-06-11 13:11:00 +0100

    IdentityGradOp, redundant index
    Summary: Remove redundant index method.
    
    Test Plan: All tests pass.
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, shirazb
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, shirazb
    
    Subscribers: #popart
    
    Maniphest Tasks: T40560
    
    Differential Revision: https://phabricator.sourcevertex.net/D46685

--- willow/include/popart/op/identity.hpp
+++ willow/include/popart/op/identity.hpp
@@ -49,8 +49,6 @@ public:
   const std::vector<GradInOutMapper> &gradInputInfo() const final;
   const std::map<int, int> &gradOutToNonGradIn() const final;
 
-  static InIndex getInIndex() { return 0; }
-  static OutIndex getOutIndex() { return 0; }
 };
 
 class IdentityLossOp : public LossOp {
