749e5e4e92ae7e014dc5271c248117ecdbbe27a6
George Roger <georger@graphcore.ai>
2021-08-31 11:07:31 +0100

    Improve linter error message.
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, fabiant
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, fabiant
    
    Maniphest Tasks: T45070
    
    Differential Revision: https://phabricator.sourcevertex.net/D50605

--- tests/popart_test_linter/check_test_has_cmake_entry.py
+++ tests/popart_test_linter/check_test_has_cmake_entry.py
@@ -27,6 +27,14 @@ def is_probably_pytest_file(file_path):
     return False
 
 
+def popart_root_dir():
+    here = Path(__file__).resolve()
+    assert 'popart' in here.parts
+    while here.parent.name != 'popart':
+        here = here.parent
+    return here.parent
+
+
 # Check the CMakeLists.txt file for a call to
 # `add_popart_py_unit_test` that takes `lint_path` as a parameter.
 # This function exits the progam with return code 1 if no test entry was found.
@@ -34,6 +42,9 @@ def check_for_test_entry(lint_path, cmakelists_path):
     with cmakelists_path.open() as f:
         for line in f.readlines():
             # Match 0 or more instances of '# ' to also match commented out entries.
+            # We catch commented out entires as these are not tests where the 
+            # cmake entry has been forgotten, but usually tests that are broken 
+            # and have a task to fix attached.
             pattern = '(#\s*)?'
             # Match instances of 'add_popart_py_unit_test(some_test_name '
             pattern += 'add_popart_py_unit_test\(\S+ '
@@ -41,8 +52,14 @@ def check_for_test_entry(lint_path, cmakelists_path):
             pattern += f'{lint_path.name}'
             if re.match(pattern, line):
                 return
+
+    test_relative_path = lint_path.relative_to(popart_root_dir())
+    cmakelists_relative_path = cmakelists_path.relative_to(popart_root_dir())
     print(
-        f"Could not find `add_popart_py_unit_test` entry for '{lint_path.name}' in '{cmakelists_path}'",
+        f"Could not find `add_popart_py_unit_test` entry for '{lint_path.name}' "
+        f"in '{cmakelists_relative_path}'. If this is intentional, please exclude the "
+        f"path \"({test_relative_path})\" in the popart .arclint file, under the"
+        f" section 'popart-test-linter'.",
         file=sys.stderr)
     exit(1)
 
