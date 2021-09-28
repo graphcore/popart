Fix softplus gradient calculation
Summary: This patch adds a missing `* grad_in` to the softplus gradient.

Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, michaell

Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, michaell

Subscribers: michaell

Maniphest Tasks: T45474

Differential Revision: https://phabricator.sourcevertex.net/D52101

tests/integration/operators_test/softplus_test.py
@@ -3,23 +3,20 @@ import itertools
 import numpy as np
 import popart
 import torch
+import torch.nn.functional as F
 import pytest
 from op_tester import op_tester
 
 
-def softplus_torch(input):
-    """Return softplus = log(exp(x) + 1)"""
-    # NOTE: The softplus implementation in PyTorch has two parameters
-    #       beta and treshold which is not present in the ONNX
-    #       specification
-    result = torch.log(torch.exp(input) + torch.ones(input.shape))
-    return result
+def create_input():
+    # Make the test on 250 numbers over a wide range of values to test saturation behaviour
+    f32info = np.finfo(np.float32)
+    return np.linspace(f32info.min, f32info.max, num=250, dtype=np.float32)
 
 
 def test_softplus(op_tester):
     """Test the softplus operator."""
-    # Make the test on 250 random numbers
-    input_data = np.random.rand(1, 250).astype(np.float32)
+    input_data = create_input()
 
     def init_builder(builder):
         """Build the softplus operator."""
@@ -32,33 +29,41 @@ def test_softplus(op_tester):
     def reference(_):
         """Return the result of the python softplus function above."""
         torch_test_data = torch.tensor(input_data, requires_grad=False)
-        result = [softplus_torch(torch_test_data)]
+        result = [F.softplus(torch_test_data)]
         return result
 
-    op_tester.run(init_builder, reference, 'infer')
-    op_tester.setPatterns(['InPlace'], enableRuntimeAsserts=False)
     op_tester.run(init_builder, reference, 'infer')
 
 
 def test_softplus_grad(op_tester):
     """Test the softplus gradient operator."""
-    input_data = np.random.rand(1, 250).astype(np.float32)
+    input_data = create_input()
 
     def init_builder(builder):
         i1 = builder.addInputTensor(input_data)
         o = builder.aiOnnx.softplus([i1])
         builder.addOutputTensor(o)
         op_tester.setPatterns(['InPlace'], enableRuntimeAsserts=False)
-        result = [o, popart.reservedGradientPrefix() + i1]
+        # Set the result to
+        # ['Softplus:0', 'Gradient___input', 'Gradient___Softplus:0']
+        result = [
+            o,
+            popart.reservedGradientPrefix() + i1,
+            popart.reservedGradientPrefix() + o
+        ]
         return result
 
-    def reference(_):
+    def reference(ref_data):
         """Return the result of the gradient of the python softplus function above."""
         torch_test_data = torch.tensor(input_data, requires_grad=True)
-        fwd = softplus_torch(torch_test_data)
-        # NOTE: We set the gradient w.r.t. to one in order to get a valid result
-        bwd = fwd.backward(torch.ones(fwd.shape))
-        result = [fwd, bwd]
+        fwd = F.softplus(torch_test_data)
+        # Return the gradient of the input tensor at index 0, i.e. the gradient of softplus
+        d__o = ref_data.getOutputTensorGrad(0)
+        # Set the gradient to torch_test_data.grad
+        fwd.backward(torch.tensor(d__o))
+        # Set the result so that it will correspond to
+        # ['Softplus:0', 'Gradient___input', 'Gradient___Softplus:0']
+        result = [fwd, torch_test_data.grad, d__o]
         return result
 
     op_tester.run(init_builder, reference, 'train')