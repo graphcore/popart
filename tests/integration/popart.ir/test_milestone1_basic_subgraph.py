b969f36ca0657717d1d9dc0a8d52dfe2d352c424
Shiraz Butt <shirazb@graphcore.ai>
2021-10-01 12:27:39 +0100

    Switch Milestone 1 to use matmul
    Summary: Ref T40307.
    
    Reviewers: matthewha, #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Reviewed By: matthewha, #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Maniphest Tasks: T40307
    
    Differential Revision: https://phabricator.sourcevertex.net/D52547

--- tests/integration/popart.ir/test_milestone1_basic_subgraph.py
+++ tests/integration/popart.ir/test_milestone1_basic_subgraph.py
@@ -20,7 +20,7 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 import test_util as tu
 
 
-class ScaleNShift(pir.Module):
+class Linear(pir.Module):
     def __init__(self):
         self.W: pir.Tensor = None
         self.b: pir.Tensor = None
@@ -29,14 +29,14 @@ class ScaleNShift(pir.Module):
               bias: bool = True) -> pir.Tensor:
         self.W = pir.subgraph_input(pir.float32, (x.shape[-1], out_features),
                                     "W")
-        y = ops.mul(x, self.W)  # TODO: Change to matmul once implemented
+        y = x @ self.W
         if bias:
             self.b = pir.subgraph_input(pir.float32, (out_features, ), "b")
             y = y + self.b
         return y
 
 
-_INPUT_SHAPE = (16, 16)
+_INPUT_SHAPE = (2, 16)
 
 
 # Build model using popart.ir API, then return the underlying Ir.
@@ -48,20 +48,27 @@ def build_model() -> Tuple[_ir.Ir, pir.HostToDeviceStream, pir.
 
     main = ir.main_graph()
     with main:
-        # NOTE: Change to (2, 16) for matmul
         x_h2d = pir.h2d_stream(pir.float32, _INPUT_SHAPE, name="x_stream")
         x = ops.host_load(x_h2d, "x")
 
-        W_data = np.random.normal(0, 0.1, _INPUT_SHAPE).astype(np.float32)
-        b_data = np.zeros(16, dtype=np.float32)
+        out_features = x.shape[-1]
+        weight_shape = (out_features, out_features)
+        bias_shape = (out_features, )
+        W_data = np.random.normal(0, 0.1, weight_shape).astype(np.float32)
+        b_data = np.zeros(bias_shape, dtype=np.float32)
 
         W = pir.variable(W_data, name="W")
         b = pir.variable(b_data, name="b")
 
-        ss = ScaleNShift()
-        ss_graph = ir.get_graph(ss, x, out_features=16)
+        lin = Linear()
+        lin_graph = ir.get_graph(lin, x, out_features=out_features)
 
-        y = ops.call(ss_graph, x, subgraph_in_to_parent_in={ss.W: W, ss.b: b})
+        y = ops.call(lin_graph,
+                     x,
+                     subgraph_in_to_parent_in={
+                         lin.W: W,
+                         lin.b: b
+                     })
 
         y_d2h = pir.d2h_stream(pir.float32, _INPUT_SHAPE, name="x_stream")
         ops.host_store(y_d2h, y)
@@ -72,12 +79,12 @@ def build_model() -> Tuple[_ir.Ir, pir.HostToDeviceStream, pir.
 def test_basic_subgraph():
     """
     Builds and executes the following (pseudocode):
-    x = ones(shape=(16, 16))
+    x = ones(shape=(2, 16))
     W = random_normal(shape=(16, 16))
     b = zeros(shape=(16,))
-    y = x * W + b
+    y = x @ W + b
 
-    Compute in numpy: y_expected = x.data() * W.data() + b.data()
+    Compute in numpy: y_expected = x.data() @ W.data() + b.data()
     assert y == y_expected
     """
     ir, x_h2d, y_d2h, W_data, b_data = build_model()
@@ -115,7 +122,7 @@ def test_basic_subgraph():
     session.weightsFromHost()
     session.run(stepio)
 
-    expected_y = np.multiply(x_data, W_data) + b_data
+    expected_y = np.matmul(x_data, W_data) + b_data
     y = anchors[y_id]
 
     assert y.shape == expected_y.shape
