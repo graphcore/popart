959aa02f65c2feb88a2a6d4253e94e5d463e179e
Matthew Haddock <matthewha@graphcore.ai>
2021-10-05 15:27:13 +0100

    Set the numpy seed for all popart.ir integration tests
    Summary: Set the seed on all tests in the integration/popart.ir/ folder.
    
    Test Plan: Run 20+ times with no issues. Printout visible when running milestone 1 test.
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, shirazb
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, shirazb
    
    Maniphest Tasks: T47140
    
    Differential Revision: https://phabricator.sourcevertex.net/D52801

---
+++ tests/integration/popart.ir/conftest.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+# See https://docs.pytest.org/en/6.2.x/fixture.html for details on this file.
+import pytest
+import numpy as np
+
+SEED = 0
+
+
+@pytest.fixture(autouse=True)
+def random_seed():
+    """Set the random seed for all tests in this directory. autouse=True will
+        use this fixture in every test.
+    """
+    print(f"Setting numpy seed to {SEED}")
+    np.random.seed(SEED)
