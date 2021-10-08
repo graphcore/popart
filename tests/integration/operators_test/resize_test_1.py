cda4cc1c438bf5735e024098cda4975f9fa1a47f
Matthew Haddock <matthewha@graphcore.ai>
2021-10-08 11:45:19 +0100

    Split resize test into two
    Summary: Test was very close to 1100s on ubuntu debug builds, see https://jenkins.sourcevertex.net/job/poplar/job/poplar_diff_ubuntu_18_04_debug/1893/ for example
    
    Test Plan: CI
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, george
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, george
    
    Subscribers: george
    
    Maniphest Tasks: T47389
    
    Differential Revision: https://phabricator.sourcevertex.net/D53039

---
+++ tests/integration/operators_test/resize_test_1.py
@@ -0,0 +1,290 @@
+# Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+import numpy as np
+import pytest
+import popart
+import torch
+import torch.nn.functional as F
+from op_tester import op_tester
+import test_util as tu
+from resize_test_0 import interpolate
+
+import onnx.backend.test.case.node.resize as onnx_resize
+
+
+def test_float16_scales(op_tester):
+    data = np.random.rand(1, 1, 2, 2).astype(np.float32)
+
+    scales = np.array([1.0, 1.0, 2.0, 3.0], dtype=np.float16)
+
+    def init_builder(builder):
+        d = builder.addInputTensor(data)
+        s = builder.aiOnnx.constant(scales)
+        o = builder.aiOnnx.resize([d, s])
+        builder.addOutputTensor(o)
+        return [o]
+
+    def reference(ref_data):
+        x = torch.tensor(data)
+        s = [i for i in scales[2:]]
+        o = interpolate(x, s)
+        return [o]
+
+    op_tester.run(init_builder, reference, 'infer')
+
+
+def test_resize11_float16_scales(op_tester):
+    data = np.random.rand(1, 1, 2, 2).astype(np.float32)
+
+    roi = np.array([], dtype=np.float32)
+    scales = np.array([1.0, 1.0, 2.0, 3.0], dtype=np.float16)
+
+    def init_builder(builder):
+        d = builder.addInputTensor(data)
+        r = builder.aiOnnxOpset11.constant(roi, False)
+        s = builder.aiOnnxOpset11.constant(scales)
+        o = builder.aiOnnxOpset11.resize([d, r, s])
+        builder.addOutputTensor(o)
+        return [o]
+
+    def reference(ref_data):
+        x = torch.tensor(data)
+        s = [i for i in scales[2:]]
+        o = interpolate(x, s)
+        return [o]
+
+    op_tester.run(init_builder, reference, 'infer')
+
+
+@pytest.mark.parametrize("scale_factor, data_shape", [(3.0001, 2), (0.51, 6)])
+@pytest.mark.parametrize("nearest_mode", ["round_prefer_floor", "pytorch"])
+def test_odd_scale_factors(op_tester, nearest_mode, scale_factor, data_shape):
+    data = np.random.rand(1, 1, data_shape).astype(np.float32)
+    scales = np.array([1.0, 1.0, scale_factor], dtype=np.float32)
+
+    def init_builder(builder):
+        d = builder.addInputTensor(data)
+        s = builder.aiOnnx.constant(scales)
+        o = builder.aiOnnx.resize([d, s])
+        builder.addNodeAttribute("nearest_mode", nearest_mode, {o})
+        builder.addOutputTensor(o)
+        return [o]
+
+    def reference(ref_data):
+        if nearest_mode == "pytorch":
+            x = torch.tensor(data)
+            s = [i for i in scales[2:]]
+            o = interpolate(x, s)
+            return [o]
+        else:
+            o = onnx_resize.interpolate_nd(data,
+                                           onnx_resize.nearest_coeffs,
+                                           scale_factors=scales)
+            return [o.astype(data.dtype)]
+
+    op_tester.run(init_builder, reference, 'infer')
+
+
+@pytest.mark.parametrize("scale_factor, data_shape", [(3.0001, 2), (0.51, 6)])
+def test_odd_scale_factors_grad(op_tester, scale_factor, data_shape):
+    data = np.random.rand(1, 1, data_shape).astype(np.float32)
+
+    scales = np.array([1.0, 1.0, scale_factor], dtype=np.float32)
+
+    x_data_shape = [int(i * j) for i, j in zip(data.shape, scales)]
+    x_data = np.random.rand(*x_data_shape).astype(np.float32)
+
+    def init_builder(builder):
+        d = builder.addInputTensor(data)
+        x = builder.addInputTensor(x_data)
+        s = builder.aiOnnx.constant(scales)
+        o = builder.aiOnnx.resize([d, s])
+        o = builder.aiOnnx.mul([o, x])
+        builder.addOutputTensor(o)
+        return [
+            o,
+            popart.reservedGradientPrefix() + d,
+            popart.reservedGradientPrefix() + o,
+        ]
+
+    def reference(ref_data):
+        a = torch.tensor(data, requires_grad=True)
+        s = [i for i in scales[2:]]
+        b = interpolate(a, s)
+        b.retain_grad()
+        o = b * torch.tensor(x_data)
+
+        d__o = ref_data.getOutputTensorGrad(0)
+        o.backward(torch.tensor(d__o))
+        return [o, a.grad, None]
+
+    op_tester.setPatterns(['MulArgGradOp'], enableRuntimeAsserts=False)
+    op_tester.run(init_builder, reference, 'train')
+
+
+@pytest.mark.parametrize(
+    "data_shape, scales",
+    [
+        # This changes the values without changing the size of the dimension.
+        ([8], [1.12]),
+        # upsample
+        ([2], [8.0]),
+        ([2, 2], [2.0, 3.0]),
+        ([2, 2], [2.5, 2.5]),
+        ([3, 2], [2.5, 2.5]),
+        ([5, 3], [2.3, 2.5]),
+        # downsample
+        ([2, 4], [0.5, 0.5]),
+        ([2, 8], [1.0, 3 / 8]),
+        ([5, 4], [0.5, 0.5]),
+        ([5, 3], [0.3, 0.5]),
+        # 3D
+        ([5, 3, 4], [2.3, 2.5, 3.0]),
+        ([5, 3, 4], [2.3, 2.5, 0.5]),
+    ])
+@pytest.mark.parametrize(
+    "coordinate_transformation_mode",
+    ["half_pixel", "pytorch_half_pixel", "asymmetric", "align_corners"])
+def test_resize_linear(op_tester, data_shape, scales,
+                       coordinate_transformation_mode):
+    data = np.random.rand(*data_shape).astype(np.float32)
+    roi = np.array([], dtype=np.float32)
+    scales = np.array(scales, dtype=np.float32)
+
+    def init_builder(builder):
+        d = builder.addInputTensor(data)
+        s = builder.aiOnnxOpset11.constant(scales, False)
+        r = builder.aiOnnxOpset11.constant(roi, False)
+        o = builder.aiOnnxOpset11.resize(
+            [d, r, s],
+            mode="linear",
+            coordinate_transformation_mode=coordinate_transformation_mode)
+        builder.addOutputTensor(o)
+        return [o]
+
+    def reference(ref_data):
+        o = onnx_resize.interpolate_nd(
+            data,
+            onnx_resize.linear_coeffs,
+            scale_factors=scales,
+            coordinate_transformation_mode=coordinate_transformation_mode)
+        return [o.astype(data.dtype)]
+
+    op_tester.run(init_builder, reference, 'infer')
+
+
+@pytest.mark.parametrize(
+    "data_shape, sizes",
+    [
+        # What happens if the shape is unchanged?
+        ([4], [4]),
+        # Some 1D cases
+        ([4], [8]),
+        ([8], [4]),
+        ([8], [3]),
+        # 2D cases
+        ([4, 4], [1, 2]),
+        ([4, 4], [9, 9]),
+        ([4, 4], [5, 3]),
+    ])
+@pytest.mark.parametrize("sizes_dtype", [np.int64, np.int32])
+def test_resize_sizes_input(op_tester, data_shape, sizes, sizes_dtype):
+    data = np.random.rand(*data_shape).astype(np.float32)
+    roi = np.array([], dtype=np.float32)
+    sizes = np.array(sizes, dtype=sizes_dtype)
+
+    def init_builder(builder):
+        d = builder.addInputTensor(data)
+        s = builder.aiOnnxOpset11.constant(sizes, False)
+        r = builder.aiOnnxOpset11.constant(roi, False)
+        o = builder.aiOnnxOpset11.resize([d, r, '', s])
+        builder.addOutputTensor(o)
+        return [o]
+
+    def reference(ref_data):
+        # Convert sizes to scales.
+        scales = []
+        assert len(data.shape) == len(sizes)
+        for di, do in zip(data.shape, sizes):
+            scales.append(do / di)
+
+        o = onnx_resize.interpolate_nd(data,
+                                       onnx_resize.nearest_coeffs,
+                                       scale_factors=scales)
+        # Check the output shape is correct.
+        assert tuple(sizes) == o.shape
+
+        return [o.astype(data.dtype)]
+
+    op_tester.run(init_builder, reference, 'infer')
+
+
+@pytest.mark.parametrize(
+    "data_shape, scales",
+    [
+        # This changes the values without changing the size of the dimension.
+        ([8], [1.12]),
+        # upsample
+        ([2], [8.0]),
+        ([2, 2], [2.0, 2.0]),
+        ([2, 2], [2.0, 3.0]),
+        ([2, 2], [2.5, 2.5]),
+        ([3, 2], [2.5, 2.5]),
+        ([5, 3], [2.3, 2.5]),
+        # downsample
+        ([2, 4], [0.5, 0.5]),
+        ([2, 8], [1.0, 3 / 8]),
+        ([5, 4], [0.5, 0.5]),
+        ([5, 3], [0.3, 0.5]),
+        # 3D
+        ([5, 3, 4], [2.3, 2.5, 3.0]),
+        ([5, 3, 4], [2.3, 2.5, 0.5]),
+    ])
+@pytest.mark.parametrize(
+    "coordinate_transformation_mode",
+    ["half_pixel", "pytorch_half_pixel", "asymmetric", "align_corners"])
+def test_resize_cubic(op_tester, data_shape, scales,
+                      coordinate_transformation_mode):
+    # This particular case has quite a large error.
+    #   Popart:
+    #   [[0.64254135 0.5632834 ]]
+    #   Reference:
+    #   [[0.6508137 0.5556082]]
+    #   Diff:
+    #   [[-0.00827235  0.00767517]]
+    # I'm not sure what is causing it, but it's the only failure in 52 tests, so skipping it for now.
+    if coordinate_transformation_mode == "pytorch_half_pixel" and data_shape == [
+            2, 4
+    ] and scales == [0.5, 0.5]:
+        pytest.skip()
+
+    data = np.random.rand(*data_shape).astype(np.float32)
+    roi = np.array([], dtype=np.float32)
+    scales = np.array(scales, dtype=np.float32)
+
+    def init_builder(builder):
+        d = builder.addInputTensor(data)
+        s = builder.aiOnnxOpset11.constant(scales, False)
+        r = builder.aiOnnxOpset11.constant(roi, False)
+        o = builder.aiOnnxOpset11.resize(
+            [d, r, s],
+            mode="cubic",
+            coordinate_transformation_mode=coordinate_transformation_mode)
+        builder.addOutputTensor(o)
+        return [o]
+
+    def reference(ref_data):
+        o = onnx_resize.interpolate_nd(
+            data,
+            onnx_resize.cubic_coeffs,
+            scale_factors=scales,
+            coordinate_transformation_mode=coordinate_transformation_mode)
+
+        return [o.astype(data.dtype)]
+
+    # The more dimensions we introduce, the more floating point error we get.
+    if len(data.shape) == 2:
+        op_tester.atol = 1e-06
+    elif len(data.shape) == 3:
+        op_tester.atol = 1e-05
+
+    op_tester.run(init_builder, reference, 'infer')
