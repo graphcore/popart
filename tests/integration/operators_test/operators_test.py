50cf927bb2b70feadae11cb0f153e7d623169a8f
Hatem Helal <hatemh@graphcore.ai>
2021-10-15 20:16:47 +0100

    Revert "Change scatter opx to use popops::multiUpdate with slice planning"
    This reverts commit 926f087a49191c2b4bec20fd273bf76061f04763.

--- tests/integration/operators_test/operators_test.py
+++ tests/integration/operators_test/operators_test.py
@@ -1932,6 +1932,98 @@ def test_pad_grad(op_tester):
     op_tester.run(init_builder, reference, 'train')
 
 
+def test_scatter_0(op_tester):
+    data = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0],
+                     [0.0, 0.0, 0.0]]).astype(np.float32)
+    indices = np.array([[1, 0, 2], [0, 2, 1]]).astype(np.int32)
+    updates = np.array([[-1.0, -1.1, -1.2], [2.0, 2.1,
+                                             2.2]]).astype(np.float32)
+    output = np.array([[2.0, -1.1, 0.0], [-1.0, 0.0, 2.2],
+                       [0.0, 2.1, -1.2]]).astype(np.float32)
+    axis = 0
+
+    def init_builder(builder):
+        i1 = builder.addInputTensor(data)
+        i2 = builder.addInputTensor(indices)
+        i3 = builder.addInputTensor(updates)
+        o = builder.aiOnnx.scatter([i1, i2, i3], axis)
+        builder.addOutputTensor(o)
+        return [
+            o,
+            popart.reservedGradientPrefix() + i1,
+            popart.reservedGradientPrefix() + i3
+        ]
+
+    def reference(ref_data):
+        data_grad = np.array([[0.0, 0.0, 1.0], [0.0, 1.0, 0.0],
+                              [1.0, 0.0, 0.0]]).astype(np.float32)
+        return [output, data_grad, np.ones_like(updates)]
+
+    op_tester.lossReduction = popart.ReductionType.Sum
+    op_tester.setPatterns(['PreUniRepl'], enableRuntimeAsserts=False)
+    op_tester.run(init_builder, reference, 'train')
+
+
+def test_scatter_1(op_tester):
+    data = np.array([[1.0, 2.0, 3.0, 4.0, 5.0]]).astype(np.float32)
+    indices = np.array([[1, 3]]).astype(np.int32)
+    updates = np.array([[-1.1, 2.1]]).astype(np.float32)
+    output = np.array([[1.0, -1.1, 3.0, 2.1, 5.0]]).astype(np.float32)
+    d_data = np.array([[1.0, 0, 1.0, 0, 1.0]]).astype(np.float32)
+    axis = 1
+
+    def init_builder(builder):
+        i1 = builder.addInputTensor(data)
+        i2 = builder.addInputTensor(indices)
+        i3 = builder.addInputTensor(updates)
+        o = builder.aiOnnx.scatter([i1, i2, i3], axis)
+        builder.addOutputTensor(o)
+        return [
+            o,
+            popart.reservedGradientPrefix() + i1,
+            popart.reservedGradientPrefix() + i3
+        ]
+
+    def reference(ref_data):
+        return [output, d_data, np.ones_like(updates)]
+
+    op_tester.lossReduction = popart.ReductionType.Sum
+    op_tester.setPatterns(['PreUniRepl'], enableRuntimeAsserts=False)
+    op_tester.run(init_builder, reference, 'train')
+
+
+def test_scatter_2(op_tester):
+    data = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0],
+                     [0.0, 0.0, 0.0]]).astype(np.float32)
+    indices = np.array([[1, 0, 2], [0, 2, 1]]).astype(np.int32)
+    updates = np.array([[-1.0, -1.1, -1.2], [2.0, 2.1,
+                                             2.2]]).astype(np.float32)
+    output = np.array([[-1.1, -1, -1.2], [2, 2.2, 2.1],
+                       [0.0, 0.0, 0.0]]).astype(np.float32)
+    axis = 1
+
+    def init_builder(builder):
+        i1 = builder.addInputTensor(data)
+        i2 = builder.addInputTensor(indices)
+        i3 = builder.addInputTensor(updates)
+        o = builder.aiOnnx.scatter([i1, i2, i3], axis)
+        builder.addOutputTensor(o)
+        return [
+            o,
+            popart.reservedGradientPrefix() + i1,
+            popart.reservedGradientPrefix() + i3
+        ]
+
+    def reference(ref_data):
+        data_grad = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0],
+                              [1.0, 1.0, 1.0]]).astype(np.float32)
+        return [output, data_grad, np.ones_like(updates)]
+
+    op_tester.lossReduction = popart.ReductionType.Sum
+    op_tester.setPatterns(['PreUniRepl'], enableRuntimeAsserts=False)
+    op_tester.run(init_builder, reference, 'train')
+
+
 def test_shape(op_tester):
     d1 = np.random.rand(2, 4, 3).astype(np.float32)
     d2 = np.zeros((4, 6), dtype=np.float32)
--- willow/include/popart/popx/op/sliceplanx.hpp
+++ willow/include/popart/popx/op/sliceplanx.hpp
@@ -14,22 +14,13 @@ class Graph;
 namespace popart {
 namespace popx {
 
-enum class SlicePlanUsedFor { Slice, Update, UpdateAdd };
+popops::SlicePlan
+createSlicePlan(const snap::Graph &graph,
+                const popart::TensorInfo &dataInfo,
+                const popart::TensorInfo &indicesInfo,
+                nonstd::optional<float> availableMemoryProportion = {},
+                nonstd::optional<size_t> axis                     = {});
 
-poplar::OptionFlags
-createSlicePlanOptions(SlicePlanUsedFor usedFor,
-                       nonstd::optional<float> availableMemoryProportion = {});
-
-popops::SlicePlan createSlicePlan(const snap::Graph &graph,
-                                  const popart::TensorInfo &dataInfo,
-                                  const popart::TensorInfo &indicesInfo,
-                                  const poplar::OptionFlags &options,
-                                  nonstd::optional<size_t> axis = {});
-
-// Align input to have same axes alignment and shape as popart IR.
-snap::Tensor alignToAxis(const snap::Tensor &input,
-                         const popart::Shape &shape,
-                         unsigned int axis);
 } // namespace popx
 } // namespace popart
 
