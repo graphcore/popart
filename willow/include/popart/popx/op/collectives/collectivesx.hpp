3c2bd2ea42571b37c8179c9c76dccb70ae38b1e9
Fabian Tschopp <fabiant@graphcore.ai>
2021-10-29 16:46:45 +0200

    Document getCollectiveLinkedGroup
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, michaell, markk
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, michaell
    
    Maniphest Tasks: T48809
    
    Differential Revision: https://phabricator.sourcevertex.net/D54397

--- willow/include/popart/popx/op/collectives/collectivesx.hpp
+++ willow/include/popart/popx/op/collectives/collectivesx.hpp
@@ -93,11 +93,79 @@ private:
 class CollectivesBaseOpx : public PopOpx {
 public:
   CollectivesBaseOpx(Op *, Devicex *);
-  // Return all linked tensors and their connected ops to coordinate tensor
-  // mapping of collective inputs and outputs
+
+  /**
+   * Function to determine which \a collective Ops need to be in the same
+   * collective linked group.
+   *
+   * Ops in the same \a collective linked group need to use the same
+   * collective balanced reorder to ensure tensor layouts of tensors that
+   * interact with each other in the graph, are compatible.
+   *
+   *  Scenarios leading to \a collective Ops belonging to the same group:
+   *
+   *  1. The \c CollectivesBaseOp::getCollectiveLinkedIndex()
+   *     is connected to the same root tensor (i.e. tensor A connects to the
+   *     \c getCollectiveLinkedIndex of a \c ReduceScatter and \c AllGather,
+   *     directly or indirectly):
+   *
+   *     A -> ReduceScatter
+   *      \-> IdentiyOp -> AllGather
+   *
+   *  2. The \a RTS enabled input/output tensors of \a RTS enabled collective
+   *     operations meet in the compute graph:
+   *
+   *     B -> ReduceScatter -> C -> AllGather -> F -> ReduceScatter -> G
+   *                            \
+   *                             VarUpdateOp
+   *                            /
+   *     D -> ReduceScatter -> E
+   *
+   *     C, E and the VarUpdateOp in this graph are replicated tensor sharded
+   *     (\a RTS) and therefore, both ReduceScatter Ops and the AllGather Op
+   *     end up in the same \a collective linked group.
+   *     B, D, F, G are not sharded, and therefore, the ReduceScatter between
+   *     F and G can be in a different \a collective linked group.
+   *
+   * The primary motivation for \a collective linked groups is "folding"
+   * multiple RTS tensors together via e.g. \a outlining.
+   * Folding in this context is when two operations or tensors that were unique
+   * now use the same code or memory, which implies that for example tensor
+   * layouts need to be identical too.
+   * If the graph has 3 \a RTS enabled variables, for example, and 2 of them
+   * use the same \c VarUpdateOp due to outlining, this implies that we need to
+   * ensure all \a RTS related Ops connected to those 2 variables use identical
+   * \a CBR (collective balanced reorder) rearrangement.
+   *
+   * \a CBR is set in the collective Ops themselves either during
+   * \c Opx::unwindTensorLayout, \c Opx:createInputTensor or \c Opx::grow
+   * by calling \c createCollectiveBalancedReorder
+   *
+   * The third variable would use a separate VarUpdateOp, and therefore is in a
+   * separate \a collective linked group, and can instantiate it's own \a CBR,
+   * even if the tensor shapes matches.
+   *
+   * \c getCollectiveLinkedGroup uses Ops that introduce \a RTS/CBR as a
+   * starting point (\c ReduceScatter & \c AllGather) and tracks all associated
+   * Ops that propagate \a RTS with a \a DFS search on the graph.
+   *
+   * \return  Returns all linked tensors and their connected ops to coordinate
+   *          tensor mapping of collective inputs and outputs
+   */
   std::pair<std::set<TensorId>, std::vector<Op *>>
   getCollectiveLinkedGroup() const;
+
+  /**
+   * Get the existing \a CBR
+   * \return Existing CBR for the input/output tensor of the collective Op
+   */
   gcl::CollectiveBalancedReorder *getCollectiveBalancedReorder() const;
+
+  /**
+   * Create a new \a CBR instance for the reference \c tensor
+   * \param tensor non-sharded reference tensor
+   * \return New CBR for the input/output tensor of the collective Op
+   */
   gcl::CollectiveBalancedReorder *
   createCollectiveBalancedReorder(snap::Tensor tensor) const;
 };
