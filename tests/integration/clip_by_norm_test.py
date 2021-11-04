Fixed gradient clipping by norm
Summary:
In some cases, gradient tensor has more than one consumer, and one of
the consumers is also used as input to other gradient computations.
Therefore, the gradient tensor must not be replaced on all consumers,
but only on the ones that are relevant for the current gradient that is
being clipped. Otherwise, gradient clipping creates a cycle by
connecting its output to input of another op that dominates the gradient
compuatation of another gradient.

For a simple model that does: `a + b * X`, and optimizer is SGD with
gradient clipping:

                   loss
                t0 /  \
           X --> Mul   \t0
              t1 /      \
    SGDVarUpdate (b)   SGDVarUpdate (a)

Clipping gradient on tensor `t0` between `loss` and `SGDVarUpdate (b)`
must not insert a gradient clipping subgraph between `loss` and `Mul`,
because the left side isn't related to the right side's gradient
clipping.

Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, fabiant, george

Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, george

Maniphest Tasks: T49367

Differential Revision: https://phabricator.sourcevertex.net/D54905

tests/integration/clip_by_norm_test.py
@@ -472,31 +472,39 @@ def test_serialized_matmul(optimizerType):
 
 def test_clipping_all_weights():
     input_channels = 6
-    reducing_dim = 2
-    output_channels = 4
+    np.random.seed(0)
 
-    lhs_shape = [input_channels, reducing_dim]
-    rhs_shape = [reducing_dim, output_channels]
-    lhs_data = np.ones((*lhs_shape, ), dtype=np.float32)
-    rhs_data = np.ones((*rhs_shape, ), dtype=np.float32)
+    a_shape = [input_channels]
+    b_shape = [input_channels]
+    a_data = np.ones((*a_shape, ), dtype=np.float32)
+    b_data = np.ones((*b_shape, ), dtype=np.float32)
+    X_data = np.random.rand(*b_shape).astype(np.float32)
+    target_data = np.random.rand(*b_shape).astype(np.float32)
 
     builder = popart.Builder()
 
-    lhs = builder.addInitializedInputTensor(lhs_data, "lhs")
-    rhs = builder.addInitializedInputTensor(rhs_data, "rhs")
+    a = builder.addInitializedInputTensor(a_data, "a")
+    b = builder.addInitializedInputTensor(b_data, "b")
+    X = builder.addInputTensor(popart.TensorInfo(X_data))
+    target = builder.addInputTensor(popart.TensorInfo(target_data))
 
-    o = builder.aiOnnx.matmul([lhs, rhs])
+    # a + b * X
+    out = builder.aiOnnx.add([a, builder.aiOnnx.mul([b, X])])
+    # MSE loss
+    sub = builder.aiOnnx.sub([target, out])
+    square = builder.aiOnnx.mul([sub, sub])
+    loss = builder.aiGraphcore.identityloss(
+        [square], reduction=popart.ReductionType.Mean)
 
-    loss = builder.aiGraphcore.l1loss([o], 0.1)
+    builder.addOutputTensor(out)
+    builder.addOutputTensor(loss)
 
     proto = builder.getModelProto()
 
     dataFlow = popart.DataFlow(
         1, {
-            o: popart.AnchorReturnType("All"),
-            rhs: popart.AnchorReturnType("Final"),
-            popart.reservedGradientPrefix() + lhs:
-            popart.AnchorReturnType("All"),
+            out: popart.AnchorReturnType("Final"),
+            loss: popart.AnchorReturnType("Final")
         })
 
     opts = popart.SessionOptions()