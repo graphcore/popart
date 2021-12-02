2aa4dde1261477232b5dd1f3f10908d3e5ca1c3b
Erik Newton <erikn@graphcore.ai>
2021-12-02 09:57:25 +0000

    Created sections for documenting limitations and added current Clip-11 limitation.
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #tech_docs, markk, jamiep
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #tech_docs, markk, jamiep
    
    Subscribers: jamiep
    
    Maniphest Tasks: T49610
    
    Differential Revision: https://phabricator.sourcevertex.net/D56664

--- docs/gen_supported_ops.py
+++ docs/gen_supported_ops.py
@@ -8,8 +8,6 @@ if len(sys.argv) < 3:
         "gen_supported_ops.py <path of popart python install> <output file>")
 
 print("Looking for popart module in " + sys.argv[1])
-print("Writing supported ops to " + sys.argv[2])
-
 for p in sys.argv[1].split(':'):
     sys.path.append(p)
 import popart
@@ -20,12 +18,14 @@ ops = dict([[x, []] for x in set([x.domain for x in supported_ops])])
 for op in supported_ops:
     ops[op.domain].append({"type": op.type, "version": op.version})
 
+print("Writing supported ops to " + sys.argv[2])
 with open(sys.argv[2], "w") as f:
     for domain in ops:
         print("Domain: " + domain, file=f)
-        print("~~~~~~~~~~~~~~\n", file=f)
+        print('-' * (8 + len(domain)), file=f)
+        print("", file=f)
 
         for op in ops[domain]:
-            print("- " + op["type"] + "-" + str(op["version"]), file=f)
+            print(f"- {op['type']}-{op['version']}", file=f)
 
         print("", file=f)
--- docs/user_guide/supported_ops.rst
+++ docs/user_guide/supported_ops.rst
@@ -1,7 +1,7 @@
 .. _popart_supported_ops:
 
 Supported operators
--------------------
+===================
 
 PopART is compatible with ONNX versions up to and including 1.6.
 (see `ONNX Versioning <https://github.com/onnx/onnx/blob/master/docs/Versioning.md>`_).
@@ -13,3 +13,18 @@ See `ONNX Operators <https://github.com/onnx/onnx/blob/master/docs/Operators.md>
 for more information.
 
 .. include:: ../supported_ops_gen.rst
+
+Limitations
+-----------
+.. warning::
+     The information provided in this section is incomplete.
+
+Limitations of ai.onnx operators
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Clip-11: Does not support variable min/max input parameters. The parameters
+  must contain a value at model initialisation and any run-time changes to
+  these parameters will not be read by the model.
+
+Limitations of ai.graphcore operators
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+There are no known limitations.
