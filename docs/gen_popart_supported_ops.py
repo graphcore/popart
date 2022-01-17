f595b7ab370c6ab75f77d59f253c76cccfc65d47
Mark Kattenbelt <markk@graphcore.ai>
2022-01-17 18:06:35 +0000

    Replace gcdocs with sphinx_resources
    Summary:
    This diff replaces use of `gcdocs` with `sphinx_resources`. This is so that:
    
    * Our local and CI doc builds match those that are manually created for public consumption.
    * We can now test features of `sphinx_resources` locally or on CI and use them with confidence (for example using intersphinx: https://phabricator.sourcevertex.net/T38382#883217)
    
    Changes include:
    
    * Re-write of `gen_docs.sh.in` to explicitly make `sphinx-build` calls to generate html/pdf docs.
    * Make the virtual environment once, rather than everytime `gen_docs.sh.in` (this limitation meant `gen_docs.sh` could only be used for one documentation item as it failed to create the virtual env multiple times in the same place while in use in a parallel build, meaning we couldn't use automodule in some docs).
    * Add some a CMake function to `popart/docs/CMakeLists.txt` to make it more DRY.
    * Remove dependency on `pdfgen` as it was causing issues (not sure we needed this).
    * Added the entire source dir as a dependency for all doc items, so they get rebuilt whenever any sources changes. This is a little drastic but better than the alternative (not rebuilding the docs unless you completely rebuild everything).
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #tech_docs, jamiep, matthewha
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #tech_docs, jamiep, matthewha
    
    Subscribers: matthewha, yanz, jamiep
    
    Maniphest Tasks: T43714
    
    Differential Revision: https://phabricator.sourcevertex.net/D59046

--- docs/gen_popart_supported_ops.py
+++ docs/gen_popart_supported_ops.py
@@ -3,13 +3,9 @@
 
 import sys
 
-if len(sys.argv) < 3:
-    sys.exit(
-        "gen_supported_ops.py <path of popart python install> <output file>")
+if len(sys.argv) < 2:
+    sys.exit("gen_popart_supported_ops.py <output file>")
 
-print("Looking for popart module in " + sys.argv[1])
-for p in sys.argv[1].split(':'):
-    sys.path.append(p)
 import popart
 
 supported_ops = popart.getSupportedOperations(False)
@@ -18,8 +14,8 @@ ops = dict([[x, []] for x in set([x.domain for x in supported_ops])])
 for op in supported_ops:
     ops[op.domain].append({"type": op.type, "version": op.version})
 
-print("Writing supported ops to " + sys.argv[2])
-with open(sys.argv[2], "w") as f:
+print("Writing supported ops to " + sys.argv[1])
+with open(sys.argv[1], "w") as f:
     for domain in ops:
         print("Domain: " + domain, file=f)
         print('-' * (8 + len(domain)), file=f)
