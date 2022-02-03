78bab6d72268416fb6628661c393c3f38f159591
evang <evan.griffiths@graphcore.ai>
2022-02-03 13:13:39 +0000

    Add documentation for the ExplicitRecompute class
    Summary: Fixes T55412
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, zigmasb, michaell, fabiant
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, fabiant
    
    Maniphest Tasks: T55412
    
    Differential Revision: https://phabricator.sourcevertex.net/D60245

--- willow/include/popart/transforms/explicitrecompute.hpp
+++ willow/include/popart/transforms/explicitrecompute.hpp
@@ -7,6 +7,51 @@
 
 namespace popart {
 
+// Consider a fragment of the training graph before the explicit recomputation
+// transform:
+//
+// CheckpointOp0
+//     |
+// RecomputeOp0
+//     |
+// RecomputeOp1 -.     ...
+//     |          \     |
+// CheckpointOp1    CheckpointOp1Grad
+//    ...              ...
+//     |                |
+//   Loss --------------
+//
+// (where CheckpointOp* is an op with
+// op->settings.recomputeType == RecomputeType::Checkpoint
+// and RecomputeOp* is an op with
+// op->settings.recomputeType == RecomputeType::Recompute)
+//
+// By marking these ops as 'recompute', the output of RecomputeOp1 does not
+// need to remain live until the recomputation of CheckpointOp1Grad. In other
+// words, the memory used to store this tensor is freed for allocation of other
+// tensors as soon as RecomputeOp1's output is read during the computation of
+// CheckpointOp1. How does this work in practice?
+//
+// After the transform, the graph fragment will look like:
+//
+// CheckpointOp0 -.
+//     |           \
+// RecomputeOp0   RecomputeOp0Clone
+//     |                  |
+// RecomputeOp1   RecomputeOp1Clone      ...
+//     |                  |               |
+// CheckpointOp1           ----- CheckpointOp1Grad
+//    ...                                ...
+//     |                                  |
+//   Loss --------------------------------
+//
+// The alternative, in the case of implicit recomputation, is to not transform
+// the graph at the IR level, and to use these recomputation settings to affect
+// the Ir lowering. In this case, the `poplar::program::Sequence`s that
+// correspond to the lowered RecomputeOps are added once to the main program as
+// scheduled in the forward pass, and then again directly preceding the
+// `poplar::program::Sequence` of the CheckpointOp1Grad. See the
+// `FindRequiredRecomputes class in irlowering.cpp
 class ExplicitRecompute : public Transform {
 public:
   static std::size_t id();
