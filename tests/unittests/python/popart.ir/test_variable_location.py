f7000a6f5b6ad08d16c408d419d573f12c270dc2
yanz <yanz@graphcore.ai>
2022-02-23 18:09:25 +0000

    Refactor RTS + Remote variables
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, oskarb, matthewha
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, matthewha
    
    Subscribers: matthewha
    
    Maniphest Tasks: T55738
    
    Differential Revision: https://phabricator.sourcevertex.net/D60613

--- tests/unittests/python/popart.ir/test_variable_location.py
+++ tests/unittests/python/popart.ir/test_variable_location.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import numpy as np
 
 import popart.ir as pir
+import popart.ir.dtypes as dtypes
 import popart.ir.ops as ops
 
 # `import test_util` requires adding to sys.path
@@ -18,22 +19,21 @@ def test_remote_variable():
     main = ir.main_graph
 
     with main:
-        x = pir.variable(1, name="x")
-        buffer = pir.RemoteBuffer(ir, x.shape, x.dtype, 1)
+        x = np.array(1).astype(np.int32)
+        buffer = pir.RemoteBuffer(ir, x.shape, dtypes.int32, 1)
         remote_x = pir.remote_variable(x, buffer, 0)
 
         y = pir.variable(2)
 
-        loaded_x = ops.remote_load(buffer, remote_x)
-
+        loaded_x = ops.remote_load(buffer, 0)
         updated_x = loaded_x + y
 
-        ops.remote_store(buffer, remote_x, updated_x)
+        ops.remote_store(buffer, 0, updated_x)
 
-        y_d2h = pir.d2h_stream(x.shape, x.dtype)
+        y_d2h = pir.d2h_stream(updated_x.shape, updated_x.dtype)
         ops.host_store(y_d2h, updated_x)
 
-    result, stored = run(ir, y_d2h, x)
+    result, stored = run(ir, y_d2h, remote_x)
     assert result == 3
     assert stored == 3
 
@@ -46,23 +46,22 @@ def test_remote_variable_replica_not_sharded():
     main = ir.main_graph
 
     with main:
-        x = pir.variable(1, name="x")
-        buffer = pir.RemoteBuffer(ir, x.shape, x.dtype, 1)
+        x = np.array(1).astype(np.int32)
+        buffer = pir.RemoteBuffer(ir, x.shape, dtypes.int32, 1)
         remote_x = pir.remote_variable(x, buffer, 0)
 
         y = pir.variable(2, name="y")
 
-        loaded_x = ops.remote_load(buffer,
-                                   remote_x)  # load x from remote memory
+        loaded_x = ops.remote_load(buffer, 0)
 
         updated_x = loaded_x + y  # add x and y
 
-        ops.remote_store(buffer, remote_x, updated_x)  # remote store x + y
+        ops.remote_store(buffer, 0, updated_x)
 
-        updated_x_d2h = pir.d2h_stream(updated_x.shape, updated_x.dtype)
-        ops.host_store(updated_x_d2h, updated_x)
+        y_d2h = pir.d2h_stream(updated_x.shape, updated_x.dtype)
+        ops.host_store(y_d2h, updated_x)
 
-    result, stored = run(ir, updated_x_d2h, x)
+    result, stored = run(ir, y_d2h, remote_x)
     np.testing.assert_equal(result, [3, 3])
     np.testing.assert_equal(stored, [3, 3])
 
@@ -75,14 +74,14 @@ def test_remote_replia_sharded_variable_gather():
     main = ir.main_graph
 
     with main:
-        x = pir.variable([1, 2], name="x")
-        buffer = pir.RemoteBuffer(ir, (x.nelms // 2, ), x.dtype, 1)
+        x = np.array([1, 2]).astype(np.int32)
+        buffer = pir.RemoteBuffer(ir, (x.size // 2, ), dtypes.int32, 1)
         remote_x = pir.remote_replica_sharded_variable(x, buffer, 0)
 
         y = pir.variable([3, 4], name="y")
 
         # loaded_x.shape = (1,)
-        loaded_x = ops.remote_load(buffer, remote_x)
+        loaded_x = ops.remote_load(buffer, 0)
         # full_x.shape = (2,)
         full_x = ops.collectives.replicated_all_gather(loaded_x)
 
@@ -90,12 +89,12 @@ def test_remote_replia_sharded_variable_gather():
 
         updated_shard = ops.collectives.replicated_reduce_scatter(
             updated_x, 'local', None, True)
-        ops.remote_store(buffer, remote_x, updated_shard)
+        ops.remote_store(buffer, 0, updated_shard)
 
         y_d2h = pir.d2h_stream(updated_x.shape, updated_x.dtype)
         ops.host_store(y_d2h, updated_x)
 
-    result, stored = run(ir, y_d2h, x)
+    result, stored = run(ir, y_d2h, remote_x)
     np.testing.assert_equal(result, [[4, 6], [4, 6]])
     np.testing.assert_equal(stored, [4, 6])
 
@@ -110,9 +109,8 @@ def test_replia_sharded_variable_gather():
     main = ir.main_graph
 
     with main:
-        x = pir.variable([1, 2], name="x")
-        buffer = pir.RemoteBuffer(ir, (x.nelms // 2, ), x.dtype, 1)
-        remote_x, loaded_x = pir.replica_sharded_variable(x, buffer, 0)
+        x = np.array([1, 2]).astype(np.int32)
+        remote_x, loaded_x = pir.replica_sharded_variable(x, dtypes.int32)
 
         y = pir.variable([3, 4])
 
@@ -129,7 +127,7 @@ def test_replia_sharded_variable_gather():
         y_d2h = pir.d2h_stream(updated_x.shape, updated_x.dtype)
         ops.host_store(y_d2h, updated_x)
 
-    result, stored = run(ir, y_d2h, x)
+    result, stored = run(ir, y_d2h, remote_x)
     np.testing.assert_equal(result, [[4, 6], [4, 6]])
     np.testing.assert_equal(stored, [4, 6])
 
@@ -145,9 +143,8 @@ def test_replica_sharded_variable_no_gather():
     main = ir.main_graph
 
     with main:
-        x = pir.variable([1, 2], name="x")
-        buffer = pir.RemoteBuffer(ir, (x.nelms // 2, ), x.dtype, 1)
-        remote_x, loaded_x = pir.replica_sharded_variable(x, buffer, 0)
+        x = np.array([1, 2]).astype(np.int32)
+        remote_x, loaded_x = pir.replica_sharded_variable(x, dtypes.int32)
 
         y = pir.variable([3, 4])
         sharded_y = ops.collectives.replicated_reduce_scatter(
@@ -159,7 +156,7 @@ def test_replica_sharded_variable_no_gather():
         y_d2h = pir.d2h_stream(updated_x.shape, updated_x.dtype)
         ops.host_store(y_d2h, updated_x)
 
-    result, stored = run(ir, y_d2h, x)
+    result, stored = run(ir, y_d2h, remote_x)
     print(result.flatten())
     assert np.allclose(result.flatten(), [4, 6])
     assert np.allclose(stored, [4, 6])
@@ -173,8 +170,8 @@ def test_remote_replia_sharded_variable_no_gather():
     main = ir.main_graph
 
     with main:
-        x = pir.variable([1, 2], name="x")
-        buffer = pir.RemoteBuffer(ir, (x.nelms // 2, ), x.dtype, 1)
+        x = np.array([1, 2]).astype(np.int32)
+        buffer = pir.RemoteBuffer(ir, (x.size // 2, ), dtypes.int32, 1)
         remote_x = pir.remote_replica_sharded_variable(x, buffer, 0)
 
         y = pir.variable([3, 4], name="y")
@@ -182,17 +179,17 @@ def test_remote_replia_sharded_variable_no_gather():
             y, 'local', None, True)
 
         # loaded_x.shape = (1,)
-        loaded_x = ops.remote_load(buffer, remote_x, "x")
+        loaded_x = ops.remote_load(buffer, 0, "x")
 
         # Add to sharded x
         updated_x = ops.scaled_add_(loaded_x, sharded_y)
 
-        ops.remote_store(buffer, remote_x, updated_x)
+        ops.remote_store(buffer, 0, updated_x)
 
         y_d2h = pir.d2h_stream(updated_x.shape, updated_x.dtype)
         ops.host_store(y_d2h, updated_x)
 
-    result, stored = run(ir, y_d2h, x)
+    result, stored = run(ir, y_d2h, remote_x)
     np.testing.assert_equal(result.flatten(), [4, 6])
     np.testing.assert_equal(stored, [4, 6])
 
@@ -205,11 +202,11 @@ def test_remote_replia_sharded_reuse_buffer():
     main = ir.main_graph
 
     with main:
-        x1 = pir.variable([1, 2], name="x1")
-        x2 = pir.variable([3, 4], name="x2")
-        buffer = pir.RemoteBuffer(ir, (x1.nelms // 2, ), x1.dtype, 2)
-        pir.remote_replica_sharded_variable(x1, buffer, 0)
-        pir.remote_replica_sharded_variable(x2, buffer, 1)
+        x1 = np.array([1, 2]).astype(np.int32)
+        x2 = np.array([3, 4]).astype(np.int32)
+        buffer = pir.RemoteBuffer(ir, (x1.size // 2, ), dtypes.int32, 2)
+        pir.remote_replica_sharded_variable(x1, buffer, 0, name="x1")
+        pir.remote_replica_sharded_variable(x2, buffer, 1, name="x2")
 
         ops.remote_load(buffer, 0)
         ops.remote_load(buffer, 1)
