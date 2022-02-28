c998bb5a3996e48549f2a5da9e166749d2ed72b6
yanz <yanz@graphcore.ai>
2022-02-28 14:35:14 +0000

    add context manager to popart.ir user guide
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, michaell
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, michaell
    
    Subscribers: michaell, matthewha
    
    Maniphest Tasks: T56022, T47521
    
    Differential Revision: https://phabricator.sourcevertex.net/D57204

---
+++ docs/user_guide/files/in_sequence_popart_ir.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2022 Graphcore Ltd. All rights reserved.
+'''
+The intention of this example is to show how to use
+popart.ir context manager in_sequence
+'''
+
+import numpy as np
+import popart.ir as pir
+import popart.ir.ops as ops
+import popart
+
+# Creating a model with popart.ir
+ir = pir.Ir()
+main = ir.main_graph
+# Op begin
+with main:
+    x = pir.variable(1, pir.float32)
+    b = pir.constant(5, pir.float32)
+
+    with pir.in_sequence():
+        ops.var_updates.copy_var_update_(x, b)
+        # host store
+        o_d2h = pir.d2h_stream(x.shape, x.dtype, name="output_stream")
+        ops.host_store(o_d2h, x)
+# Op end
+dataFlow = popart.DataFlow(
+    batchesPerStep=1,
+    anchorTensors={o_d2h.tensor_id: popart.AnchorReturnType("All")})
+
+ir = ir._pb_ir
+ir.setDataFlow(dataFlow)
+opts = ir.getSessionOptions()
+opts.useHostCopyOps = True
+opts.enableExplicitMainLoops = True
+ir.updateVertices()
+ir.setIsPrepared()
+
+device = popart.DeviceManager().createCpuDevice()
+session = popart.InferenceSession.fromIr(ir=ir, deviceInfo=device)
+session.prepareDevice()
+anchors = session.initAnchorArrays()
+
+# run the model
+stepio = popart.PyStepIO({}, anchors)
+session.weightsFromHost()
+session.run(stepio)
+
+print(f"Result is {anchors[o_d2h.tensor_id]}")
