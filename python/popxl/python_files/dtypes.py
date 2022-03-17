9f2500311746ffc633d794cdc6ddf3c53710d0d4
Josh Levy-Kramer <joshl@graphcore.ai>
2022-03-17 13:30:01 +0000

    Popxl dtype: enable pytorch conversion. Add name property
    Summary:
    Add pytorch dtype conversion
    Add dtype name as public property
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, matthewha, oskarb
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, matthewha
    
    Subscribers: michaell
    
    Maniphest Tasks: T33421
    
    Differential Revision: https://phabricator.sourcevertex.net/D63242

--- python/popxl/python_files/dtypes.py
+++ python/popxl/python_files/dtypes.py
@@ -6,6 +6,11 @@ from typing import Any, Mapping
 import numpy as np
 import popart._internal.ir as _ir
 
+try:
+    import torch
+except ModuleNotFoundError:
+    torch = None
+
 # A dictionary to map from numpy to popxl types.
 _NP_TO_POPXL: Mapping[np.dtype, 'dtype'] = {}
 # A dictionary to map from string to popxl types.
@@ -14,6 +19,8 @@ _STR_TO_POPXL: Mapping[str, 'dtype'] = {}
 _PY_TO_POPXL: Mapping[Any, 'dtype'] = {}
 # A dictionart to map from popart._ir to popxl types.
 _PB_TO_POPXL: Mapping[_ir.DataType, 'dtype'] = {}
+# A dictionary to map from PyTorch to popxl types
+_PT_TO_POPXL: Mapping[Any, 'dtype'] = {}
 
 
 class dtype:
@@ -50,33 +57,65 @@ class dtype:
         self._is_signed: bool = None
         self._name: str = None
         self._np_type: np.dtype = None
+        self._pt_type = None
+        self._py_type = None
         self._pb_dtype: _ir.DataType = None
         raise TypeError(f"Cannot create {self.__module__}.dtype instances.")
 
     @property
+    def name(self) -> str:
+        """Name of dtype"""
+        return self._name
+
+    @property
     def is_complex(self) -> bool:
+        """True if a complex number dtype. False otherwise."""
         return self._is_complex
 
     @property
     def is_int(self) -> bool:
+        """True if an integer number dtype. False otherwise."""
         return self._is_int
 
     @property
     def is_floating_point(self) -> bool:
+        """True if a float point number dtype. False otherwise."""
         return self._is_floating_point
 
     @property
     def is_signed(self) -> bool:
+        """True if a signed number dtype. False otherwise."""
         return self._is_signed
 
     @classmethod
-    def as_dtype(cls, type_value) -> 'dtype':
+    def from_name(cls, name: str) -> 'dtype':
+        """Convert a dtype name into a dtype
+
+        Args:
+            name (str): Dtype name
+
+        Raises:
+            ValueError: If name does not exist
+
+        Returns:
+            dtype
+        """
+        try:
+            return _STR_TO_POPXL[name]
+        except (KeyError, TypeError):
+            pass
+
+        raise ValueError(
+            f'There is not a `popxl.dtype` that has a name: {name}.')
+
+    @classmethod
+    def as_dtype(cls, type_value: Any) -> 'dtype':
         """Converts the given `type_value` to a `popxl.dtype`.
 
         Args:
             type_value:
-                A `numpy.dtype`, string, a built-in Python type or a
-                `numpy.ndarray` which can be converted to a `popxl.dtype`.
+                A `numpy.dtype`, `torch.dtype`, string, a built-in Python type or a
+                `numpy.ndarray`, `torch.tensor` which can be converted to a `popxl.dtype`.
 
         Raises:
             ValueError:
@@ -106,6 +145,11 @@ class dtype:
             pass
 
         try:
+            return _PT_TO_POPXL[type_value]
+        except (KeyError, TypeError):
+            pass
+
+        try:
             return _NP_TO_POPXL[np.dtype(type_value)]
         except (KeyError, TypeError):
             pass
@@ -113,6 +157,11 @@ class dtype:
         if hasattr(type_value, 'dtype'):
             try:
                 return _NP_TO_POPXL[type_value.dtype]
+            except (KeyError, TypeError):
+                pass
+
+            try:
+                return _PT_TO_POPXL[type_value.dtype]
             except (KeyError, TypeError):
                 pass
 
@@ -135,7 +184,28 @@ class dtype:
             return self._np_type
         else:
             raise TypeError(f'`popxl.{self._name}` does not have a '
-                            f'corresponding NumPy type.')
+                            f'corresponding NumPy dtype.')
+
+    def as_torch(self) -> np.dtype:
+        """Converts the `popxl.dtype` to a corresponding `torch.dtype`.
+
+        Raises:
+            TypeError:
+                If the `popxl.dtype` cannot be converted to a `torch.dtype`.
+            ModuleNotFoundError:
+                If PyTorch is not installed.
+
+        Returns:
+            torch.dtype:
+                A `torch.dtype` corresponding to `popxl.dtype`.
+        """
+        if torch is None:
+            raise ModuleNotFoundError("PyTorch is not installed.")
+        if self._pt_type is not None:
+            return self._pt_type
+        else:
+            raise TypeError(f'`popxl.{self._name}` does not have a '
+                            f'corresponding PyTorch dtype.')
 
     def __repr__(self) -> str:
         return f'{self.__module__}.{self._name}'
@@ -149,7 +219,8 @@ class dtype:
             is_floating_point: bool,
             is_signed: bool,
             np_type: np.dtype,
-            py_type: str,
+            pt_type: str,
+            py_type: Any,
             pb_type: _ir.DataType,
     ) -> 'dtype':
         """Factory method to construct `dtype` instances.
@@ -167,6 +238,10 @@ class dtype:
                 Is the type signed.
             np_type (np.dtype):
                 A `numpy.dtype` that corresponds to the created `dtype`.
+            pt_type (str):
+                A PyTorch dtype name that corresponds to the created `dtype`.
+            py_type (Any):
+                Python native type that corresponds to the created `dtype`.
             pb_type (_ir.DataType):
                 The corresponding low-level `pybind11` `DataType`.
 
@@ -174,7 +249,7 @@ class dtype:
             dtype:
                 A new `dtype` instance.
         """
-        global _NP_TO_POPXL, _STR_TO_POPXL, _PY_TO_POPXL
+        global _NP_TO_POPXL, _STR_TO_POPXL, _PY_TO_POPXL, _PT_TO_POPXL
 
         self: 'dtype' = super().__new__(cls)
         self._is_complex = is_complex
@@ -182,12 +257,18 @@ class dtype:
         self._is_floating_point = is_floating_point
         self._is_signed = is_signed
         self._name = name
-        self._np_type = np_type
-        self._pb_dtype = pb_type
 
         if np_type is not None:
             assert np_type not in _NP_TO_POPXL
+            np_type = np.dtype(np_type)
             _NP_TO_POPXL[np_type] = self
+            self._np_type = np_type
+
+        if pt_type is not None and torch is not None:
+            assert pt_type not in _PT_TO_POPXL
+            pt_type = getattr(torch, pt_type)
+            _PT_TO_POPXL[pt_type] = self
+            self._pt_type = pt_type
 
         assert name not in _STR_TO_POPXL
         _STR_TO_POPXL[name] = self
@@ -195,34 +276,37 @@ class dtype:
         if py_type is not None:
             assert py_type not in _PY_TO_POPXL
             _PY_TO_POPXL[py_type] = self
+            self._py_type = py_type
 
         if pb_type is not None:
             assert py_type not in _PB_TO_POPXL
             _PB_TO_POPXL[pb_type] = self
+            self._pb_dtype = pb_type
 
         return self
 
 
 # yapf: disable
 # Fixed point types
-bool = dtype._factory('bool', False, False, False, False, np.dtype('bool_'), builtins.bool, _ir.DataType.BOOL)
-int8 = dtype._factory('int8', False, True, False, True, np.dtype('int8'), None, _ir.DataType.INT8)
-int16 = dtype._factory('int16', False, True, False, True, np.dtype('int16'), None, _ir.DataType.INT16)
-int32 = dtype._factory('int32', False, True, False, True, np.dtype('int32'), None, _ir.DataType.INT32)
-int64 = dtype._factory('int64', False, True, False, True, np.dtype('int64'), None, _ir.DataType.INT64)
-uint8 = dtype._factory('uint8', False, True, False, False, np.dtype('uint8'), None, _ir.DataType.UINT8)
-uint16 = dtype._factory('uint16', False, True, False, False, np.dtype('uint16'), None, _ir.DataType.UINT16)
-uint32 = dtype._factory('uint32', False, True, False, False, np.dtype('uint32'), None, _ir.DataType.UINT32)
-uint64 = dtype._factory('uint64', False, True, False, False, np.dtype('uint64'), None, _ir.DataType.UINT64)
+# Args: name, is_complex, is_int, is_floating_point, is_signed, np_type, pt_type, py_type, pb_type
+bool = dtype._factory('bool', False, False, False, False, 'bool_', 'bool', builtins.bool, _ir.DataType.BOOL)
+int8 = dtype._factory('int8', False, True, False, True, 'int8', 'int8', None, _ir.DataType.INT8)
+int16 = dtype._factory('int16', False, True, False, True, 'int16', 'int16', None, _ir.DataType.INT16)
+int32 = dtype._factory('int32', False, True, False, True, 'int32', 'int32', None, _ir.DataType.INT32)
+int64 = dtype._factory('int64', False, True, False, True, 'int64', 'int64', None, _ir.DataType.INT64)
+uint8 = dtype._factory('uint8', False, True, False, False, 'uint8', 'uint8', None, _ir.DataType.UINT8)
+uint16 = dtype._factory('uint16', False, True, False, False, 'uint16', None, None, _ir.DataType.UINT16)
+uint32 = dtype._factory('uint32', False, True, False, False, 'uint32', None, None, _ir.DataType.UINT32)
+uint64 = dtype._factory('uint64', False, True, False, False, 'uint64', None, None, _ir.DataType.UINT64)
 
 # Floating point types
-float16 = dtype._factory('float16', False, False, True, True, np.dtype('float16'), None, _ir.DataType.FLOAT16)
-float32 = dtype._factory('float32', False, False, True, True, np.dtype('float32'), builtins.float, _ir.DataType.FLOAT)
-float64 = dtype._factory('float64', False, False, True, True, np.dtype('float64'), None, _ir.DataType.DOUBLE)
+float16 = dtype._factory('float16', False, False, True, True, 'float16', 'float16', None, _ir.DataType.FLOAT16)
+float32 = dtype._factory('float32', False, False, True, True, 'float32', 'float32', builtins.float, _ir.DataType.FLOAT)
+float64 = dtype._factory('float64', False, False, True, True, 'float64', 'float64', None, _ir.DataType.DOUBLE)
 
 # Complex types
-complex64 = dtype._factory('complex64', True, False, False, True, np.dtype('complex64'), None, _ir.DataType.COMPLEX64)
-complex128 = dtype._factory('complex128', True, False, False, True, np.dtype('complex128'), None, _ir.DataType.COMPLEX128)
+complex64 = dtype._factory('complex64', True, False, False, True, 'complex64', 'complex64', None, _ir.DataType.COMPLEX64)
+complex128 = dtype._factory('complex128', True, False, False, True, 'complex128', 'complex128', None, _ir.DataType.COMPLEX128)
 # yapf: enable
 
 # Type aliases
--- tests/unittests/python/popxl/test_dtype.py
+++ tests/unittests/python/popxl/test_dtype.py
@@ -6,6 +6,7 @@ import torch
 
 import popxl
 from popxl.testing import get_all_dtypes, get_all_int_dtypes
+from popxl.dtypes import _PT_TO_POPXL, _NP_TO_POPXL
 
 
 # TODO(T38031): Delete this utility function.
@@ -39,13 +40,13 @@ class Testdtype:
                 # PyTorch doesn't have unsigned integers. These are tested
                 # below.
                 continue
-            torch_dtype = eval(f'torch.{popxl_dtype._name}')
+            torch_dtype = eval(f'torch.{popxl_dtype.name}')
             assert torch_dtype.is_complex == popxl_dtype.is_complex
             assert torch_dtype.is_floating_point == popxl_dtype.is_floating_point
             assert torch_dtype.is_signed == popxl_dtype.is_signed
             assert popxl_dtype.is_int == (popxl_dtype in int_dtypes)
         for popxl_dtype in uint_dtypes:
-            torch_dtype = eval(f'torch.{popxl_dtype._name[1:]}')
+            torch_dtype = eval(f'torch.{popxl_dtype.name[1:]}')
             assert torch_dtype.is_complex == popxl_dtype.is_complex
             assert torch_dtype.is_floating_point == popxl_dtype.is_floating_point
             assert torch_dtype.is_signed == True
@@ -59,7 +60,7 @@ class Testdtype:
     def test_conversion_numpy(self):
         popxl_dtypes = get_all_dtypes()
         np_dtypes = [
-            eval(f'np.{popxl_dtype._name}') for popxl_dtype in popxl_dtypes
+            eval(f'np.{popxl_dtype.name}') for popxl_dtype in popxl_dtypes
         ]
 
         for popxl_dtype, np_dtype in zip(popxl_dtypes, np_dtypes):
@@ -73,7 +74,7 @@ class Testdtype:
 
     def test_conversion_string(self):
         popxl_dtypes = get_all_dtypes()
-        names = [popxl_dtype._name for popxl_dtype in popxl_dtypes]
+        names = [popxl_dtype.name for popxl_dtype in popxl_dtypes]
 
         for popxl_dtype, name in zip(popxl_dtypes, names):
             assert popxl_dtype == popxl.dtype.as_dtype(name)
@@ -88,3 +89,29 @@ class Testdtype:
 
         for py_type, popxl_dtype in py_to_pir.items():
             assert popxl_dtype == popxl.dtype.as_dtype(py_type)
+
+    def test_conversion_torch(self):
+        assert len(_PT_TO_POPXL) > 0
+
+        # Dict of NumPy dtype -> torch dtype (when the correspondence exists)
+        # Adapted from torch internals
+        numpy_to_torch_dtype_dict = {
+            np.dtype('bool'): torch.bool,
+            np.dtype('uint8'): torch.uint8,
+            np.dtype('int8'): torch.int8,
+            np.dtype('int16'): torch.int16,
+            np.dtype('int32'): torch.int32,
+            np.dtype('int64'): torch.int64,
+            np.dtype('float16'): torch.float16,
+            np.dtype('float32'): torch.float32,
+            np.dtype('float64'): torch.float64,
+            np.dtype('complex64'): torch.complex64,
+            np.dtype('complex128'): torch.complex128
+        }
+
+        torch_to_np = {t: np for np, t in numpy_to_torch_dtype_dict.items()}
+
+        for torch_type, popxl_type in _PT_TO_POPXL.items():
+            np_type = torch_to_np[torch_type]
+            popxl_type_via_torch = _NP_TO_POPXL[np_type]
+            assert popxl_type_via_torch == popxl_type
