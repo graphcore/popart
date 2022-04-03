Fix bug where VariableSettings CommGroup is not respected by AllReduce in gradient and accumulator reduction.
Summary:
- Fixes VariableSettings bug
- Minor API cleanups
- Amend tests for collectives, variable settings and RTS correctness

Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, markk, johannesm, matthewha, michaell

Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, markk

Subscribers: bjornf, johannesm

Maniphest Tasks: T59382

Differential Revision: https://phabricator.sourcevertex.net/D64053

tests/integration/collectives_test.py
@@ -2,6 +2,7 @@
 import numpy as np
 import popart
 import test_util as tu
+import pytest
 
 
 @tu.requires_ipu
@@ -41,12 +42,88 @@ def test_replicated_allreduce():
 
 
 @tu.requires_ipu
+@pytest.mark.parametrize(
+    "commGroupAndVarSettings",
+    [(None, False),
+     (popart.CommGroup(popart.CommGroupType.Consecutive, 2), False),
+     (popart.CommGroup(popart.CommGroupType.Consecutive, 2), True)])
+def test_replicated_reducescatter(commGroupAndVarSettings):
+    commGroup = commGroupAndVarSettings[0]
+    useVarSettings = commGroupAndVarSettings[1]
+
+    replicatedGraphCount = 4
+    numIpus = replicatedGraphCount
+    replicas_per_group = replicatedGraphCount
+    num_groups = 1
+    num_shards = replicatedGraphCount
+    if commGroup != None:
+        num_shards = replicatedGraphCount // commGroup.replicaGroupSize
+        if useVarSettings:
+            num_groups = commGroup.replicaGroupSize
+            replicas_per_group = replicatedGraphCount // num_groups
+
+    input_data = np.array(range(12 * num_groups), dtype=np.float32)
+
+    if useVarSettings:
+        input_data = input_data.reshape([num_groups, -1])
+
+    builder = popart.Builder()
+
+    if useVarSettings:
+        variableSetting = popart.VariableSettings(commGroup)
+        builder.embedReplicationFactor(replicatedGraphCount)
+    else:
+        variableSetting = popart.VariableSettings()
+
+    t = builder.addInitializedInputTensor(input_data, variableSetting, "input")
+    o = builder.aiGraphcore.replicatedreducescatter([t], commGroup=commGroup)
+    builder.addOutputTensor(t)
+    builder.addOutputTensor(o)
+    proto = builder.getModelProto()
+
+    dataFlow = popart.DataFlow(1, {o: popart.AnchorReturnType("All")})
+    opts = popart.SessionOptions()
+    opts.enableReplicatedGraphs = True
+    opts.replicatedGraphCount = replicatedGraphCount
+
+    with tu.create_test_device(numIpus=numIpus) as device:
+        session = popart.InferenceSession(fnModel=proto,
+                                          dataFlow=dataFlow,
+                                          userOptions=opts,
+                                          deviceInfo=device)
+
+        session.prepareDevice()
+        session.weightsFromHost()
+
+        anchors = session.initAnchorArrays()
+
+        inputs = {}
+        stepio = popart.PyStepIO(inputs, anchors)
+
+        session.run(stepio)
+
+    ground_truth = num_shards * input_data.reshape(
+        [num_groups, num_shards, -1])
+
+    print(ground_truth)
+    print(anchors[o])
+
+    for i in range(num_groups):
+        for j in range(replicas_per_group):
+            assert np.allclose(
+                anchors[o][i * replicas_per_group + j].flatten(),
+                ground_truth[i][j % num_shards].flatten())
+
+
+@tu.requires_ipu
 def test_gcl_comm_group_attrs():
     input_data = np.array(range(10), dtype=np.float32)
     builder = popart.Builder()
     t = builder.addInitializedInputTensor(input_data, "input")
-    o1 = builder.aiGraphcore.replicatedallreduce(
-        [t], commGroup=[popart.CommGroupType.All, 4])
+    o1 = builder.aiGraphcore.replicatedallreduce([t],
+                                                 commGroup=popart.CommGroup(
+                                                     popart.CommGroupType.All,
+                                                     4))
     with builder.commGroup(popart.CommGroupType.Consecutive, 4):
         o2 = builder.aiGraphcore.replicatedallreduce([t])
     with builder.commGroup(popart.CommGroupType.Orthogonal, 4):