71ac97ea008754e375e7614448598f6f2cb38da1
Jaynie Padayachee <jayniep@graphcore.ai>
2022-04-19 13:04:32 +0200

    PopART C++ API: Add Doxygen comments to inputshapeinfo.hpp
    Summary: Linter fixes
    
    Test Plan: !docs-only
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, michaell
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, michaell
    
    Subscribers: michaell, matthewha
    
    Maniphest Tasks: T55782
    
    Differential Revision: https://phabricator.sourcevertex.net/D65025

--- willow/include/popart/inputshapeinfo.hpp
+++ willow/include/popart/inputshapeinfo.hpp
@@ -6,25 +6,63 @@
 
 namespace popart {
 
-// What is known about the Ir before it is run.
-// This knowledge can sometimes be compiled into the Ir,
-// and for certain backends is even required, for example
-// the IPU requires all Stream Tensor shapes.
-// In the future (TODO T5252) it will also contain indices for slicing
-// tensors (I think the LSTM from pytorch might require this)
+/**
+ * Class that contains what is known about the input tensors (as TensorInfo
+ * objects) in the IR prior to compilation.
+ *
+ * This knowledge can sometimes be compiled into the IR, and for certain
+ * backends is even required, for example the IPU requires all Stream Tensor
+ * shapes.
+ */
 class InputShapeInfo {
 public:
+  /**
+   * Default constructor for the InputShapeInfo class.
+   */
   InputShapeInfo() = default;
 
+  /**
+   * Add the identifier and TensorInfo object for a tensor to the
+   * InputShapeInfo object.
+   *
+   * \param TensorId The identifier of the tensor for which information is being
+   *     added.
+   * \param TensorInfo The tensor information to be added.
+   */
   void add(TensorId, const TensorInfo &);
+
+  /**
+   * Get the information of a tensor.
+   *
+   * \param TensorId The identifier of the tensor for which to get the tensor
+   *     information.
+   */
   const TensorInfo &get(TensorId) const;
+
+  /**
+   * Check if the InputShapeInfo object contains information for a tensor.
+   *
+   * \param TensorId The identifier of the tensor to check.
+   *
+   * \returns If `true`, the InputShapeInfo object contains information for the
+   *     tensor. If `false`, the InputShapeInfo object does not contain
+   *     information for the tensor.
+   */
   bool has(TensorId) const;
 
-  // return all unique TensorIds of tensors with any
-  // information stored in this object, either TensorInfo
-  // or an actual tensor
+  /**
+   * Get all unique tensor identifiers of tensors in the InputShapeInfo object.
+   *
+   * \returns Vector of tensor identifiers.
+   */
   std::vector<TensorId> getAllTensorIds() const;
 
+  /**
+   * Get all information contained the InputShapeInfo object.
+   *
+   * \returns Map of tensor identifiers and the corresponding tensor
+   * information.
+   */
   const std::map<TensorId, TensorInfo> &getInfos() const { return infos; }
 
 private:
