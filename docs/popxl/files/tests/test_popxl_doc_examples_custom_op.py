d6127908ddb4a4808bfda83aa596bc51d4ed00b0
Mark Kattenbelt <markk@graphcore.ai>
2022-04-21 19:38:43 +0000

    Put custom op tests in separate ctest and run separately.
    Test Plan: Ran this locally 1000 times and it did not fail. Without this change, running the doc_examples tests in parallel did sometimes fail locally, although not with the failure mode that was originally reported in the ticket.
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, matthewha
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, matthewha
    
    Maniphest Tasks: T60381
    
    Differential Revision: https://phabricator.sourcevertex.net/D65540

---
+++ docs/popxl/files/tests/test_popxl_doc_examples_custom_op.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2022 Graphcore Ltd. All rights reserved.
+import os
+from pathlib import Path
+
+import sys
+
+# In the source dir the examples_tester module lives in docs/shared/files/tests.
+# We add this to the system path so this file can be ran from the source dir.
+dir_docs = Path(__file__).parent.parent.parent.parent.resolve()
+dir_shared_tests = dir_docs.joinpath("shared", "files", "tests")
+sys.path.append(str(dir_shared_tests))
+
+from examples_tester import ExamplesTester
+
+working_dir = Path(os.path.dirname(__file__)).parent
+
+
+class TestPythonDocExamples(ExamplesTester):
+    """Test simple running of the examples included in the docs"""
+
+    def test_documentation_popxl_custom_op(self):
+        """Test the popxl custom op"""
+        filename = "run_leaky_relu_popxl.py"
+        self.run_python(filename,
+                        file_dir=working_dir,
+                        working_dir=working_dir,
+                        input_data=0.01)
---
+++ docs/user_guide/files/tests/test_popart_doc_examples_custom_op.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2022 Graphcore Ltd. All rights reserved.
+import os
+from pathlib import Path
+
+import sys
+
+# In the source dir the examples_tester module lives in docs/shared/files/tests.
+# We add this to the system path so this file can be ran from the source dir.
+dir_docs = Path(__file__).parent.parent.parent.parent.resolve()
+dir_shared_tests = dir_docs.joinpath("shared", "files", "tests")
+sys.path.append(str(dir_shared_tests))
+
+from examples_tester import ExamplesTester
+
+working_dir = Path(os.path.dirname(__file__)).parent
+
+
+class TestPythonDocExamples(ExamplesTester):
+    """Test simple running of the examples included in the docs"""
+
+    def test_documentation_custom_op(self):
+        """Test the weights example"""
+        filename = "run_leaky_relu_popart.py"
+        self.run_python(filename,
+                        file_dir=working_dir,
+                        working_dir=working_dir,
+                        input_data=0.1)
