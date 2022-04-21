9a4c3d61c2ab471816227f5746e4b3cc5dc20e06
Mark Kattenbelt <markk@graphcore.ai>
2022-04-21 10:46:32 +0100

    Re-enable most doc example tests
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, shirazb
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, shirazb
    
    Subscribers: shirazb
    
    Maniphest Tasks: T60381
    
    Differential Revision: https://phabricator.sourcevertex.net/D65480

--- docs/user_guide/files/tests/test_popart_doc_examples.py
+++ docs/user_guide/files/tests/test_popart_doc_examples.py
@@ -53,13 +53,13 @@ class TestPythonDocExamples(ExamplesTester):
                         file_dir=working_dir,
                         working_dir=working_dir)
 
-    def test_documentation_custom_op(self):
-        """Test the weights example"""
-        filename = "run_leaky_relu_popart.py"
-        self.run_python(filename,
-                        file_dir=working_dir,
-                        working_dir=working_dir,
-                        input_data=0.1)
+    #def test_documentation_custom_op(self):
+    #    """Test the weights example"""
+    #    filename = "run_leaky_relu_popart.py"
+    #    self.run_python(filename,
+    #                    file_dir=working_dir,
+    #                    working_dir=working_dir,
+    #                    input_data=0.1)
 
 
 class TestPopartCustomOperatorCube(ExamplesTester):
