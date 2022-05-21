5d1294d16d7c759137810b2a9bd8fb2be51ab421
Roman Derco <romand@graphcore.ai>
2022-05-21 14:32:56 +0100

    Document write variables data test
    Summary: Document write variables data test.
    
    Test Plan: No test.
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, markk
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, markk
    
    Subscribers: matthewha, #popart
    
    Maniphest Tasks: T62601
    
    Differential Revision: https://phabricator.sourcevertex.net/D67354

--- tests/integration/popxl/test_write_variables_data_cache.py
+++ tests/integration/popxl/test_write_variables_data_cache.py
@@ -6,20 +6,15 @@ from popxl import ops
 
 
 class TestWriteVariablesData:
-    def test_write_variables_data_no_cache(self):
-        ir = popxl.Ir()
-        with ir.main_graph:
-            v = popxl.variable(1, popxl.float32)
-            d2h = popxl.d2h_stream(v.shape, v.dtype)
-            ops.host_store(d2h, v)
-
-        with popxl.Session(ir, 'ipu_hw') as sess:
-            assert sess.run()[d2h] == 1
-            sess.write_variable_data(v, 2)
-            assert sess.run()[d2h] == 2
-            sess.write_variable_data(v, 3)
-            assert sess.run()[d2h] == 3
-
+    # Test that write_variable_data updates variable data on host and
+    # that its weight is updated on device when using POPART_CACHE_DIR, [A].
+    # This test was failing, see T60847. But was passing when POPART_CACHE_DIR
+    # was not used, [B].
+    # The reason was that for [B] weights are populated by IR and we were
+    # updating IR when calling write_variable_data.
+    # For [A] the weights were not updated as in this case
+    # they are not populated by IR but during second executablex constructor - deserialized
+    # version. The issue was fixed by updating weights when calling write_variable_data.
     def test_write_variables_data_cache(self, tmp_path):
         opts = popart.SessionOptions()
         popart.getLogger().setLevel('DEBUG')
@@ -34,5 +29,7 @@ class TestWriteVariablesData:
             assert sess.run()[d2h] == 1
             sess.write_variable_data(v, 2)
             assert sess.run()[d2h] == 2
+            sess.write_variable_data(v, 3)
+            assert sess.run()[d2h] == 3
 
         del os.environ['POPART_CACHE_DIR']
