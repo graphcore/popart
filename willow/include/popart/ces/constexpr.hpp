38f5c32d315f013a60779cab890359b04e767f77
Shiraz Butt <shirazb@graphcore.ai>
2022-12-06 12:19:55 +0000

    Optimise away some TensorData copies during const-folding
    Summary:
    Fixes T71993.
    !ci_branch_mk2
    
    Add `Tensors::makeConstInit(std::vector<char> &&)` that uses `TensorDataByEmplaceOf`; update `ConstExprUtil::makeTensorConstInit` and its callsites to use this.
    In `Tensor::getDataViaGraphTraversal`, use `setTensorDataByEmplaceOf` on returned `vec<char>` instead of unnecessarily copying.
    In `Tensor::getDataViaGraphTraversal`, optimise search over callsites to only make 1 copy of the data instead of `O(callsites)`, then use `setTensorDataByEmplaceOf` instead of copying.
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, artemiyb
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, artemiyb
    
    Subscribers: artemiyb
    
    Maniphest Tasks: T71993
    
    Differential Revision: https://phabricator.sourcevertex.net/D78630

--- willow/include/popart/ces/constexpr.hpp
+++ willow/include/popart/ces/constexpr.hpp
@@ -75,7 +75,7 @@ public:
 private:
   // make the tensor `name` into a constInit tensor
   static void
-  makeTensorConstInit(const TensorId name, const void *data, Graph &);
+  makeTensorConstInit(const TensorId name, std::vector<char> data, Graph &);
 };
 
 // Manager class for ConstExprOp's
