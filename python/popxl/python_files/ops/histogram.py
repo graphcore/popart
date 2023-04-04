54a2a3f6a8e1aa1fd4c1f16e96f8556078fc1c28
Jamie Packer <jamiep@graphcore.ai>
2023-04-04 12:15:53 +0200

    Improve wording for PopXL histogram op
    Summary:
    Remove a redundant (and slightly ungrammatical) sentence.
    !docs-only
    !ci_branch_mk2
    
    Test Plan: CI
    
    Reviewers: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, shirazb
    
    Reviewed By: #popart, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, shirazb
    
    Maniphest Tasks: T15620
    
    Differential Revision: https://phabricator.sourcevertex.net/D82548

--- python/popxl/python_files/ops/histogram.py
+++ python/popxl/python_files/ops/histogram.py
@@ -13,20 +13,27 @@ def histogram(t: Tensor, levels: List[float], absolute_of_input: bool) -> Tensor
     r"""
     Compute the histogram of the input tensor.
 
+    All but the last bin are half-open. In other words, if `levels` is:
+
+    ```
+    [1, 2, 3, 4]
+    ```
+
+    then the first bin is [1, 2) (including 1, but excluding 2) and the second [2, 3). The last bin, however, is [3, 4], which includes 4.
+
     See also `PyTorch torch.histc <https://pytorch.org/docs/stable/generated/torch.histc.html>`__, :py:func:`NumPy histogram <numpy.histogram>`.
 
     Args:
         t:
             Input tensor.
         levels:
-            A monotonically increasing list of bin edges
+            A monotonically increasing list of bin edges.
         absolute_of_input:
-            Absolute the inputs tensor before gathers the histogram representing
             If True, the absolute value of each input is calculated before comparison to the `levels` data.
 
     Returns:
         Tensor:
-            Output counts.
+            Counts of the number of values in each bin.
     """
     ctx = get_current_context()
     g = ctx.graph
